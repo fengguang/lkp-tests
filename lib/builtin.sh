5def2a2be2fbaed16445836ebe87572a424208ad
Fengguang Wu <fengguang.wu@intel.com>
2014-08-22 21:31:02 +0800

    lib/builtin.sh: move redirection into sleep()
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/builtin.sh
+++ lib/builtin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-exec 50< /dev/tty50
-
 sleep()
 {
 	local seconds=${1%s}
 	[[ $seconds =~ 'm' ]] && seconds=$(( ${seconds%m} * 60 ))
 	[[ $seconds =~ 'h' ]] && seconds=$(( ${seconds%h} * 3600 ))
+
+	exec 50< /dev/tty50
 	read -t $seconds -u 50
 }
 
