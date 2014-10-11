ec8d0cac26ecef8d28641de08f46633cd6dd6bca
Fengguang Wu <fengguang.wu@intel.com>
2014-10-11 19:53:38 +0800

    bounds.rb: add caching
    To avoid excessive file stat().
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/bounds.rb
+++ lib/bounds.rb
@@ -3,8 +3,19 @@
 def is_valid_stats_range(stats_field, num)
 	monitor = stats_field.split('.')[0]
 	range_file = "#{LKP_SRC}/etc/valid-range-#{monitor}.yaml"
-	if File.exist? range_file
-		stats_range = load_json range_file
+
+	$__valid_range_cache ||= {}
+	unless $__valid_range_cache.include?(range_file)
+		if File.exist? range_file
+			$__valid_range_cache[range_file] = load_json range_file
+		else
+			$__valid_range_cache[range_file] = nil
+		end
+	end
+
+	stats_range = $__valid_range_cache[range_file]
+
+	if stats_range
 		stats_range.each { |k, v|
 			if stats_field =~ %r{^#{k}$}
 				min = v[0]
