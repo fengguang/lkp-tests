5ad346d8b7fe8d6bbbe654a4f91cc4fe32e80460
Fengguang Wu <fengguang.wu@intel.com>
2015-01-04 14:02:25 -0800

    lib/env.sh: define portable way to get number of processors
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/env.sh
+++ lib/env.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+has_cmd()
+{
+	command -v "$1" >/dev/null
+}
+
+nproc()
+{
+	if has_cmd 'nproc'; then
+		command 'nproc'
+	elif has_cmd 'getconf'; then
+		getconf '_NPROCESSORS_CONF'
+	else
+		grep -c '^processor' /proc/cpuinfo
+	fi
+}
+
 role()
 {
 	# $host_roles will be determined at job schedule time and
