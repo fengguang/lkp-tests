b0561331c5ffc4769c9ae626621603555f72d45b
Fengguang Wu <fengguang.wu@intel.com>
2015-01-03 21:59:45 -0800

    lib/env.sh: switch /bin/sh
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/env.sh
+++ lib/env.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 role()
 {
 	# $host_roles will be determined at job schedule time and
 	# set accordingly in each scheduled job
-	[[ " $host_roles " =~ " $1 " ]]
+	local __my_host_rules=" $host_roles "
+
+	[ "${__my_host_rules#* $1 }" != "$__my_host_rules" ]
 }
 
