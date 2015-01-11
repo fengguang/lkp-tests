95cc993621cace7858cd909335699fd90385fe90
Fengguang Wu <fengguang.wu@intel.com>
2015-01-10 20:17:51 -0800

    monitors: introduce wait_timeout()
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/wait.sh
+++ lib/wait.sh
@@ -4,7 +4,21 @@ WAIT_POST_TEST_CMD="$LKP_SRC/monitors/event/wait post-test"
 
 wait_post_test()
 {
-	$WAIT_POST_TEST_CMD
+	$WAIT_POST_TEST_CMD "$@"
+}
+
+wait_timeout()
+{
+	local timeout="${1:-1}"
+	local exit_code
+
+	# wait returns
+	# - 0 on post-test => job finished, let's exit too
+	# - 62 on timeout
+	# - others on error => exit to avoid busy looping on caller side
+	$WAIT_POST_TEST_CMD --timeout "$timeout"
+	exit_code=$?
+	[ $exit_code -eq 62 ] || exit $exit_code
 }
 
 echo $$ >> $TMP/.pid-wait-monitors
