b43ff1cb4953b5c4272a2328e5b95a04d85cf8d8
Zexi Li <zexi.li@intel.com>
2015-03-25 17:14:18 +0800

    bin/run-local.sh: small shell script for running JOB.sh
    It's a simplified shell version of bin/run-local and for running job.sh attached in our report emails.
    
    Signed-off-by: Zexi Li <zexi.li@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

---
+++ bin/run-local.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+[ -n "$LKP_SRC" ] || export LKP_SRC=$(dirname $(dirname $(readlink -e -v $0)))
+export TMP=/tmp/lkp
+export PATH=$PATH:$LKP_SRC/bin
+export BENCHMARK_ROOT=/lkp/benchmarks
+
+usage()
+{
+    cat <<EOF
+Usage: run-local [--dry-run] [-o RESULT_ROOT] JOB_SCRIPT
+
+options:
+    -o  RESULT_ROOT         dir for storing all results
+EOF
+    exit 1
+}
+
+while getopts "o:" opt
+do
+    case $opt in
+    o ) opt_result_root="$OPTARG" ;;
+    ? ) usage ;;
+    esac
+done
+
+shift $(($OPTIND-1))
+job_script=$1
+[ -n "$job_script" ] || usage
+
+if [ -z $opt_result_root ]; then
+    [ -n "$RESULT_ROOT" ] || usage
+    mkdir -p -m 02775 $RESULT_ROOT
+else
+    mkdir -p -m 02775 $opt_result_root
+    export RESULT_ROOT=$(realpath $opt_result_root)
+fi
+
+export TMP_RESULT_ROOT=$RESULT_ROOT
+rm -rf $TMP
+mkdir $TMP
+
+$job_script run_job
+
+$LKP_SRC/bin/post-run
+$LKP_SRC/monitors/event/wakeup job-finished
