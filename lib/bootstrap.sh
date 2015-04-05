bda9a7b603fb07fd91b386b68bdd16547066d5d8
Fengguang Wu <fengguang.wu@intel.com>
2015-04-05 19:47:48 +0800

    export bootstrap and test runner scripts
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

---
+++ lib/bootstrap.sh
@@ -0,0 +1,358 @@
+#!/bin/sh
+
+mount_tmpfs()
+{
+	# ubuntu etc/init/mounted-tmp.conf wrongly mounted /tmp:
+	# mount -t tmpfs -o size=1048576,mode=1777 overflow /tmp
+	# grep -q /tmp /proc/mounts && umount /tmp
+
+	mountpoint -q /tmp && return
+
+	mount -t tmpfs -o mode=1777 tmp /tmp
+}
+
+network_ok()
+{
+	local i
+	for i in /sys/class/net/*/
+	do
+		[ "${i#*/lo/}" != "$i" ] && continue
+		[ "$(cat $i/operstate)" = 'up' ]	&& return 0
+		[ "$(cat $i/carrier)" = '1' ]	&& return 0
+	done
+	return 1
+}
+
+setup_network()
+{
+	network_ok && return
+	$LKP_DEBUG_PREFIX $LKP_SRC/bin/run-ipconfig
+	network_ok && return
+
+	local err_msg='IP-Config: Auto-configuration of network failed'
+	dmesg | grep -q -F "$err_msg" || {
+		# Include $err_msg in the error message so that it matches
+		# /lkp/printk-error-messages
+		# and be detected as a dmesg error. Add some prefix/sufix
+		# to slightly distinguish it from kernel DHCP failure message.
+		echo "!!! $err_msg !!!" >&2
+		echo "!!! $err_msg !!!" > /dev/ttyS0
+	}
+	reboot
+	exit
+}
+
+add_lkp_user()
+{
+	getent passwd lkp >/dev/null && return
+
+	mkdir -p /home
+	groupadd --gid 1090 lkp
+	useradd --uid 1084 --gid 1090 lkp
+}
+
+run_ntpdate()
+{
+	[ "$LKP_SERVER" = inn ] || return
+	[ -x '/usr/sbin/ntpdate' ] || return
+
+	local hour="$(date +%H)"
+
+	ntpdate -b $LKP_SERVER
+
+	[ "$hour" != "$(date +%H)" ] && [ -x '/etc/init.d/hwclock.sh' ] && {
+		# update hardware clock, to carry the adjusted time across reboots
+		/etc/init.d/hwclock.sh restart 2> /dev/null
+	}
+}
+
+setup_hostname()
+{
+	export HOSTNAME=${testbox:-localhost}
+
+	echo $HOSTNAME > /tmp/hostname
+	ln -fs /tmp/hostname /etc/hostname
+
+	hostname $HOSTNAME
+}
+
+setup_hosts()
+{
+	if [ -f '/etc/hosts-orig' ]; then
+		cp /etc/hosts-orig /tmp/hosts
+	else
+		cp /etc/hosts /tmp/hosts
+	fi
+	echo "127.0.0.1 $HOSTNAME.sh.intel.com  $HOSTNAME" >> /tmp/hosts
+	ln -fs /tmp/hosts /etc/hosts
+}
+
+announce_bootup()
+{
+	local version="$(cat /proc/sys/kernel/version | cut -f1 -d' ' | cut -c2-)"
+	local release="$(cat /proc/sys/kernel/osrelease)"
+	local mac="$(ip link | awk '/ether/ {print $2; exit}')"
+
+	echo 'Kernel tests: Boot OK!'
+
+	# make sure to output something if serial console is not ttyS0
+	# this helps diagnose serial console connections
+	for ttys in ttyS0 ttyS1 ttyS2 ttyS3
+	do
+		echo "LKP: HOSTNAME $HOSTNAME, MAC $mac, kernel $release $version, serial console /dev/$ttys" > /dev/$ttys 2>/dev/null
+	done
+}
+
+redirect_stdout_stderr()
+{
+	exec  > $TMP/stdout
+	exec 2> $TMP/stderr
+
+	ln -s /usr/bin/tail $LKP_SRC/bin/tail-to-console
+	ln -s /usr/bin/tail $LKP_SRC/bin/tail-to-output
+	ln -s /usr/bin/tail $LKP_SRC/bin/tail-to-serial
+
+	# write log to screen as well so that we can see them
+	# even network is broken
+
+	for i in $(seq 10)
+	do
+		[ -c /dev/console ] &&
+		[ -c /dev/ttyS0   ] && break
+		sleep 1
+	done
+
+	# the test fixes "cannot create /dev/console: Input/output error"
+	[ -c /dev/console ] &&
+	tail-to-console -f $TMP/stdout $TMP/stderr > /dev/console &
+
+	tail-to-output  -f $TMP/stdout $TMP/stderr > $TMP/output &
+
+	# some machines do not have serial console, writing to /dev/ttyS0 may fail
+	[ -c /dev/ttyS0 ] &&
+	echo > /dev/ttyS0 2>/dev/null && {
+		tail-to-serial -f $TMP/stdout $TMP/stderr > /dev/ttyS0 2>/dev/null &
+	}
+}
+
+fixup_packages()
+{
+	ldconfig
+
+	[ -x /usr/bin/gcc ] && [ ! -e /usr/bin/cc ] &&
+	ln -sf gcc /usr/bin/cc
+
+	# hpcc dependent library
+	[ -e /usr/lib/atlas-base/atlas/libblas.so.3 ] && [ ! -e /usr/lib/libblas.so.3 ] &&
+	ln -sf /usr/lib/libblas.so.3 /usr/lib/atlas-base/atlas/libblas.so.3
+
+	local aclocal_bin
+	for aclocal_bin in /usr/bin/aclocal-*
+	do
+		[ -x "$aclocal_bin" ] && [ ! -e /usr/bin/aclocal ] &&
+		ln -sf $aclocal_bin /usr/bin/aclocal
+	done
+}
+
+mount_debugfs()
+{
+	if [ ! -e /sys/kernel/debug/tracing/ ]; then
+		mount -t debugfs none /sys/kernel/debug
+	fi
+}
+
+mount_cgroup()
+{
+	[ -f "$CGROUP_MNT/tasks" ] && return
+	awk 'NR > 1 {print "\\s\\+" $1 "\\."}' /proc/cgroups > $TMP/availble-cgroup_subsys
+	cgroup_subsys=$(grep -o -f $TMP/availble-cgroup_subsys $job)
+	[ -n "$cgroup_subsys" ] || return
+	cgroup_subsys=$(echo $cgroup_subsys | sed -e 's/\. /,/g' -e 's/\.$//')
+	cmd mkdir -p $CGROUP_MNT
+	cmd mount -t cgroup -o $cgroup_subsys none $CGROUP_MNT
+}
+
+mount_rootfs()
+{
+	[ -n "$rootfs_partition" ] || return
+	mkdir -p /opt/rootfs
+	mount $rootfs_partition /opt/rootfs && return
+	mkfs.ext4 -q $rootfs_partition
+	mount $rootfs_partition /opt/rootfs
+}
+
+validate_result_root()
+{
+	[ -n "$RESULT_ROOT" ] || RESULT_ROOT=$result_root
+	[ -n "$RESULT_ROOT" ] || {
+		echo 'No RESULT_ROOT' >&2
+		run_job_failed=1
+		job_done_boot_next
+	}
+}
+
+should_do_cifs()
+{
+	grep -q clear-linux-os /etc/os-release && return 0
+	grep -q -w 'nfs' /proc/filesystems && return 1
+	modprobe nfs 2>/dev/null
+	grep -q -w 'nfs' /proc/filesystems && return 1
+	return 0
+}
+
+mount_result_root()
+{
+	[ -n "$result_service" ] || {
+		if should_do_cifs; then
+			result_service=//$LKP_SERVER/result
+		else
+			result_service=$LKP_SERVER:/result
+		fi
+	}
+
+	case $result_service in
+		*:*)
+			mount.nfs $result_service $RESULT_MNT || return
+			result_fs=nfs
+			;;
+		//*/*)
+			modprobe cifs 2>/dev/null
+			mount.cifs -o guest $result_service $RESULT_MNT || return
+			result_fs=cifs
+			;;
+		*)
+			echo "unknown result_service $result_service" >&2
+			return 1
+			;;
+	esac
+
+	mountpoint -q $RESULT_MNT
+}
+
+setup_result_root()
+{
+	validate_result_root
+
+	echo RESULT_ROOT=$RESULT_ROOT
+	echo job=$job
+
+	export TMP_RESULT_ROOT=$TMP/result
+	mkdir -p $TMP_RESULT_ROOT
+
+	RESULT_PREFIX=/$LKP_SERVER
+	export RESULT_ROOT=$RESULT_PREFIX$RESULT_ROOT
+	export RESULT_MNT=$RESULT_PREFIX/result
+
+	mkdir -p $RESULT_MNT
+	mount_result_root $RESULT_MNT || {
+		if grep -q -w $result_fs /proc/filesystems; then
+			set_job_state 'error_mount'
+			sleep 300
+		else
+			set_job_state "miss_$result_fs"
+		fi
+		job_done_boot_next
+	}
+
+	local files="$(echo $RESULT_ROOT/*)"
+	[ -e "$files" ] && echo "RESULT_ROOT not empty: $files" >&2
+}
+
+record_dmesg()
+{
+	killall rsyslogd klogd 2>/dev/null || :
+	dmesg > $RESULT_ROOT/dmesg
+	ln -s $(command -v cat) $LKP_SRC/bin/cat-kmsg
+	stdbuf -o0 -e0 cat-kmsg /proc/kmsg >> $RESULT_ROOT/dmesg &
+	echo $! > $TMP/pid-dmesg
+	[ -n "$BASH_VERSION" ] && disown # quiet the "Terminated" notification to stderr
+
+	[ -n "$netconsole_port" ] && {
+		# use default gateway as netconsole server
+		netconsole_server=$(ip -4 route list 0/0 | cut -f3 -d' ')
+		modprobe netconsole netconsole=@/,$netconsole_port@$netconsole_server/ 2>/dev/null
+	}
+}
+
+# in case someone is logged in, give him at most 10hour time to do manual checks
+wait_on_manual_check()
+{
+	# extra sleep for user login after a failed job,
+	# if "wait_debug_on_fail" is defined in the job file
+	[ "$run_job_failed" != 0 ] && [ -n "$wait_debug_on_fail" ] && {
+		sleep $wait_debug_on_fail
+	}
+
+	for i in $(seq 600)
+	do
+		# t100 has XWindow auto login
+		if [ "$HOSTNAME" = 't100' ]; then
+			local users="$(users)"
+			[ "${users#* }" != "$users" ] || break
+		else
+			[ -n "$(users)" ] || break
+		fi
+		disturbed=1
+		[ "$i" = 1 ] && set_job_state 'manual_check'
+		sleep 60
+	done
+	return $disturbed
+}
+
+tbox_cant_kexec()
+{
+	[ "$(virt-what)" = 'kvm' ] && return 0
+
+	# following tbox are buggy while using kexec to boot
+	[ "${HOSTNAME#*lkp-nex04}"	!= "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-t410}"	!= "$HOSTNAME" ] && return 0
+
+	[ -x '/sbin/kexec' ] || return 0
+
+	return 1
+}
+
+jobfile_append_var()
+{
+	local i
+	for i
+	do
+		echo "$i" >> $job_script
+	done
+}
+
+set_job_state()
+{
+	jobfile_append_var "job_state=$1"
+}
+
+job_done()
+{
+	:
+}
+
+boot_next()
+{
+	tbox_cant_kexec && {
+		reboot
+		exit
+	}
+
+	mount_rootfs
+
+	local secs=300
+	while true
+	do
+		$LKP_SRC/bin/kexec-lkp $pxe_user
+		sleep $secs || exit # killed by reboot
+		secs=$(( secs + 300 ))
+	done
+}
+
+job_done_boot_next() {
+	touch $TMP/job-finished
+	$LKP_SRC/monitors/event/wakeup job-finished
+	wait_on_manual_check
+	[ -n "$disturbed" ] || job_done
+	boot_next
+}
---
+++ lib/kexec.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+read_kernel_cmdline_vars_from_append()
+{
+	for i in $1
+	do
+		[ "$i" != "${i#job=}" ]			&& export "$i"
+		[ "$i" != "${i#initrd=}" ]		&& export "$i"
+		[ "$i" != "${i#bm_initrd=}" ]		&& export "$i"
+		[ "$i" != "${i#job_initrd=}" ]		&& export "$i"
+		[ "$i" != "${i#lkp_initrd=}" ]		&& export "$i"
+		[ "$i" != "${i#modules_initrd=}" ]	&& export "$i"
+		[ "$i" != "${i#tbox_initrd=}" ]		&& export "$i"
+	done
+}
+
+download_kernel_initrd()
+{
+	local _initrd
+	local initrds
+	local kernel=$(awk  '/^KERNEL / { print $2; exit }' $NEXT_JOB)
+	kernel=$(echo $kernel | sed 's/^\///')
+
+	append=$(grep -m1 '^APPEND ' $NEXT_JOB | sed 's/^APPEND //')
+	rm -f /tmp/initrd-* /tmp/modules.cgz
+
+	read_kernel_cmdline_vars_from_append "$append"
+	append=$(echo "$append" | sed -r 's/ [a-z_]*initrd=[^ ]+//g')
+
+	echo "downloading kernel image ..."
+	set_job_state "wget_kernel"
+	kernel_file=$CACHE_DIR/$kernel
+	wget "http://$LKP_SERVER:$LKP_CGI_PORT/~$LKP_USER/$kernel" -nv -N -P $(dirname $kernel_file) || {
+		echo "failed to download kernel: $kernel" 1>&2
+		exit 1
+	}
+
+	echo "downloading initrds ..."
+	set_job_state "wget_initrd"
+	for _initrd in $(echo $initrd $tbox_initrd $job_initrd $lkp_initrd $bm_initrd $modules_initrd | tr , ' ')
+	do
+		_initrd=$(echo $_initrd | sed 's/^\///')
+		local file=$CACHE_DIR/$_initrd
+		wget -nv -N "http://$LKP_SERVER:$LKP_CGI_PORT/~$LKP_USER/$_initrd" -P $(dirname $file) || {
+			echo Failed to download $_initrd
+			exit 1
+		}
+		initrds="${initrds}$file "
+	done
+
+	[ -n "$initrds" ] && {
+		concatenate_initrd="/tmp/initrd-$$"
+		cat $initrds > $concatenate_initrd
+		initrd_option="--initrd=$concatenate_initrd"
+	}
+	return 0
+}
+
+kexec_to_next_job()
+{
+	echo "geting new job..."
+	[ -n "$HOSTNAME" ] || HOSTNAME=$testbox
+	[ -n "$HOSTNAME" ] || HOSTNAME=$(hostname)
+	wget "http://$LKP_SERVER:$LKP_CGI_PORT/~$LKP_USER/cgi-bin/gpxelinux.cgi?hostname=${HOSTNAME}&lkp_wtmp" \
+	     -nv -O $NEXT_JOB
+	grep -q "^KERNEL " $NEXT_JOB || {
+		echo "no KERNEL found" 1>&2
+		cat $NEXT_JOB
+		exit 1
+	}
+
+	download_kernel_initrd
+
+	set_job_state "booting"
+
+	echo "LKP: kexec loading..."
+	echo kexec -l $kernel_file $initrd_option --append=\"$append\"
+	kexec -l $kernel_file $initrd_option --append="$append"
+
+	if [ -n "$(find /etc/rc6.d -name '[SK][0-9][0-9]kexec' 2>/dev/null)" ]; then
+		# expecting the system to run "kexec -e" in some rc6.d/* script
+		echo "LKP: rebooting"
+		echo "LKP: rebooting" > /dev/ttyS0 &
+		reboot
+		sleep 100 || exit	# exit if reboot kills sleep as expected
+	fi
+
+	# run "kexec -e" manually. This is not a clean reboot and may lose data,
+	# so run umount and sync first to reduce the risks.
+	umount -a
+	sync
+	echo "LKP: kexecing"
+	echo "LKP: kexecing" > /dev/ttyS0 &
+	kexec -e
+
+	set_job_state "kexec_fail"
+
+	# in case kexec failed
+	echo "LKP: rebooting after kexec"
+	echo "LKP: rebooting after kexec" > /dev/ttyS0 &
+	reboot
+}
+
+# cache kernel for only one week to avoid "no splace left" issue
+cleanup_kernel_cache()
+{
+	local kernel_cache=$1
+	local cleanup_stamp=$kernel_cache/cleanup_stamp/$(date +%U)
+	[ -d "$cleanup_stamp" ] && return
+	mkdir $cleanup_stamp -p
+
+	find "$kernel_cache" \( -type f -mtime +7 -delete \) -or \( -type d -ctime +7 -empty -delete \)
+}
+
