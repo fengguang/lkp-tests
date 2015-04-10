392d5f1944390eb804cf4fed8a3b95fadb17b5b7
Huang Ying <ying.huang@intel.com>
2015-04-10 16:24:25 +0800

    Add calss Cached
    To be used as super class for classes to cache their instances.  For
    example, Commit can cache its instances.
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/common.rb
+++ lib/common.rb
@@ -3,3 +3,14 @@
 def deepcopy(o)
 	Marshal.load(Marshal.dump(o))
 end
+
+# Cache the instances of the class
+class Cached
+end
+
+class << Cached
+	def cached_new(key, *args)
+		@cache ||= {}
+		@cache[key] or @cache[key] = new(*args)
+	end
+end
