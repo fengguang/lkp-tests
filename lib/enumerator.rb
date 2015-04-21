61f750d8bd652adc7ca0949142326e9fd44a2309
Huang Ying <ying.huang@intel.com>
2015-04-21 15:26:52 +0800

    lib/enumerator.rb: Enhance enumerator
    Add some new functionality to the enumerator
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

---
+++ lib/enumerator.rb
@@ -0,0 +1,55 @@
+# Enhancement to enumerable and enumerator
+
+module Enumerable
+	def feach(func)
+		block_given? or return enum_for(__method__, func)
+
+		each { |*args|
+			func.call(*args)
+			yield *args
+		}
+	end
+
+	def fmap(func)
+		block_given? or return enum_for(__method__, func)
+
+		each { |*args|
+			yield func.call(*args)
+		}
+	end
+
+	def fselect(func, &b)
+		block_given? or return enum_for(__method__, func)
+
+		each { |*args|
+			yield *args if func.call(*args)
+		}
+	end
+
+	def fchain(func, &b)
+		block_given? or return enum_for(__method__, func)
+
+		each { |*args|
+			func.call(*args, &b)
+		}
+	end
+end
+
+class EnumeratorCollection
+	include Enumerable
+
+	def initialize(*enums)
+		@enumerators = enums
+	end
+
+	def <<(enum)
+		@enumerators << enum
+		self
+	end
+
+	def each(&b)
+		@enumerators.each { |enum|
+			enum.each(&b)
+		}
+	end
+end
