6595370a9d1d698df076a64d3aeac4f1737d4a98
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2015-05-15 14:03:50 +0800

    job.sh: fix bashism array using
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/job.sh
+++ lib/job.sh
@@ -85,16 +85,14 @@ wait_other_nodes()
 					 "direct_macs=${direct_macs// /+}" \
 					 "direct_ips=${direct_ips// /+}"
 
-	[ -n "$direct_macs" ] && {
-		local macs_arr=($direct_macs)
-		local ips_arr=($direct_ips)
-		for idx in $(seq 0 $((${#macs_arr[@]} - 1)))
-		do
-			DIRECT_DEVICE=$(ip link | grep -B1 ${macs_arr[$idx]} | awk -F': ' 'NR==1 {print $2}') \
-			DIRECT_IP=${ips_arr[$idx]} \
-			$LKP_DEBUG_PREFIX $LKP_SRC/bin/run-ipconfig
-		done
-	}
+	local idx=1
+	for mac in $direct_macs
+	do
+		DIRECT_DEVICE=$(ip link | grep -B1 $mac | awk -F': ' 'NR==1 {print $2}') \
+		DIRECT_IP=$(echo $direct_ips | cut -d' ' -f $idx) \
+		$LKP_DEBUG_PREFIX $LKP_SRC/bin/run-ipconfig
+		((idx++))
+	done
 
 	# exit if either of the other nodes failed its job
 
