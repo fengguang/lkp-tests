3303d60f82c2ff7e51d4629f5f6bf96fa1db6bd1
Zexi Li <zexi.li@intel.com>
2015-06-08 11:52:03 +0800

    Support archlinux packages installation
    bin/setup-local: support install archlinux packages
    lib/install.rb: get distro packages by parsing #{distro}-packages.yaml file
    distro/archlinux-packages.yaml: descript the different package name between archlinux and debian.
    distro/scripts/*: external scripts to install distro packages
    
    Note:
    	Generic packages' name are based on debian, the difference and
    	nonexit of each packages are descriped in 'archlinux-packages.yaml'.
    e.g:
    	xz-utils: xz # 'xz-utils' is 'xz' packagename in debian, will replace 'xz-utils' packagename if distro is archlinux
    	bsdtar: # bsdstar not exist in archlinux, will not install when distro is archlinux
    
    Signed-off-by: Zexi Li <zexi.li@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/install.rb
+++ lib/install.rb
@@ -2,16 +2,31 @@
 
 LKP_SRC ||= ENV['LKP_SRC']
 
+require 'yaml'
+
 def get_dependency_packages(distro, script)
-	file = "#{LKP_SRC}/distro/#{distro}/#{script}"
-	return nil unless File.exist? file
+	base_file = "#{LKP_SRC}/distro/debian/#{script}"
+	return [] unless File.exist? base_file
 
+	generic_packages = []
+	File.read(base_file).each_line do |line|
+                line.sub(/#.*/, '')
+                generic_packages.concat line.split
+        end
+
+	# generic_packages based on debian
+	return generic_packages if distro == 'debian'
+
+	distro_file = "#{LKP_SRC}/distro/#{distro}-packages.yaml"
+	distro_packages = YAML.load_file(distro_file)
 	packages = []
-	File.read(file).each_line do |line|
-		line.sub(/#.*/, '')
-		packages.concat line.split
-	end
+	generic_packages.each_with_index { |pkg_name, index|
+		if distro_packages.include? pkg_name
+			packages.push distro_packages[pkg_name]
+		else
+			packages.push generic_packages[index]
+		end
+	}
 
-	return packages
+	return packages.flatten.compact
 end
-
