1bc23f8a58bfd77eee1a08781291f71c6b5c0594
Huang, Ying <ying.huang@intel.com>
2015-06-10 09:40:06 +0800

    lib/compare.rb: Refactor compare
    - Use property
    - Re-organize code in Comparer
    - Use GroupResult::MatrixExporter instead of a function with complex
      parameters
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/compare.rb
+++ lib/compare.rb
@@ -11,13 +11,6 @@ module Compare
 	REL_WIDTH = 10
 	ERR_WIDTH = 6
 
-	COMPARE_AXIS_KEYS = :compare_axis_keys
-	MRESULT_ROOTS = :_result_roots
-	ALL_STATS = :all_stats
-	SET_STAT_KEYS = :set_stat_keys
-	INCLUDE_STATS = :include_stats
-	GROUP_BY_STAT = :group_by_stat
-
 	STAT_KEY = :stat_key
 	FAILURE = :failure
 	GROUP = :group
@@ -30,77 +23,111 @@ module Compare
 	FAILS = :fails
 	CHANGES = :changes
 
-	AXES_AS_NUM = :axes_as_num
-	AXES_PREFIX = :axes_prefix
-	SORT = :sort
+	class Comparer
+		include Property
+		# following properties are parameters for compare
+		prop_reader :stat_calc_funcs
+		prop_with :mresult_roots, :compare_axis_keys,
+			  :use_all_stat_keys, :include_stat_keys,
+			  :use_stat_keys, :group_by_stat
 
-	class Groups
 		private
 
-		def initialize(params)
-			@params = params
-			group
+		def initialize(params = nil)
+			set_params params
+			@stat_calc_funcs = [Compare.method(:calc_stat_change)]
 		end
 
 		def calc_common_axes(axes)
 			as = deepcopy(axes)
-			@params[COMPARE_AXIS_KEYS].each { |ak| as.delete ak }
+			compare_axis_keys.each { |ak| as.delete ak }
 			as
-		end
-
-		def group
-			map = {}
-			@params[MRESULT_ROOTS].each { |_rt|
-				as = calc_common_axes(_rt.axes)
-				as.freeze
-				cg = map[as] ||= Group.new(@params, as)
-				cg.add_mresult_root _rt
-			}
-			@compare_groups = map.values
-			@compare_groups.reject! { |cg| cg._result_roots.size < 2 }
 		end
 
 		public
 
-		attr_reader :compare_groups
+		def set_params(params)
+			set_prop *params.flatten if params
+			self
+		end
 
-		def each_group(&b)
-			block_given? or return enum_for(__method__)
+		def compare_groups
+			map = {}
+			mresult_roots.each { |_rt|
+				as = calc_common_axes(_rt.axes)
+				as.freeze
+				cg = map[as] ||= Group.new(self, as)
+				cg.add_mresult_root _rt
+			}
+			groups = map.values
+			groups.reject! { |cg| cg.mresult_roots.size < 2 }
+			groups
+		end
 
-			@compare_groups.each &b
+		# stat calc func is a function object with signature:
+		# stat -> <ignored>
+		def add_stat_calc_funcs(*calcs)
+			@stat_calc_funcs += calcs
 		end
 
 		def each_changed_stat(&b)
 			block_given? or return enum_for(__method__)
 
-			@compare_groups.each { |g|
+			compare_groups.each { |g|
 				g.each_changed_stat &b
 			}
+		end
+
+		def do_compare
+			compare_groups.map { |g|
+				stat_enum = g.each_changed_stat
+				GroupResult.new g, stat_enum
+			}
+		end
+
+		def show_compare_result(compare_result)
+			if group_by_stat
+				Compare.show_by_stats compare_result
+			else
+				Compare.show_by_group compare_result
+			end
+		end
+
+		# - select _result_roots: set_mresult_roots
+		# - set compare axis: set_compare_axis
+		# - get compare result: do_compare
+		#   - group the result roots: compare_groups
+		#   - calc changed stats keys: Group::calc_changed_stats
+		#   - calc changes: in Group::each_changed_stat
+		# - show compare result: show_compare_result
+		def compare
+			result = do_compare
+			show_compare_result result
 		end
 	end
 
 	class Group
+		prop_reader :mresult_roots, :common_axes
+
 		private
 
-		def initialize(params, common_axes)
-			@params = params
+		def initialize(comparer, common_axes)
+			@comparer = comparer
 			@common_axes = common_axes
-			@_result_roots = []
+			@mresult_roots = []
 		end
 
 		def calc_compare_axeses
-			compare_axis_keys = @params[COMPARE_AXIS_KEYS]
-			@_result_roots.map { |_rt|
+			compare_axis_keys = @comparer.compare_axis_keys
+			@mresult_roots.map { |_rt|
 				_rt.axes.select { |k,v| compare_axis_keys.index k }
 			}
 		end
 
 		public
 
-		attr_reader :_result_roots, :common_axes
-
 		def add_mresult_root(_rt)
-			@_result_roots << _rt
+			@mresult_roots << _rt
 		end
 
 		def compare_axeses
@@ -108,12 +135,12 @@ module Compare
 		end
 
 		def matrixes
-			@matrixes ||= _result_roots.map { |_rt| _rt.matrix.freeze }
+			@matrixes ||= mresult_roots.map { |_rt| _rt.matrix.freeze }
 		end
 
 		def complete_matrixes
 			unless @complete_matrixes
-				cms = _result_roots.zip(matrixes).map { |_rt, m|
+				cms = mresult_roots.zip(matrixes).map { |_rt, m|
 					_rt.complete_matrix m
 				}
 				@complete_matrixes = cms
@@ -121,49 +148,58 @@ module Compare
 			@complete_matrixes
 		end
 
-		def calc_all_stats
+		def get_all_stat_keys
 			stat_keys = []
 			matrixes.each { |m|
 				stat_keys |= m.keys
 			}
 			stat_keys.delete 'stats_source'
-			@changed_stats = stat_keys
+			stat_keys
 		end
 
-		def set_stat_keys(stat_keys)
-			@changed_stats = stat_keys
-		end
-
-		def calc_changed_stats
-			changed_stats = []
-			mfile0 = @_result_roots[0].matrix_file
-			@_result_roots.drop(1).each { |_rt|
-				changed_stats |= get_changed_stats(_rt.matrix_file, mfile0).keys
+		def _calc_changed_stat_keys
+			changed_stat_keys = []
+			mfile0 = @mresult_roots[0].matrix_file
+			@mresult_roots.drop(1).each { |_rt|
+				changed_stat_keys |= get_changed_stats(_rt.matrix_file, mfile0).keys
 			}
-			@changed_stats = changed_stats
+			changed_stat_keys
 		end
 
-		def include_stats(stat_res)
-			astats = all_stats
-			matched = stat_res.map { |sre|
+		def get_include_stat_keys
+			stat_key_res = @comparer.include_stat_keys
+			return [] unless stat_key_res
+			astats = get_all_stat_keys
+			stat_key_res.map { |sre|
 				re = Regexp.new(sre)
-				astats.select { |stat| re.match stat }
+				astats.select { |stat_key| re.match stat_key }
 			}.flatten
-			@changed_stats |= matched
 		end
 
-		def changed_stats
-			@changed_stats || calc_all_stats
+		def calc_changed_stat_keys
+			if @comparer.use_all_stat_keys
+				stat_keys = get_all_stat_keys
+			elsif @comparer.use_stat_keys
+				stat_keys = @comparer.use_stat_keys
+			else
+				stat_keys = _calc_changed_stat_keys
+			end
+			stat_keys | get_include_stat_keys
+		end
+
+		def changed_stat_keys
+			@changed_stat_keys ||= calc_changed_stat_keys
 		end
 
 		def each_changed_stat
 			block_given? or return enum_for(__method__)
 
+			calc_funcs = @comparer.stat_calc_funcs
 			ms = matrixes
 			cms = complete_matrixes
 			runs = ms.map { |m| matrix_cols m }
 			cruns = cms.map { |m| matrix_cols m }
-			changed_stats.each { |stat_key|
+			changed_stat_keys.each { |stat_key|
 				failure = is_failure stat_key
 				tms = failure ? ms : cms
 				truns = failure ? runs : cruns
@@ -174,26 +210,21 @@ module Compare
 					VALUES => tms.map { |m| m[stat_key] },
 					RUNS => truns
 				}
+				calc_funcs.each { |calc_func|
+					calc_func.(stat)
+				}
 				yield stat
 			}
 		end
 	end
 
-	class GroupData
+	class GroupResult
 		def initialize(group, stat_enum)
 			@group = group
 			@stat_enum = stat_enum
 		end
 
-		attr_reader :group, :stat_enum
-
-		def matrix(data_key = AVGS)
-			m = {}
-			@stat_enum.each { |stat|
-				m[stat[STAT_KEY]] = stat[data_key]
-			}
-			m
-		end
+		prop_reader :group, :stat_enum
 
 		def common_axes
 			@group.common_axes
@@ -213,18 +244,41 @@ module Compare
 			@group.compare_axeses
 		end
 
-		def matrix_with_axes(data_key = nil, params = {})
-			cas = compare_axeses
+		def matrix_exporter
+			MatrixExporter.new self
+		end
+	end
+
+	class GroupResult::MatrixExporter
+		include Property
+		prop_with :data_type, :include_axes, :axes_as_num,
+			  :axis_prefix, :sort, :sort_stat_key
+		def initialize(group_result)
+			@group_result = group_result
+			@data_type = AVGS
+			@axes_as_num = true
+			@axis_prefix = ""
+			@sort = true
+		end
+
+		def matrix
+			m = {}
+			@group_result.stat_enum.each { |stat|
+				m[stat[STAT_KEY]] = stat[@data_type]
+			}
+			m
+		end
+
+		def matrix_with_axes
+			cas = @group_result.compare_axeses
 			cas_keys = cas[0].keys
-
-			data_key ||= AVGS
-			axes_as_num = params.fetch(AXES_AS_NUM, true)
-			prefix = params.fetch(AXES_PREFIX, "")
-			sort_key = params.fetch(SORT, prefix + cas_keys[0])
-
+			if @sort
+				sort_key = @sort_stat_key ||
+					   @axis_prefix + cas_keys[0]
+			end
 			axis_converter = lambda { |axis_key|
-				if axes_as_num && (axes_as_num == true ||
-						   axes_as_num.index(axis_key))
+				if @axes_as_num && (@axes_as_num == true ||
+						    @axes_as_num.index(axis_key))
 					return method(:string_to_num)
 				else
 					return ->x{x}
@@ -234,15 +288,25 @@ module Compare
 			m = {}
 			cas_keys.each { |axis_key|
 				conv = axis_converter.(axis_key)
-				m[prefix + axis_key] = cas.map { |as|
+				m[@axis_prefix + axis_key] = cas.map { |as|
 					conv.(as[axis_key])
 				}
 			}
-			m.merge! matrix(data_key)
-			m = sort_matrix(m, sort_key) if sort_key
+			m.merge!(matrix)
+			m = sort_matrix(m, sort_key) if @sort
 			m
 		end
+
+		def call
+			if @include_axes
+				matrix_with_axes
+			else
+				matrix
+			end
+		end
 	end
+
+	## Stat Calculation Functions
 
 	def self.calc_failure_fail(stat)
 		return unless stat[FAILURE]
@@ -284,50 +348,7 @@ module Compare
 		calc_perf_change stat
 	end
 
-	def self.do_compare(params)
-		groups = Groups.new(params)
-
-		groups.each_group { |g|
-			if params[ALL_STATS]
-				g.calc_all_stats
-			elsif params[SET_STAT_KEYS]
-				g.set_stat_keys params[SET_STAT_KEYS]
-			else
-				g.calc_changed_stats
-				include_stats = params[INCLUDE_STATS]
-				if include_stats
-					g.include_stats(include_stats)
-				end
-			end
-		}
-
-		data = groups.each_group.map { |g|
-			stat_enum = g.each_changed_stat.feach(method(:calc_stat_change))
-
-			GroupData.new(g, stat_enum)
-		}
-
-		data
-	end
-
-	def self.sort_stats(stat_enum)
-		stats = stat_enum.to_a
-		stat_base_map = {}
-		stats.each { |stat|
-			base = stat_key_base stat[STAT_KEY]
-			stat[STAT_BASE] = base
-			stat_base_map[base] ||= stat[FAILURE] ? -10000 : 0
-			stat_base_map[base] += 1
-		}
-		AllTests.each { |test|
-			c = stat_base_map[test]
-			if c and c > 0
-				stat_base_map[test] = 0
-			end
-		}
-		stats.sort_by! { |stat| [stat_base_map[stat[STAT_BASE]], stat[STAT_KEY]] }
-		stats.each
-	end
+	## Compare result renderer
 
 	def self.show_failure_change(stat)
 		return unless stat[FAILURE]
@@ -418,7 +439,7 @@ module Compare
 	end
 
 	def self.show_group(group, stat_enum)
-		nr_header = group._result_roots.size - 1
+		nr_header = group.mresult_roots.size - 1
 		failure, perf = stat_enum.partition { |stat| stat[FAILURE] }
 		show_group_header group
 
@@ -441,10 +462,29 @@ module Compare
 		puts
 	end
 
-	def self.show_by_group(compare_data)
-		compare_data.each { |gd|
+	def self.show_by_group(compare_result)
+		compare_result.each { |gd|
 			show_group gd.group, gd.stat_enum
 		}
+	end
+
+	def self.sort_stats(stat_enum)
+		stats = stat_enum.to_a
+		stat_base_map = {}
+		stats.each { |stat|
+			base = stat_key_base stat[STAT_KEY]
+			stat[STAT_BASE] = base
+			stat_base_map[base] ||= stat[FAILURE] ? -10000 : 0
+			stat_base_map[base] += 1
+		}
+		AllTests.each { |test|
+			c = stat_base_map[test]
+			if c and c > 0
+				stat_base_map[test] = 0
+			end
+		}
+		stats.sort_by! { |stat| [stat_base_map[stat[STAT_BASE]], stat[STAT_KEY]] }
+		stats.each
 	end
 
 	def self.group_by_stat(stat_enum)
@@ -457,8 +497,8 @@ module Compare
 		stat_map
 	end
 
-	def self.show_by_stats(compare_data)
-		stat_enums = compare_data.map { |d|
+	def self.show_by_stats(compare_result)
+		stat_enums = compare_result.map { |d|
 			sort_stats d.stat_enum
 		}
 		stat_enum = EnumeratorCollection.new(*stat_enums)
@@ -476,61 +516,49 @@ module Compare
 		}
 	end
 
-	def self.show_compare_data(compare_data, params)
-		if params[GROUP_BY_STAT]
-			show_by_stats(compare_data)
-		else
-			show_by_group(compare_data)
-		end
-	end
+	## Helper functions
 
-	def self.compare(params)
-		data = do_compare(params)
-		show_compare_data(data, params)
-	end
-
-	def self.compare_commits(commits, params_in = {})
-		# calc compare groups
-		# calc change stats
-		# calc stat change
-		# group result
-		# output
+	def self.commits_comparer(commits, params = nil)
 		_result_roots = commits.map { |c| MResultRootCollection.new('commit' => c).to_a }.flatten
 		compare_axis_keys = ['commit']
-		params = {
-			MRESULT_ROOTS => _result_roots,
-			COMPARE_AXIS_KEYS => compare_axis_keys
-		}.merge(params_in)
-		compare(params)
+		comparer = Comparer.new
+		comparer.set_mresult_roots _result_roots
+		comparer.set_compare_axis_keys compare_axis_keys
+		comparer.set_params params
 	end
+
+	def self.compare_commits(commits, params = nil)
+		comparer = commits_comparer commits, params
+		comparer.compare
+	end
+
+	## Test functions
 
 	def self.test_compare_commits
 		commits = ['f5c0a122800c301eecef93275b0c5d58bb4c15d9', '3a8b36f378060d20062a0918e99fae39ff077bf0']
-		compare_axis_keys = ['commit', 'rwmode']
+		comparer = commits_comparer commits
+		comparer.set_compare_axis_keys(['commit', 'rwmode']).
+			set_use_all_stats(false).
+			set_group_by_stat(false)
+
 		pager {
-			#compare_commits(commits,
-			#		 ALL_STATS => true,
-			#		 GROUP_BY_STAT => true)
-			compare_commits(commits, ALL_STATS => false,
-					GROUP_BY_STAT => false,
-					COMPARE_AXIS_KEYS => compare_axis_keys)
+			comparer.compare
 		}
 	end
 
 	def self.test_incomplete_run
-		_rt_ = '/result/lkp-sb02/fileio/performance-600s-100%-1HDD-ext4-64G-1024f-seqrd-sync/debian-x86_64-2015-02-07.cgz/x86_64-rhel/'
-		rts_ = ['9eccca0843205f87c00404b663188b88eb248051', '06e5801b8cb3fc057d88cb4dc03c0b64b2744cda'].
-			     map { |c| MResultRoot.new(_rt_ + c) }
-		rts_.each { |_rt|
+		_rt = '/result/lkp-sb02/fileio/performance-600s-100%-1HDD-ext4-64G-1024f-seqrd-sync/debian-x86_64-2015-02-07.cgz/x86_64-rhel/'
+		_rts = ['9eccca0843205f87c00404b663188b88eb248051', '06e5801b8cb3fc057d88cb4dc03c0b64b2744cda'].
+			     map { |c| MResultRoot.new(_rt + c) }
+		_rts.each { |_rt|
 			puts "#{_rt.runs}"
 		}
-		params = {
-			MRESULT_ROOTS => rts_,
-			COMPARE_AXIS_KEYS => ['commit'],
-			ALL_STATS => false,
-		}
+		comparer = Comparer.new
+		comparer.set_mresult_roots(_rts).
+			set_compare_axis_keys(['commit']).
+			set_use_all_stats(false)
 		page {
-			compare(params)
+			comparer.compare
 		}
 	end
 
@@ -541,12 +569,11 @@ module Compare
 			'test' => 'ram_copy',
 		).to_a
 		compare_axis_keys = ['load']
-		params = {
-			MRESULT_ROOTS => _result_roots,
-			COMPARE_AXIS_KEYS => compare_axis_keys,
-		}
+		comparer = Comparer.new
+		comparer.set_mresult_roots(_result_roots).
+			set_compare_axis_keys(compare_axis_keys)
 		pager {
-			compare(params)
+			comparer.compare
 		}
 	end
 end
