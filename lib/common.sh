d137fe09c25a29139b0c28232868850ac5252fb4
Huang, Ying <ying.huang@intel.com>
2015-06-11 09:31:49 +0800

    Enhance lkp rm
    - support multiple parameters (for bash glob in command line)
    - convert relative path to absolute path
    - ask for confirm and run the command directly
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

---
+++ lib/common.sh
@@ -0,0 +1,16 @@
+# common utility functions
+
+is_abs_path()
+{
+	[[ "${1:0:1}" = "/" ]]
+}
+
+abs_path()
+{
+	local path="$1"
+	if is_abs_path $path; then
+		echo $path
+	else
+		echo $PWD/$path
+	fi
+}
