d385153d5c380b6d2f96967208fc628e25cfcd1c
Philip Li <philip.li@intel.com>
2015-06-19 12:26:52 +0800

    update Base.command_lines to handle invalid byte sequence in US-ASCII
    Signed-off-by: Philip Li <philip.li@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/lkp_git.rb
+++ lib/lkp_git.rb
@@ -116,20 +116,23 @@ module Git
 		public
 		def command_lines(cmd, opts = [], chdir = true, redirect = '')
 			command_lines = command(cmd, opts, chdir)
+
 			begin
 				command_lines.split("\n")
 			rescue Exception => e
-				STDERR.puts "GIT error: #{cmd} #{opts}: #{e.message}"
-
-				begin
+				# to deal with "GIT error: cat-file ["commit", "9f86262dcc573ca195488de9ec6e4d6d74288ad3"]: invalid byte sequence in US-ASCII"
+				# - one possibility is the encoding of string is wrongly set (due to unknown reason), e.g. UTF-8 string's encoding is set as US-ASCII
+				#   thus to force_encoding to utf8_string and compare to error check of utf8_string, if same, will consider as wrong encoding info
+				utf8_command_lines = command_lines.clone.force_encoding("UTF-8")
+				if utf8_command_lines == utf8_command_lines.encode("UTF-8", "UTF-8", undef: :replace)
+					utf8_command_lines.split("\n")
+				else
+					STDERR.puts "GIT error: #{cmd} #{opts}: #{e.message}"
 					STDERR.puts "GIT env: LANG = #{ENV['LANG']}, LANGUAGE = #{ENV['LANGUAGE']}, LC_ALL = #{ENV['LC_ALL']}, "\
-					            "encoding = #{command_lines.encoding}, "\
-					            "scrub = #{command_lines == command_lines.scrub}, "\
-					            "utf8 = #{command_lines.force_encoding("UTF-8") == command_lines.encode("UTF-8", "UTF-8", undef: :replace)}"
-				rescue
-				end
+					            "encoding = #{command_lines.encoding}"
 
-				command_lines.encode("UTF-8", "binary", invalid: :replace, undef: :replace).split("\n")
+					command_lines.encode("UTF-8", "binary", invalid: :replace, undef: :replace).split("\n")
+				end
 			end
 		end
 	end
