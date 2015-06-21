b7a4d057c92d4c25215da61a80f10331eb374ed2
Fengguang Wu <fengguang.wu@intel.com>
2015-06-21 15:45:26 +0800

    build: introduce make_config()
    scripts/kconfig/conf --silentoldconfig Kconfig
    .config:5931:warning: symbol value 'm' invalid for USB_ISP1760_HCD
    *
    * Restart config...
    *
    *
    * RCU Subsystem
    *
    RCU Implementation
    > 1. Tree-based hierarchical RCU (TREE_RCU)
    choice[1]: 1
    Task_based RCU implementation using voluntary context switch (TASKS_RCU) [Y/n/?] y
    Consider userspace as in RCU extended quiescent state (RCU_USER_QS) [Y/n/?] y
    Force context tracking (CONTEXT_TRACKING_FORCE) [Y/n/?] y
    Tree-based hierarchical RCU fanout value (RCU_FANOUT) [64] 64
    Tree-based hierarchical RCU leaf-level fanout value (RCU_FANOUT_LEAF) [16] 16
    Disable tree-based hierarchical RCU auto-balancing (RCU_FANOUT_EXACT) [Y/n/?] y
    Accelerate last non-dyntick-idle CPU's grace periods (RCU_FAST_NO_HZ) [Y/n/?] y
    Real-time priority to use for RCU worker threads (RCU_KTHREAD_PRIO) [0] (NEW) aborted!
    
    Console input/output is redirected. Run 'make oldconfig' to update configuration.
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/build.sh
+++ lib/build.sh
@@ -7,6 +7,14 @@ make()
 	/usr/bin/make -j $nr_cpu "$@"
 }
 
+make_config()
+{
+        (
+        set -o pipefail
+        { yes ''; true; } | make "$@" | head --bytes=1000K
+        )
+}
+
 build_complete()
 {
 	echo "$(date +'%F %T') make finished"
