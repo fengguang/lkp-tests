193e139c58d03e65a1891dce6e0b08ebb095cc4b
Zexi Li <zexi.li@intel.com>
2015-07-06 14:04:26 +0800

    lib/install.sh: add sync_distro_sources function
    Should do source mirror sync action first when we want
    to install packages in testbox.
    
    And distinguish fedora 22 'dnf' package manager.
    
    Signed-off-by: Zexi Li <zexi.li@intel.com>
    Signed-off-by: Huang, Ying <ying.huang@intel.com>

--- lib/install.sh
+++ lib/install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+. $LKP_SRC/lib/detect-system.sh
+
+sync_distro_sources()
+{
+	detect_system
+	distro=$_system_name_lowercase
+	distro_version=$_system_version
+
+	case $distro in
+	debian|ubuntu) apt-get update ;;
+	fedora)
+		if [ $distro_version -ge 22 ]; then
+			dnf update
+		else
+			yum update
+		fi ;;
+	archlinux) yaourt -Sy ;;
+	*) echo "Not support $distro to do update" ;;
+	esac
+}
+
 adapt_packages()
 {
 	local distro_file="$LKP_SRC/distro/adaptation/$distro"
