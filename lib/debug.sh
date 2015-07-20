171f7bf75be92d7976e3eb6846418cd0440cfd18
Huang, Ying <ying.huang@intel.com>
2015-07-20 14:53:00 +0800

    lib/debug.sh: Enhance debug.sh
    Output to stderr in die.  Add dump_call_stack for bash
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>

--- lib/debug.sh
+++ lib/debug.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-die()
+dump_call_stack()
 {
-	echo "$@"
-	exit 1
+	[ -n "$BASHPID" ] || return
+
+	local stack_depth=${#FUNCNAME[@]}
+	local i
+	for i in $(seq 0 $stack_depth); do
+		[[ $i -eq $stack_depth ]] && break
+		echo "  ${BASH_SOURCE[i+1]}:${BASH_LINENO[i]}: ${FUNCNAME[i+1]}" >&2
+	done
 }
 
+die()
+{
+	echo "$@" 1>&2
+	dump_call_stack
+	exit 1
+}
