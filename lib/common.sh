3f52d9dd8f5fe8e06909b6062bd891364a9aaac6
Huang, Ying <ying.huang@intel.com>
2015-07-20 14:52:58 +0800

    Move query_var_from_yaml to lib/common.sh
    To use it in other places too.
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>

--- lib/common.sh
+++ lib/common.sh
@@ -1,5 +1,7 @@
 # common utility functions
 
+. $LKP_SRC/lib/debug.sh
+
 is_abs_path()
 {
 	[[ "${1:0:1}" = "/" ]]
@@ -13,4 +15,13 @@ abs_path()
 	else
 		echo $PWD/$path
 	fi
+}
+
+query_var_from_yaml()
+{
+	local key=$1
+	local yaml_file=${2:--}
+	[ $# -ge 1 ] || die "Invalid parmeters: $*"
+
+	sed -ne "1,\$s/^$key[[:space:]]*:[[:space:]]*\\(.*\\)[[:space:]]*\$/\\1/p" "$yaml_file"
 }
