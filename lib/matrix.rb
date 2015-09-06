fengguang/lkp-tests

commit 77bdc371ff81b85ad7e2591d3a7839ff1203e301

lib/matrix.rb: Fix duplicated cols in unite stats
Signed-off-by: Huang, Ying <ying.huang@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/matrix.rb
@@ -3,6 +3,7 @@
 LKP_SRC ||= ENV['LKP_SRC']
 
 MAX_MATRIX_COLS = 100
+STATS_SOURCE_KEY = 'stats_source'
 
 require 'set'
 
@@ -146,12 +147,21 @@ def shrink_matrix(matrix, max_cols)
 	end
 end
 
+def matrix_delete_col(matrix, col)
+	matrix.each { |k, v|
+		v.delete_at col
+	}
+end
+
 def unite_to(stats, matrix_root, max_cols = nil)
 	matrix_file = matrix_root + '/matrix.json'
 
 	matrix = load_matrix_file(matrix_root + '/matrix.json')
 	matrix = load_matrix_file(matrix_root + '/matrix.yaml') unless matrix
 	matrix = {} unless matrix
+
+	dup_col = matrix[STATS_SOURCE_KEY].index stats[STATS_SOURCE_KEY]
+	matrix_delete_col(matrix, dup_col) if dup_col
 
 	matrix = add_stats_to_matrix(stats, matrix)
 	shrink_matrix(matrix, max_cols) if max_cols

commit 05c3273edf914b129a72a349c4dd791e24f81cc1

lib/matrix.rb: Fix unite stats
Fix a regression introduced by my previous commit:

77bdc371ff81b85ad7e2591d3a7839ff1203e301
lib/matrix.rb: Fix duplicated cols in unite stats

The newly created matrix is not considered.

Signed-off-by: Huang, Ying <ying.huang@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/matrix.rb
@@ -158,10 +158,13 @@ def unite_to(stats, matrix_root, max_cols = nil)
 
 	matrix = load_matrix_file(matrix_root + '/matrix.json')
 	matrix = load_matrix_file(matrix_root + '/matrix.yaml') unless matrix
-	matrix = {} unless matrix
 
-	dup_col = matrix[STATS_SOURCE_KEY].index stats[STATS_SOURCE_KEY]
-	matrix_delete_col(matrix, dup_col) if dup_col
+	if matrix
+		dup_col = matrix[STATS_SOURCE_KEY].index stats[STATS_SOURCE_KEY]
+		matrix_delete_col(matrix, dup_col) if dup_col
+	else
+		matrix = {}
+	end
 
 	matrix = add_stats_to_matrix(stats, matrix)
 	shrink_matrix(matrix, max_cols) if max_cols