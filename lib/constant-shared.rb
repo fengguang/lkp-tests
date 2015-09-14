6cf4f864322eb58c3232f7699912dcdabde5cb8d
Huang, Ying <ying.huang@intel.com>
2015-09-14 08:55:37 +0800

    Add lib/constant-shared.rb and use it in lib/constant.rb
    lib/constant-shared.rb will have syntax compatible with Ruby and
    Shell, so that is can be used by Ruby and Shell to avoid duplicated
    defined constant between them.
    
    Signed-off-by: Huang, Ying <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/constant-shared.rb
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#########################################################
+# CAUTION: For constants shared between shell and ruby
+#          Be careful to write code compatible with shell
+#          and ruby!!!
+#########################################################
+
+##
+## Errno
+##
+
+ERR_SUCCESS=0
+ERR_ASYNC=12
+ERR_INVALID_COMMIT=13
+ERR_INVALID=14		# Invalid argument
+ERR_UNSUPPORTED=15	# Unsupported feature
+ERR_UNKNOWN=127		# Unknown error
+
+##
+## Other
+##
+
+KERNEL_ROOT='/pkg/linux'
+
+BOOT_TEST_CASE='boot'
+DMESG_BOOT_FAILURES_STAT_KEY='dmesg.boot_failures'
--- lib/constant.rb
+++ lib/constant.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 LKP_SRC ||= ENV['LKP_SRC']
 
+require "#{LKP_SRC}/lib/shared-constant.rb"
+
 DEVEL_HOURLY_KCONFIGS = ['x86_64-rhel']
-
-KERNEL_ROOT = '/pkg/linux'
-
-BOOT_TEST_CASE = 'boot'
-DMESG_BOOT_FAILURES_STAT_KEY = 'dmesg.boot_failures'
