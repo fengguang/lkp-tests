e0a2192b580607ee6f0491da489efb5746541625
Philip Li <philip.li@intel.com>
2015-11-03 13:10:56 +0800

    add dump_exception API to record detail information including local vars
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/error.rb
+++ lib/error.rb
@@ -4,3 +4,8 @@ LKP_SRC ||= ENV["LKP_SRC"]
 def puts_err(*messages)
 	$stderr.puts "#{File.basename $PROGRAM_NAME}: #{messages.join ' '}"
 end
+
+def dump_exception(e, binding = nil)
+	$stderr.puts e.message
+	$stderr.puts e.backtrace.join "\n"
+end
