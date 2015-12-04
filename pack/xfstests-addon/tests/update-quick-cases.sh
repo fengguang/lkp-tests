9cd9abb8ce917b741dc30fc68ff9eba2259ea29b
Fengguang Wu <fengguang.wu@intel.com>
2015-12-04 13:48:06 +0800

    update-quick-cases.sh: add maintenance comments
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- pack/xfstests-addon/tests/update-quick-cases.sh
+++ pack/xfstests-addon/tests/update-quick-cases.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# after upgrading xfstests,
+# - run this script to add new cases to $1-quick
+# - queue jobs
+# - adjust the cases grouping based on the test results' run time (rebalance)
+#   or failure status (identify broken cases, either fix it up or move to the
+#   $1-broken group)
+# - each finalized group should ideally have 10-30m total runtime
+
 [[ $1 ]] || exit
 
 mv $1-quick .$1-quick
