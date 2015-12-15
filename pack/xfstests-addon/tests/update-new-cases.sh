2d6c07b76be0e8083d096b6227a576365c93ec8e
Yilong Ren <yilongx.ren@intel.com>
2015-12-15 09:59:24 +0800

    add one new sub-job to xfstests
    We just want to test the new add subcase after upgrade xfstests, so add one new sub-job.
    And due to the file $fs-quick have been used, we use $fs-new to instead of it.
    
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- pack/xfstests-addon/tests/update-new-cases.sh
+++ pack/xfstests-addon/tests/update-new-cases.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # after upgrading xfstests,
-# - run this script to add new cases to $1-quick
+# - run this script to add new cases to $1-new
 # - queue jobs
 # - adjust the cases grouping based on the test results' run time (rebalance)
 #   or failure status (identify broken cases, either fix it up or move to the
 #   $1-broken group)
 # - each finalized group should ideally have 10-30m total runtime
 
-[[ $1 ]] || exit
+[[ $1 ]] ||
+{
+	echo "Usage: $0 < ext4 | xfs | btrfs | generic | ... >"
+	exit
+}
 
-mv $1-quick .$1-quick
-ls $1/??? | grep -v -f <(cat $1-*) | cut -f2 -d/ > $1-quick
+[ -f $1-new ] && mv $1-new .$1-new
+ls $1/??? | grep -v -f <(cat $1-*) | cut -f2 -d/ > $1-new
