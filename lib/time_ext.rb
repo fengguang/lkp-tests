ebb7b7057a2a9468b0a30bd39676eec2256f85da
Philip Li <philip.li@intel.com>
2016-02-16 12:26:58 +0800

    add Time.days_of_month api
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/time_ext.rb
+++ lib/time_ext.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'date'
+
 class Time
 	def next_month(i = 1)
 		year, month = self.year, self.month + i
@@ -16,5 +18,11 @@ class Time
 	def next_day
 		date = self + 24 * 3600
 		Time.new(date.year, date.month, date.day)
+	end
+
+	class << self
+		def days_of_month(year, month)
+			Date.new(year, month, -1).day
+		end
 	end
 end
