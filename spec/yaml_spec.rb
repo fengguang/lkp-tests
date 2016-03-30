c00fb32de83f1e0e91c5dc07427bc3d52a7c6b38
Wenzhong Sun <wenzhong.sun@intel.com>
2016-03-30 16:08:40 +0800

    yaml_spec.rb: add test spec for load_yaml_with_flock and save_yaml_with_flock methods
    Signed-off-by: Wenzhong Sun <wenzhong.sun@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'timeout'
 
 $LOAD_PATH.concat($LOAD_PATH.shift(3))
 
@@ -18,6 +19,59 @@ describe WTMP do
 			result = described_class.load_tail("time: 2015-08-06 20:25:44 +0800state: running")
 			expect(result).to be nil
 		end
+	end
+end
+
+describe "load_yaml_with_flock" do
+	TEST_YAML_FILE = "/tmp/test.yaml".freeze
+
+	before(:example) do
+		File.open(TEST_YAML_FILE, 'w') { |f|
+			f.write("key1: value1\nkey2: value2\n")
+		}
+	end
+
+	it "should return correct value" do
+		yaml = load_yaml_with_flock TEST_YAML_FILE
+		expect(yaml['key2']).to eq 'value2'
+	end
+
+	it "should return nil due to flock by other process" do
+		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR|File::CREAT, 0664)
+		f.flock(File::LOCK_EX)
+
+		yaml = Timeout::timeout(0.001) { load_yaml_with_flock TEST_YAML_FILE } rescue nil
+		f.close
+		expect(yaml).to be nil
+	end
+
+	after(:example) do
+		FileUtils.rm TEST_YAML_FILE
+		FileUtils.rm TEST_YAML_FILE + '.lock'
+	end
+end
+
+describe "save_yaml_with_flock" do
+	TEST_YAML_OBJ = {"key1"=>"value1","key2"=>"value2"}
+
+	it "should save yaml file correctly" do
+		save_yaml_with_flock TEST_YAML_OBJ, TEST_YAML_FILE
+		yaml = load_yaml TEST_YAML_FILE
+		expect(yaml['key2']).to eq 'value2'
+	end
+
+	it "should return false due to flock by other process" do
+		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR|File::CREAT, 0664)
+		f.flock(File::LOCK_EX)
+
+		Timeout::timeout(0.001) { save_yaml_with_flock TEST_YAML_OBJ, TEST_YAML_FILE } rescue nil
+		f.close
+		expect(File.exist? TEST_YAML_FILE).to be false
+	end
+
+	after(:example) do
+		FileUtils.rm_rf TEST_YAML_FILE
+		FileUtils.rm_rf TEST_YAML_FILE + '.lock'
 	end
 end
 
