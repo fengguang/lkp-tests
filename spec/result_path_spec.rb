880894c70cdce40995c1392ba72136e8b47a09e3
Philip Li <philip.li@intel.com>
2016-04-01 12:29:47 +0800

    spec: fix result_path test failure
    rli9@~/git/lkp-tests$rake spec spec=result_path
    PWD = /home/rli9/git/lkp-tests
    ENV['LKP_SRC'] = /home/rli9/git/lkp-tests
    /usr/bin/ruby2.1 -I/home/rli9/.rvm/gems/ruby-2.2.1/gems/rspec-support-3.3.0/lib:/home/rli9/.rvm/gems/ruby-2.2.1/gems/rspec-core-3.3.0/lib
    /home/rli9/.rvm/gems/ruby-2.2.1/gems/rspec-core-3.3.0/exe/rspec --pattern spec/\*\*\{,/\*/\*\*\}/result_path_spec.rb
    .........
    
    Finished in 0.00306 seconds (files took 0.10049 seconds to load)
    9 examples, 0 failures
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/result_path_spec.rb
+++ spec/result_path_spec.rb
@@ -16,13 +16,15 @@ describe ResultPath do
 			expect(result_path['kconfig']).to eq 'x86_64-rhel'
 			expect(result_path['compiler']).to eq 'gcc-4.9'
 			expect(result_path['commit']).to eq '0f57d86787d8b1076ea8f9cbdddda2a46d534a27'
-			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc/0').to be true
-			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc/').to be true
-			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be true
+
+			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/0').to be true
+			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/').to be true
+			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be true
 			expect(result_path['testcase']).to eq 'build-dpdk'
 			expect(result_path['dpdk_config']).to eq 'x86_64-native-linuxapp-gcc'
-			expect(result_path['dpdk_compiler']).to eq 'gcc'
+			expect(result_path['dpdk_compiler']).to eq 'gcc-4.9'
 			expect(result_path['dpdk_commit']).to eq '60c5c5692107abf4157d48493aa2dec01f6b97cc'
+
 			expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4/').to be true
 			expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4').to be true
 			expect(result_path['testcase']).to eq 'hwinfo'
@@ -48,59 +50,67 @@ describe ResultPath do
 			expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel').to be false
 		end
 
-		describe "project_info" do
-			it "should handle default path" do
-				result_path = ResultPath.new
+		it "should fail when result root is invalid" do
+			result_path = ResultPath.new
 
-				result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2'
+			expect(result_path.parse_result_root '/result/build-dpdk/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/x86_64-native-linuxapp-gcc/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be false
+			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be false
+			expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc/0').to be false
+		end
+	end
 
-				result_path.each_commit do |project, commit_axis|
-					expect(project).to eq 'linux'
-					expect(commit_axis).to eq 'commit'
-				end
-			end
+	valid_dpdk_result_root = '/result/build-dpdk/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/0'
 
-			it "should handle dpdk path" do
-				result_path = ResultPath.new
+	describe "each_commit" do
+		it "should handle default path" do
+			result_path = ResultPath.new
 
-				result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc'
+			result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2'
 
-				result_path.each_commit do |project, commit_axis|
-					expect(project).to eq 'dpdk'
-					expect(commit_axis).to eq 'dpdk_commit'
-				end
+			result_path.each_commit do |project, commit_axis|
+				expect(project).to eq 'linux'
+				expect(commit_axis).to eq 'commit'
 			end
 		end
 
-		describe "each_commit" do
-			it "should return enumerator" do
-				result_path = ResultPath.new
-				result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc'
+		it "should handle dpdk path" do
+			result_path = ResultPath.new
 
-				expect(result_path.each_commit.any? {|project| project == 'dpdk'}).to be true
-			end
+			result_path.parse_result_root valid_dpdk_result_root
+
+			project_mappings = Hash[result_path.each_commit.map {|project, commit_axis| [project, commit_axis]}]
+			expect(project_mappings.size).to eq 2
+			expect(project_mappings['linux']).to eq 'commit'
+			expect(project_mappings['dpdk']).to eq 'dpdk_commit'
 		end
 
-		describe "commit_axis" do
-			it "should handle dpdk path" do
-				result_path = ResultPath.new
+		it "should return enumerator" do
+			result_path = ResultPath.new
+			result_path.parse_result_root valid_dpdk_result_root
 
-				result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc'
-				expect(result_path.commit_axis).to eq 'dpdk_commit'
-			end
+			expect(result_path.each_commit.any? {|project| project == 'dpdk'}).to be true
+		end
+	end
 
-			it "should handle linux path" do
-				result_path = ResultPath.new
+	describe "commit_axis" do
+		it "should handle dpdk path" do
+			result_path = ResultPath.new
 
-				result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2'
-				expect(result_path.commit_axis).to eq 'commit'
-			end
+			result_path.parse_result_root valid_dpdk_result_root
+			expect(result_path.commit_axis).to eq 'commit'
 		end
 
-		describe "maxis_keys" do
-			it "should handle dpdk path" do
-				expect(ResultPath.maxis_keys('build-dpdk').size).to eq 3
-			end
+		it "should handle linux path" do
+			result_path = ResultPath.new
+
+			result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2'
+			expect(result_path.commit_axis).to eq 'commit'
+		end
+	end
+
+	describe "maxis_keys" do
+		it "should handle dpdk path" do
+			expect(ResultPath.maxis_keys('build-dpdk').size).to eq 4
 		end
 	end
 end
