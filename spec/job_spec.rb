b3724a0512e7bdb1fd1cb277a271be8f8368d810
Yilong Ren <yilongx.ren@intel.com>
2016-04-08 11:54:02 +0800

    spec/job_sepc.rb: add qemu project
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -13,6 +13,16 @@ describe Job do
 			end
 		end
 
+		it "should recognize qemu project" do
+			job = Job.new
+			job.load('/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/2/job.yaml')
+
+			expect(job.project).to eq 'qemu'
+			job.each_jobs do |j|
+				expect(j.project).to eq 'qemu'
+			end
+		end
+
 		it "should recognize linux project" do
 			job = Job.new
 			job.load('/result/aim7/600-mem_rtns_1/lkp-a03/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/171912fdeb2159d6cbf60bda4d0438da9fb1c731/0/job.yaml')
