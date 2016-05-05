588522c8030dfd3418ca642520ba21b157e38afb
Philip Li <philip.li@intel.com>
2016-05-05 14:56:01 +0800

    repo_spec: load DEFAULTS for repo spec
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/repo_spec.rb
+++ lib/repo_spec.rb
@@ -10,15 +10,34 @@ class RepoSpec
 	ROOT_DIR = LKP_SRC + '/repo'
 
 	def initialize(name)
-		file = Dir[File.join(ROOT_DIR, '*', name)].first
-		assert file, "can't find #{name} spec under #{ROOT_DIR}"
+		spec_path = Dir[File.join(ROOT_DIR, '*', name)].first
+		assert spec_path, "can't find #{name} spec under #{ROOT_DIR}"
 
-		@spec = YAML.load_file(file)
-		assert @spec, "invalid spec #{file}"
+		@spec = YAML.load_file(spec_path)
+		assert @spec, "invalid spec #{spec_path}"
+
+		defaults_spec_path = File.join(File.dirname(spec_path), 'DEFAULTS')
+		if File.exist? defaults_spec_path
+			defaults_spec = YAML.load_file(defaults_spec_path)
+			assert defaults_spec, "invalid defaults spec #{defaults_spec_path}"
+
+			@spec = defaults_spec.merge(@spec)
+		end
 	end
 
 	def [](key)
 		@spec[key]
 	end
 
+	class << self
+		def url_to_remote(url)
+			unless @url_remotes
+				names = Dir[File.join(ROOT_DIR, '*', '*')].map {|file| File.basename file}.reject {|name| name == 'DEFAULTS'}
+				@url_remotes = Hash[names.map {|name| [self.new(name)['url'], name]}]
+			end
+
+			@url_remotes[url]
+		end
+
+	end
 end
