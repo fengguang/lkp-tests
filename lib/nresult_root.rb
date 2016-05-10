63a729840548d75933ad854a985d5aabe9911f52
Huang Ying <ying.huang@intel.com>
2016-05-10 16:11:08 +0800

    Add nfs-test job
    Used to debug NFS hang issue.
    
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nresult_root.rb
+++ lib/nresult_root.rb
@@ -282,7 +282,7 @@ class MResultRootTableSet
 		['boot', 'audio', 'cpu-hotplug', 'ext4-frags', 'ftq', 'ftrace_onoff', 'fwq',
 		 'galileo', 'irda-kernel', 'kernel_selftests', 'kvm-unit-tests','kvm-unit-tests-qemu',
 		 'locktorture', 'mce-test',  'otc_kernel_qa-ts_ltp_ddt', 'piglit', 'pm-qa',
-		 'qemu', 'rcutorture', 'suspend', 'trinity', 'ndctl']
+		 'qemu', 'rcutorture', 'suspend', 'trinity', 'ndctl', 'nfs-test']
 	OTHER_TESTCASES =
 		['0day-boot-tests', '0day-kbuild-tests', 'android-kpi', 'build-dpdk',
 		 'build-qemu','build-android', 'convert-lkpdoc-to-html', 'convert-lkpdoc-to-html-css',
