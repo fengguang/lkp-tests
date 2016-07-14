2fbafb16d05dce9ccc3c1a8f326b094de491abb1
Philip Li <philip.li@intel.com>
2016-07-14 15:54:09 +0800

    spec: move wtmp related tests to wtmp_spec
    Acked-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/wtmp_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'timeout'
+require "#{LKP_SRC}/lib/yaml"
+
+describe WTMP do
+	describe 'load' do
+		it 'handles control character' do
+			result = described_class.load("time: 2015-08-06 20:25:44 +0800\nstate: running\n\ntime: 2015-08-06 20:29:21 +0800\nstate: finished\n\n\u0000\u0000\u0000\u0000")
+
+			expect(result['state']).to eq 'finished'
+		end
+	end
+
+	describe 'load_tail' do
+		context 'error' do
+			it 'returns nil' do
+				actual = described_class.load_tail("time: 2015-08-06 20:25:44 +0800state: running")
+
+				expect(actual).to be nil
+			end
+		end
+	end
+end
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -2,23 +2,6 @@ require 'spec_helper'
 require 'timeout'
 require "#{LKP_SRC}/lib/yaml"
 
-describe WTMP do
-	describe "load" do
-		it "should handle control character" do
-			result = described_class.load("time: 2015-08-06 20:25:44 +0800\nstate: running\n\ntime: 2015-08-06 20:29:21 +0800\nstate: finished\n\n\u0000\u0000\u0000\u0000")
-
-			expect(result['state']).to eq 'finished'
-		end
-	end
-
-	describe "load_tail" do
-		it "should return nil if error" do
-			result = described_class.load_tail("time: 2015-08-06 20:25:44 +0800state: running")
-			expect(result).to be nil
-		end
-	end
-end
-
 describe "load_yaml_with_flock" do
 	TEST_YAML_FILE = "/tmp/test.yaml".freeze
 
