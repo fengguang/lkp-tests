9e32820fe2de025f3a5056dc5674e572800449db
Fengguang Wu <fengguang.wu@intel.com>
2016-07-18 07:26:47 +0800

    repo_spec: reuse load_remotes()
    This also avoids repeatedly loading repo files.
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/repo_spec.rb
+++ lib/repo_spec.rb
@@ -5,26 +5,18 @@ LKP_CORE_SRC ||= ENV['LKP_CORE_SRC'] || LKP_SRC
 
 require 'yaml'
 require "#{LKP_SRC}/lib/assert"
+require "#{LKP_SRC}/lib/lkp_git"
 
 class RepoSpec
 	ROOT_DIR = LKP_SRC + '/repo'
 
 	def initialize(name)
+		$remotes ||= load_remotes
+
 		@name = name
+		@spec = $remotes[name]
 
-		spec_path = Dir[File.join(ROOT_DIR, '*', @name)].first
-		assert spec_path, "can't find #{@name} spec under #{ROOT_DIR}"
-
-		@spec = YAML.load_file(spec_path)
-		assert @spec, "invalid spec #{spec_path}"
-
-		defaults_spec_path = File.join(File.dirname(spec_path), 'DEFAULTS')
-		if File.exist? defaults_spec_path
-			defaults_spec = YAML.load_file(defaults_spec_path)
-			assert defaults_spec, "invalid defaults spec #{defaults_spec_path}"
-
-			@spec = defaults_spec.merge(@spec)
-		end
+		assert @spec, "can't find repo spec #{@name}"
 	end
 
 	def [](key)
