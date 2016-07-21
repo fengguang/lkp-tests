5c811e0020f8474f21eaad9a1b6e3bd19b7377e9
Fengguang Wu <fengguang.wu@intel.com>
2016-07-21 13:31:07 +0800

    run-in-kvm: make it work
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- rootfs/kexec/run-in-kvm.sh
+++ rootfs/kexec/run-in-kvm.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
-kvm_params=(
--m 2G
--kernel vmlinuz
--initrd rootfs.cgz
--append hostname=vm-kexec
--net nic,vlan=0,macaddr=00:00:00:00:00:ff,model=e1000
--net user,vlan=0,hostfwd=tcp::2222-:22
+kernel=vmlinuz
+initrd=rootfs.cgz
+hostname=vm-kexec
+
+kvm=(
+	qemu-system-x86_64 -cpu kvm64 -enable-kvm
+
+	-kernel $kernel
+	-initrd $initrd
+
+	-smp 2
+	-m 2G
+
+	-net nic,vlan=0,macaddr=00:00:00:00:00:ff,model=e1000
+	-net user,vlan=0,hostfwd=tcp::2222-:22
+
+	-boot order=nc
+	-no-reboot
+	-watchdog i6300esb
+
+	-display none
+	-serial stdio
+	-monitor null
 )
 
-qemu-system-x86_64 --enable-kvm "${kvm_params[@]}"
+append=(
+	debug
+	sched_debug
+	apic=debug
+	ignore_loglevel
+	earlyprintk=ttyS0,115200
+	sysrq_always_enabled
+	panic=10
+	hung_task_panic=1
+	softlockup_panic=1
+	nmi_watchdog=panic
+	prompt_ramdisk=0
+	console=ttyS0,115200
+	console=tty0
+	vga=normal
+	root=/dev/ram0
+	rcupdate.rcu_cpu_stall_timeout=100
+	drbd.minor_count=8
+	rw
 
+	path_prefix='http://bee.sh.intel.com/~lkp/'
+	hostname=$hostname
+)
+
+"${kvm[@]}" --append "${append[*]}"
