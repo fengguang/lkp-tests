07ce567448232e76ec272610bfa2efaf10756622
Fengguang Wu <fengguang.wu@intel.com>
2016-08-15 10:20:32 +0800

    need_kconfig: add arch constraints
    To fix boot error
    
      early console in setup code
      This kernel requires an x86-64 CPU, but only detected an i686 CPU.
      Unable to boot - please use a kernel appropriate for your CPU.
    
    caused by
    
      qemu-system-i386 -enable-kvm -kernel /pkg/linux/x86_64-rhel/gcc-6/...
    
    which is queued by queue_rhel_boot_tests():
    
            /lkp/lkp/src/sbin/queue -q boot -t vm -b $branch -k $commit -c $kconfig -R $repeat -s "compiler: $compiler" boot.yaml
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- filters/need_kconfig.rb
+++ filters/need_kconfig.rb
@@ -21,7 +21,7 @@ def check_kconfig(kconfig_lines, line)
 end
 
 def check_all(kconfig_lines)
-	Array(___).each do |e|
+	$___.each do |e|
 		unless check_kconfig(kconfig_lines, e)
 			if __FILE__ =~ /suggest_kconfig/
 				puts "suggest kconfig: #{e}"
@@ -31,6 +31,39 @@ def check_all(kconfig_lines)
 		end
 	end
 end
+
+def check_arch_constraints
+	model = self['model']
+	rootfs = self['rootfs']
+	kconfig = self['kconfig']
+
+	case model
+	when /^qemu-system-x86_64/
+		case rootfs
+		when /-x86_64/
+			# Check kconfig to find mismatches earlier, in cases
+			# when the exact kernel is still not available:
+			# - commit=BASE|HEAD|CYCLIC_BASE|CYCLIC_HEAD late binding
+			# - know exact commit, however yet to compile the kernel
+			raise Job::ParamError, "32bit kernel cannot run 64bit rootfs: '#{kconfig}' '#{rootfs}'" if kconfig =~ /^i386-/
+			$___ << 'CONFIG_X86_64=y'
+		when /-i386/
+			$___ << 'CONFIG_IA32_EMULATION=y' if kconfig =~ /^x86_64-/
+		end
+	when /^qemu-system-i386/
+		case rootfs
+		when /-x86_64/
+			raise Job::ParamError, "32bit QEMU cannot run 64bit rootfs: '#{model}' '#{rootfs}'"
+		when /-i386/
+			raise Job::ParamError, "32bit QEMU cannot run 64bit kernel: '#{model}' '#{kconfig}'" if kconfig =~ /^x86_64-/
+			$___ << 'CONFIG_X86_32=y'
+		end
+	end
+end
+
+$___ = Array(___)
+
+check_arch_constraints
 
 if kconfig_lines = read_kconfig_lines
 	check_all(kconfig_lines)
