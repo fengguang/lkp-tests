159284fed26860457673470c22fce953f5e9d1cc
Fengguang Wu <fengguang.wu@intel.com>
2016-09-02 19:41:47 +0800

    git: add timeout
    It can block for hours.
    
        \_ /bin/bash /lkp/renyl/src/tests/pack
            \_ tee -a /tmp/lkp/result/pack
            \_ /bin/bash /lkp/renyl/src/sbin/pack -c -s /osima
                \_ git clone -q https://git.kernel.org/pub/scm
                    \_ git-remote-https origin https://git.ker
                        \_ git fetch-pack --stateless-rpc --st
                            \_ git index-pack --stdin --fix-th
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/git.sh
+++ lib/git.sh
@@ -15,28 +15,30 @@ git_clone_update()
 
 	source_dir=$PWD/$dir
 
+	local git="timeout 60m git"
+
 	if [ -d $dir/.git ]; then
 		(
 			cd $dir
 			for retry in 1 2
 			do
 				echo \
-				git remote update origin
-				git remote update origin 2>&1 ||
-				git remote update origin 2>&1
+				$git remote update origin
+				$git remote update origin 2>&1 ||
+				$git remote update origin 2>&1
 
 				echo \
-				git checkout -q origin/master
-				git checkout -q origin/master 2>&1 && break
+				$git checkout -q origin/master
+				$git checkout -q origin/master 2>&1 && break
 			done
 		)
 	else
 		rm -fr "$dir" 2>/dev/null
 		echo \
-		git clone -q "$@" $url $dir
-		git clone -q "$@" $url $dir 2>&1 ||
-		git clone -q "$@" $url $dir 2>&1 ||
-		git clone -q "$@" $url $dir
+		$git clone -q "$@" $url $dir
+		$git clone -q "$@" $url $dir 2>&1 ||
+		$git clone -q "$@" $url $dir 2>&1 ||
+		$git clone -q "$@" $url $dir
 	fi
 }
 
