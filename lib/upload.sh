a78070591ffd8823055c43a52535fa6cb5be79f1
Yilong Ren <yilongx.ren@intel.com>
2016-09-04 12:01:13 +0800

    lib/upload.sh: add some sanity check
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/upload.sh
+++ lib/upload.sh
@@ -9,15 +9,20 @@ is_local_server()
 
 upload_files()
 {
+	local files
 	local file
 	local ret=0
+
+	[ $# -ne 0 ] || return
 
 	if has_cmd rsync && is_local_server; then
 		rsync -a --ignore-missing-args --min-size=1 "$@" rsync://$LKP_SERVER$JOB_RESULT_ROOT/
 	elif has_cmd curl; then
-		for file
+		files=$(find "$@" -type f -size +0 2>/dev/null)
+		[ -n "$files" ] || return
+
+		for file in $files
 		do
-			[ -s "$file" ] || continue
 			curl -T $file http://$LKP_SERVER$JOB_RESULT_ROOT/ || ret=$?
 		done
 		return $ret
@@ -25,15 +30,13 @@ upload_files()
 		# NFS is the last resort -- it seems unreliable, either some
 		# content has not reached NFS server during post processing, or
 		# some files occasionally contain some few '\0' bytes.
-		for file
-		do
-			[ -s "$file" ] || continue
-			chown lkp.lkp "$file"
-			chmod ug+w    "$file"
-			cp -p "$file" $RESULT_ROOT/ || ret=$?
-		done
-		[ "$ret" -ne 0 ] && ls -l "$@" $RESULT_ROOT 2>&1
-		return $ret
+
+		chown -R lkp.lkp "$@"
+		chmod -R  ug+w "$@"
+		cp -a "$@" $RESULT_ROOT/ || {
+			ls -l "$@" $RESULT_ROOT 2>&1
+			return 1
+		}
 	fi
 }
 
