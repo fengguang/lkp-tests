ac27ce92601717446719234171a0679bddb29107
Fengguang Wu <fengguang.wu@intel.com>
2016-09-04 13:47:37 +0800

    upload: refactor code into functions
    CC: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/upload.sh
+++ lib/upload.sh
@@ -7,39 +7,56 @@ is_local_server()
 	return 1
 }
 
-upload_files()
+upload_files_rsync()
 {
-	local files
+	rsync -a --ignore-missing-args --min-size=1 "$@" rsync://$LKP_SERVER$JOB_RESULT_ROOT/
+}
+
+upload_files_curl()
+{
 	local file
 	local ret=0
 
+	for file
+	do
+		curl -T "$file" http://$LKP_SERVER$JOB_RESULT_ROOT/$file || ret=$?
+	done
+
+	return $ret
+}
+
+upload_files_copy()
+{
+	chown -R lkp.lkp "$@"
+	chmod -R ug+w "$@"
+
+	cp -a "$@" $RESULT_ROOT/ && return
+
+	ls -l "$@" $RESULT_ROOT 2>&1
+	return 1
+}
+
+upload_files()
+{
 	[ $# -ne 0 ] || return
 
 	if has_cmd rsync && is_local_server; then
-		rsync -a --ignore-missing-args --min-size=1 "$@" rsync://$LKP_SERVER$JOB_RESULT_ROOT/
-		return $?
+		upload_files_rsync "$@"
+		return
 	fi
 
-	files=$(find "$@" -type f -size +0 2>/dev/null)
+	local files=$(find "$@" -type f -size +0 2>/dev/null)
 	[ -n "$files" ] || return
 
 	if has_cmd curl; then
-		for file in $files
-		do
-			curl -T $file http://$LKP_SERVER$JOB_RESULT_ROOT/$file || ret=$?
-		done
-		return $ret
+		upload_files_curl "$@"
+		return
 	else
 		# NFS is the last resort -- it seems unreliable, either some
 		# content has not reached NFS server during post processing, or
 		# some files occasionally contain some few '\0' bytes.
-
-		chown -R lkp.lkp $files
-		chmod -R  ug+w $files
-		cp -a $files $RESULT_ROOT/ || {
-			ls -l $files $RESULT_ROOT 2>&1
-			return 1
-		}
+		upload_files_copy "$@"
+		return
 	fi
 }
 
