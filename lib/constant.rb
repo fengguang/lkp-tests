eb7cb1adb437526d328c069886388b5960ea85a5
Yilong Ren <yilongx.ren@intel.com>
2016-09-06 09:28:25 +0800

    lib/constant.rb: switch to new kconfig
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/constant.rb
+++ lib/constant.rb
@@ -3,5 +3,5 @@ LKP_SRC ||= ENV['LKP_SRC']
 
 require "#{LKP_SRC}/lib/constant-shared.rb"
 
-DEVEL_HOURLY_KCONFIGS = ['x86_64-rhel']
+DEVEL_HOURLY_KCONFIGS = ['x86_64-rhel-7.2']
 GIT_ROOT_DIR = '/c/repo'
