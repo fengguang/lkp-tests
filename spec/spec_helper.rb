d2b9bdb25cc3c8f9753824882975eaeeb9495e03
Philip Li <philip.li@intel.com>
2016-11-18 18:02:52 +0800

    spec: add coverage check
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'rspec'
 
+if ENV['GENERATE_COVERAGE'] == 'true'
+	require 'simplecov'
+	require 'simplecov-rcov'
+	SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+	SimpleCov.start
+end
+
 $LOAD_PATH.delete_if {|p| File.expand_path(p) == File.expand_path("./lib")}
 
 LKP_SRC ||= ENV['LKP_SRC']
