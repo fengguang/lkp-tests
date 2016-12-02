0b89064c4c277515ed48509f0f15a74380352b1b
Huang Ying <ying.huang@intel.com>
2016-12-02 11:11:36 +0800

    Git: Make it possible for open to return nil for non-exist git tree
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/git_ext.rb
+++ lib/git_ext.rb
@@ -46,6 +46,10 @@ module Git
 
 			working_dir = options[:working_dir] || "#{GIT_ROOT_DIR}/#{options[:project]}"
 
+			if options[:may_not_exist] && !Dir.exist?(working_dir)
+				return nil
+			end
+
 			Git.orig_open(working_dir, options)
 		end
 
