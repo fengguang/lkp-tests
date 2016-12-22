f0428cf75344f088b4e01f6d6fc8d9f8e9cd73b1
Yilong Ren <yilongx.ren@intel.com>
2016-12-22 18:10:02 +0800

    lib/nresult_root.rb: add mce-log to MResultRootTableSet
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nresult_root.rb
+++ lib/nresult_root.rb
@@ -283,7 +283,7 @@ class MResultRootTableSet
 		 'galileo', 'irda-kernel', 'kernel_selftests', 'kvm-unit-tests','kvm-unit-tests-qemu',
 		 'locktorture', 'mce-test',  'otc_ddt', 'piglit', 'pm-qa',
 		 'qemu', 'rcutorture', 'suspend', 'trinity', 'ndctl', 'nfs-test', 'hwsim', 'idle-inject',
-		 'mdadm-selftests', 'xsave-test', 'nvml', 'test_bpf']
+		 'mdadm-selftests', 'xsave-test', 'nvml', 'test_bpf', 'mce-log']
 	OTHER_TESTCASES =
 		['0day-boot-tests', '0day-kbuild-tests', 'build-dpdk', 'build-nvml',
 		 'build-qemu', 'convert-lkpdoc-to-html', 'convert-lkpdoc-to-html-css',
