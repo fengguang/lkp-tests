02ed91fa3dd57e737591d72d07046aa018eef6c5
Lei Lu <leix.lu@intel.com>
2017-01-10 02:55:26 -0500

    spec: refine spec code to reduce rubocop issues
    Signed-off-by: Lei Lu <leix.lu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/result_path_spec.rb
+++ spec/result_path_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 require "#{LKP_SRC}/lib/result"
 
 describe ResultPath do
-	describe '.parse_result_root' do
+	describe '#parse_result_root' do
 		context 'when given valid result root' do
 			it 'succeeds' do
 				result_path = described_class.new
 
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2').to be true
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/').to be true
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27').to be true
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2')).to be true
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/')).to be true
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27')).to be true
 				expect(result_path['testcase']).to eq 'aim7'
 				expect(result_path['path_params']).to eq 'performance-2000-fork_test'
 				expect(result_path['tbox_group']).to eq 'brickland3'
@@ -18,24 +18,24 @@ describe ResultPath do
 				expect(result_path['compiler']).to eq 'gcc-4.9'
 				expect(result_path['commit']).to eq '0f57d86787d8b1076ea8f9cbdddda2a46d534a27'
 
-				expect(result_path.parse_result_root '/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/2').to be true
-				expect(result_path.parse_result_root '/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/').to be true
-				expect(result_path.parse_result_root '/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92').to be true
+				expect(result_path.parse_result_root('/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/2')).to be true
+				expect(result_path.parse_result_root('/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/')).to be true
+				expect(result_path.parse_result_root('/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92')).to be true
 				expect(result_path['testcase']).to eq 'build-qemu'
 				expect(result_path['qemu_config']).to eq 'x86_64-softmmu'
 				expect(result_path['qemu_commit']).to eq 'a58047f7fbb055677e45c9a7d65ba40fbfad4b92'
 
-				expect(result_path.parse_result_root '/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/0').to be true
-				expect(result_path.parse_result_root '/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/').to be true
-				expect(result_path.parse_result_root '/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be true
+				expect(result_path.parse_result_root('/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/0')).to be true
+				expect(result_path.parse_result_root('/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/')).to be true
+				expect(result_path.parse_result_root('/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc')).to be true
 				expect(result_path['testcase']).to eq 'build-dpdk'
 				expect(result_path['rootfs']).to eq 'dpdk-rootfs'
 				expect(result_path['dpdk_config']).to eq 'x86_64-native-linuxapp-gcc'
 				expect(result_path['dpdk_compiler']).to eq 'gcc-4.9'
 				expect(result_path['dpdk_commit']).to eq '60c5c5692107abf4157d48493aa2dec01f6b97cc'
 
-				expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4/').to be true
-				expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4').to be true
+				expect(result_path.parse_result_root('/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4/')).to be true
+				expect(result_path.parse_result_root('/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da4')).to be true
 				expect(result_path['testcase']).to eq 'hwinfo'
 				expect(result_path['path_params']).to eq 'performance-1'
 				expect(result_path['tbox_group']).to eq 'lkp-a03'
@@ -49,18 +49,18 @@ describe ResultPath do
 		context 'when given invalid result root' do
 			it 'fails' do
 				result_path = described_class.new
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a2').to be false
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/').to be false
-				expect(result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9').to be false
-				expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da').to be false
-				expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/').to be false
-				expect(result_path.parse_result_root '/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel').to be false
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a2')).to be false
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/')).to be false
+				expect(result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9')).to be false
+				expect(result_path.parse_result_root('/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/0df1f2487d2f0d04703f142813d53615d62a1da')).to be false
+				expect(result_path.parse_result_root('/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel/')).to be false
+				expect(result_path.parse_result_root('/result/lkp-a03/hwinfo/performance-1/debian-x86_64.cgz/x86_64-rhel')).to be false
 
-				expect(result_path.parse_result_root '/result/build-qemu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/').to be false
+				expect(result_path.parse_result_root('/result/build-qemu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/')).to be false
 
-				expect(result_path.parse_result_root '/result/build-dpdk/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/x86_64-native-linuxapp-gcc/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be false
-				expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc').to be false
-				expect(result_path.parse_result_root '/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc/0').to be false
+				expect(result_path.parse_result_root('/result/build-dpdk/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/x86_64-native-linuxapp-gcc/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc')).to be false
+				expect(result_path.parse_result_root('/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc')).to be false
+				expect(result_path.parse_result_root('/result/build-dpdk/x86_64-native-linuxapp-gcc/gcc/60c5c5692107abf4157d48493aa2dec01f6b97cc/0')).to be false
 			end
 		end
 	end
@@ -68,11 +68,11 @@ describe ResultPath do
 	valid_dpdk_result_root = '/result/build-dpdk/dpdk-rootfs/x86_64-native-linuxapp-gcc/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/gcc-4.9/60c5c5692107abf4157d48493aa2dec01f6b97cc/0'
 	valid_qemu_result_root = '/result/build-qemu/x86_64-softmmu/a58047f7fbb055677e45c9a7d65ba40fbfad4b92/2'
 
-	describe '.each_commit' do
+	describe '#each_commit' do
 		it 'handles default path' do
 			result_path = described_class.new
 
-			result_path.parse_result_root '/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2'
+			result_path.parse_result_root('/result/aim7/performance-2000-fork_test/brickland3/debian-x86_64-2015-02-07.cgz/x86_64-rhel/gcc-4.9/0f57d86787d8b1076ea8f9cbdddda2a46d534a27/2')
 
 			result_path.each_commit do |project, commit_axis|
 				expect(project).to eq 'linux'
@@ -106,7 +106,7 @@ describe ResultPath do
 			result_path = described_class.new
 			result_path.parse_result_root valid_dpdk_result_root
 
-			expect(result_path.each_commit.any? { |project| project == 'dpdk'}).to be true
+			expect(result_path.each_commit.any? { |project| project == 'dpdk' }).to be true
 		end
 	end
 
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -6,9 +6,9 @@ describe 'load_yaml_with_flock' do
 	TEST_YAML_FILE = '/tmp/test.yaml'.freeze
 
 	before(:example) do
-		File.open(TEST_YAML_FILE, 'w') { |f|
-			f.write("key1: value1\nkey2: value2\n")
-		}
+		File.open(TEST_YAML_FILE, 'w') do |f|
+		   	f.write("key1: value1\nkey2: value2\n")
+		end
 	end
 
 	it 'returns correct value' do
@@ -16,13 +16,12 @@ describe 'load_yaml_with_flock' do
 		expect(yaml['key2']).to eq 'value2'
 	end
 
-	it 'returns nil due to flock by other process' do
-		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR|File::CREAT, 0664)
+	it 'raises Timeout Error due to flock by other process' do
+		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR | File::CREAT, 0o0664)
 		f.flock(File::LOCK_EX)
 
-		yaml = Timeout::timeout(0.001) { load_yaml_with_flock TEST_YAML_FILE } rescue nil
+		expect { Timeout.timeout(0.001) { load_yaml_with_flock TEST_YAML_FILE } }.to raise_error(Timeout::Error)
 		f.close
-		expect(yaml).to be nil
 	end
 
 	after(:example) do
@@ -32,7 +31,7 @@ describe 'load_yaml_with_flock' do
 end
 
 describe 'save_yaml_with_flock' do
-	TEST_YAML_OBJ = {'key1'=>'value1','key2'=>'value2'}
+	TEST_YAML_OBJ = { 'key1' => 'value1', 'key2' => 'value2' }.freeze
 
 	it 'saves yaml file' do
 		save_yaml_with_flock TEST_YAML_OBJ, TEST_YAML_FILE
@@ -40,13 +39,11 @@ describe 'save_yaml_with_flock' do
 		expect(yaml['key2']).to eq 'value2'
 	end
 
-	it 'returns false due to flock by other process' do
-		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR|File::CREAT, 0664)
+	it 'raises Timeout Error due to flock by other process' do
+		f = File.open(TEST_YAML_FILE + '.lock', File::RDWR | File::CREAT, 0o0664)
 		f.flock(File::LOCK_EX)
-
-		Timeout::timeout(0.001) { save_yaml_with_flock TEST_YAML_OBJ, TEST_YAML_FILE } rescue nil
+		expect { Timeout.timeout(0.001) { save_yaml_with_flock TEST_YAML_OBJ, TEST_YAML_FILE } }.to raise_error(Timeout::Error)
 		f.close
-		expect(File.exist? TEST_YAML_FILE).to be false
 	end
 
 	after(:example) do
@@ -56,7 +53,7 @@ describe 'save_yaml_with_flock' do
 end
 
 describe 'yaml_merge_included_files' do
-	YAML_MERGE_SPEC = <<EOF
+	YAML_MERGE_SPEC = <<EOF.freeze
 contents: &borrow-1d
   #{YAML.load_file('jobs/borrow-1d.yaml').to_json}
 
@@ -96,8 +93,8 @@ EOF
 	yaml = yaml_merge_included_files(YAML_MERGE_SPEC, File.dirname(__FILE__))
 	expects = YAML.load(yaml)
 	expects.each do |k, v|
-		next unless Symbol === k
-		next unless Array === v and v.size >= 2
+		next unless k.instance_of?(Symbol)
+		next unless v.instance_of?(Array) && v.size >= 2
 		it k.to_s do
 			expect(v[0]).to eq v[1]
 		end
