f7aaaefd3df8c922ef9678e755114f767ecaa84f
Lei Lu <leix.lu@intel.com>
2017-01-10 09:45:03 -0500

    spec: add unit test for test_bpf
    Signed-off-by: Lei Lu <leix.lu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/test_bpf_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'parse result' do
+	context "when result not have keyword 'jited' and 'check'" do
+			it 'returns formatted result' do
+				actual = `echo "[   15.423329] test_bpf: #255 BPF_MAXINSNS: Too many instructions PASS" | ${LKP_SRC}/stats/test_bpf`
+				expect(actual).to include('BPF_MAXINSNS:_Too_many_instructions.pass: 1')
+			end
+	end
+end
