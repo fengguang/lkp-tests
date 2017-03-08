e162fbe087023967a99a50fdb5a655096f943dd9
Philip Li <philip.li@intel.com>
2017-03-08 18:19:45 +0800

    lib: introduce log.rb
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/log.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+LKP_SRC ||= ENV['LKP_SRC']
+
+require 'logger'
+
+log_formatter = proc do |severity, datetime, _progname, msg|
+  msg = if msg.is_a? Exception
+          ["#{msg.backtrace.first}: #{msg.message.split("\n").first} (#{msg.class.name})", msg.backtrace[1..-1].map { |m| "\tfrom #{m}" }].flatten
+        else
+          msg.to_s.split("\n")
+        end
+
+  msg.map { |m| "#{datetime} #{severity} -- #{m}\n" }.join
+end
+
+$log = Logger.new(STDOUT)
+$log.formatter = log_formatter
+
+$log_error = Logger.new(STDERR)
+$log_error.formatter = log_formatter
+
+# below methods are available
+#   - log_debug
+#   - log_info
+%w(debug info).each do |severity|
+  define_method("log_#{severity}") do |*args, &block|
+    $log.send(severity, *args, &block)
+  end
+end
+
+alias log log_info
+
+# below methods are available
+#   - log_warn
+#   - log_error
+%w(warn error).each do |severity|
+  define_method("log_#{severity}") do |*args, &block|
+    $log_error.send(severity, *args, &block)
+  end
+end
+
+def log_verbose(*args, &block)
+  return unless ENV['LKP_VERBOSE']
+
+  $log.debug(*args, &block)
+end
