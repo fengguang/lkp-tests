4ec387c86bd596c799ee74503435f76e7845edc8
Huang Ying <ying.huang@intel.com>
2017-03-13 09:47:22 +0800

    lib/ftrace.sh: Fix issues reported by shellcheck
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/ftrace.sh
+++ lib/ftrace.sh
@@ -8,22 +8,22 @@ FTRACE_EVENTS_DIR=$TMP/ftrace_events
 
 ftrace_get()
 {
-	local target=$1
-	cat $TRACING/$target
+	target=$1
+	cat "$TRACING/$target"
 }
 
 ftrace_set()
 {
-	local target=$1
+	target=$1
 	shift
-	stdbuf -oL echo "$@" > $TRACING/$target
+	stdbuf -oL echo "$@" > "$TRACING/$target"
 }
 
 ftrace_append()
 {
-	local target=$1
+	target=$1
 	shift
-	stdbuf -oL echo "$@" >> $TRACING/$target
+	stdbuf -oL echo "$@" >> "$TRACING/$target"
 }
 
 echo_time()
@@ -37,31 +37,31 @@ ftrace_reset()
 	ftrace_set current_tracer nop
 	for knob in set_event set_ftrace_filter set_graph_function trace
 	do
-		ftrace_set $knob
+		ftrace_set "$knob"
 	done
 }
 
 ftrace_test_save_time_delta()
 {
-	local time_delta_file=$TMP_RESULT_ROOT/ftrace_time_delta
+	time_delta_file="$TMP_RESULT_ROOT/ftrace_time_delta"
 
-	[ "$HOSTNAME" = avoton2 ] &&
-		grep -q -F uptime $TRACING/trace_clock && {
+	[ "$(hostname)" = avoton2 ] &&
+		grep -q -F uptime "$TRACING/trace_clock" && {
 			ftrace_set trace_clock uptime
-			echo 0 > $time_delta_file
+			echo 0 > "$time_delta_file"
 			return
 		}
 
 	ftrace_reset
 	ftrace_set tracing_on 1
-	local date_ts="$(date '+%s.%N')"
+	date_ts="$(date '+%s.%N')"
 	ftrace_set trace_marker a long long timestamp detect string
-	local trace_ts="$(grep -m1 -o '[0-9.]\+: tracing_mark_write: a long long timestamp detect string' $TRACING/trace)" || {
+	trace_ts="$(grep -m1 -o '[0-9.]\+: tracing_mark_write: a long long timestamp detect string' $TRACING/trace)" || {
 		echo "Failed to detect ftrace time delta."
 		exit 0
 	}
 	ftrace_set tracing_on 0
-	echo "$date_ts - ${trace_ts%%:*}" | bc > $time_delta_file
+	echo "$date_ts - ${trace_ts%%:*}" | bc > "$time_delta_file"
 	ftrace_set trace
 }
 
@@ -81,40 +81,40 @@ ftrace_set_params()
 	# ftrace_test_save_time_delta
 	ftrace_reset
 
-	[ -n "$buffer_size_kb" ] && ftrace_set buffer_size_kb $buffer_size_kb
+	[ -n "$buffer_size_kb" ] && ftrace_set buffer_size_kb "$buffer_size_kb"
 
 	if [ -n "$events" ]; then
-		mkdir -p $FTRACE_EVENTS_DIR
+		mkdir -p "$FTRACE_EVENTS_DIR"
 		for evt in $events; do
-			ftrace_append set_event $evt
-			fmt=$(echo $TRACING/events/*/$evt/format)
-			if [ -f $fmt ]; then
-				cp $fmt $FTRACE_EVENTS_DIR/$evt.fmt
+			ftrace_append set_event "$evt"
+			fmt=$(echo $TRACING/events/*/"$evt"/format)
+			if [ -f "$fmt" ]; then
+				cp "$fmt" "$FTRACE_EVENTS_DIR/$evt.fmt"
 			fi
 		done
 	fi
 
-	ftrace_set current_tracer $current_tracer
+	ftrace_set current_tracer "$current_tracer"
 
 	if [ -n "$ftrace_filters" ]; then
 		for f in $ftrace_filters; do
-			ftrace_append set_ftrace_filter $f
+			ftrace_append set_ftrace_filter "$f"
 		done
 	fi
 
 	if [ -n "$graph_functions" ]; then
 		for f in $graph_functions; do
-			ftrace_append set_graph_function $f
+			ftrace_append set_graph_function "$f"
 		done
 	fi
 
 	if [ -n "$ftrace_options" ]; then
 		for opt in $ftrace_options; do
-			ftrace_set trace_options $opt
+			ftrace_set trace_options "$opt"
 		done
 	fi
 
-	[ -n "$trace_clock" ] && ftrace_set trace_clock $trace_clock
+	[ -n "$trace_clock" ] && ftrace_set trace_clock "$trace_clock"
 }
 
 ftrace_show_params()
@@ -122,7 +122,7 @@ ftrace_show_params()
 	for param in buffer_size_kb set_event current_tracer set_ftrace_filter \
 				    set_graph_function tracing_on trace_options
 	do
-		echo $param: $(ftrace_get $param)
+		echo "$param:" "$(ftrace_get "$param")"
 	done
 }
 
@@ -141,14 +141,14 @@ ftrace_stop()
 ftrace_run()
 {
 	echo_time "going to sleep for $delay seconds"
-	$WAIT_POST_TEST_CMD --timeout $delay
+	$WAIT_POST_TEST_CMD --timeout "$delay"
 
 	ftrace_show_params
 
 	ftrace_start
-	$WAIT_POST_TEST_CMD --timeout $duration
+	$WAIT_POST_TEST_CMD --timeout "$duration"
 	ftrace_stop
 
-	touch $TMP_RESULT_ROOT/ftrace.data
-	[ -n "$events" ] && upload_files -t ftrace_events $FTRACE_EVENTS_DIR/*
+	touch "$TMP_RESULT_ROOT/ftrace.data"
+	[ -n "$events" ] && upload_files -t ftrace_events "$FTRACE_EVENTS_DIR"/*
 }
