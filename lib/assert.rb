535c0a59c1e028e12b6fa2359ef29b371f861157
Philip Li <philip.li@intel.com>
2017-03-24 18:08:33 +0800

    lib/assert: refactor code
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/assert.rb
+++ lib/assert.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# FIXME rli9 leverage an existing lib
+# rli9 FIXME: leverage an existing lib
 def assert(cond, message)
-	raise "ASSERT fail: #{message}" unless cond
+  raise "ASSERT fail: #{message}" unless cond
 end
