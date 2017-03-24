39c46aeecebf81ab1490687d29621c4850692a11
Philip Li <philip.li@intel.com>
2017-03-24 18:08:31 +0800

    lib/time_ext: refactor code
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/time_ext.rb
+++ lib/time_ext.rb
@@ -3,26 +3,38 @@
 require 'date'
 
 class Time
-	def next_month(i = 1)
-		year, month = self.year, self.month + i
-		year, month = year + 1, month - 12 if month > 12
-		Time.new(year, month)
-	end
+  def next_month(i = 1)
+    year = self.year
+    month = self.month + i
 
-	def prev_month(i = 1)
-		year, month = self.year, self.month - i
-		year, month = year - 1, month + 12 if month <= 0
-		Time.new(year, month)
-	end
+    if month > 12
+      year += 1
+      month -= 12
+    end
 
-	def next_day
-		date = self + 24 * 3600
-		Time.new(date.year, date.month, date.day)
-	end
+    Time.new(year, month)
+  end
 
-	class << self
-		def days_of_month(year, month)
-			Date.new(year, month, -1).day
-		end
-	end
+  def prev_month(i = 1)
+    year = self.year
+    month = self.month - i
+
+    if month <= 0
+      year -= 1
+      month += 12
+    end
+
+    Time.new(year, month)
+  end
+
+  def next_day
+    date = self + 24 * 3600
+    Time.new(date.year, date.month, date.day)
+  end
+
+  class << self
+    def days_of_month(year, month)
+      Date.new(year, month, -1).day
+    end
+  end
 end
