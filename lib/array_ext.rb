0370541aea16f61820ba2419f720cedb795b3b5f
Philip Li <philip.li@intel.com>
2017-03-24 18:08:32 +0800

    lib/array_ext: refactor code
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/array_ext.rb
+++ lib/array_ext.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 class Array
-	# multiple two arrays via multiple element with same index,
-	# return the result array.
-	def pos_mulitple(an_arr)
-		zip(an_arr).map { |v1, v2| v1 * v2 }
-	end
+  # multiple two arrays via multiple element with same index,
+  # return the result array.
+  def pos_mulitple(an_arr)
+    zip(an_arr).map { |v1, v2| v1 * v2 }
+  end
 
-	def sum
-		self.inject {|sum, i| sum + i}
-	end
+  def sum
+    inject { |sum, i| sum + i }
+  end
 end
