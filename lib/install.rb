3c402fe88ef7e1a8fe1d71294566afce00bc0429
Philip Li <philip.li@intel.com>
2017-03-24 18:08:36 +0800

    lib/install: refactor code
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/install.rb
+++ lib/install.rb
@@ -5,31 +5,31 @@ LKP_SRC ||= ENV['LKP_SRC']
 require 'yaml'
 
 def adapt_packages(distro, generic_packages)
-	distro_file = "#{LKP_SRC}/distro/adaptation/#{distro}"
-	return generic_packages unless File.exist? distro_file
+  distro_file = "#{LKP_SRC}/distro/adaptation/#{distro}"
+  return generic_packages unless File.exist? distro_file
 
-	distro_packages = YAML.load_file(distro_file)
+  distro_packages = YAML.load_file(distro_file)
 
-	generic_packages.map do |pkg|
-		if distro_packages.include? pkg
-			distro_packages[pkg].to_s.split
-		else
-			pkg
-		end
-	end
+  generic_packages.map do |pkg|
+    if distro_packages.include? pkg
+      distro_packages[pkg].to_s.split
+    else
+      pkg
+    end
+  end
 end
 
 def get_dependency_packages(distro, script)
-	base_file = "#{LKP_SRC}/distro/depends/#{script}"
-	return [] unless File.exist? base_file
+  base_file = "#{LKP_SRC}/distro/depends/#{script}"
+  return [] unless File.exist? base_file
 
-	generic_packages = []
-	File.read(base_file).each_line do |line|
+  generic_packages = []
+  File.read(base_file).each_line do |line|
                 line.sub(/#.*/, '')
                 generic_packages.concat line.split
-        end
+  end
 
-	packages = adapt_packages(distro, generic_packages)
+  packages = adapt_packages(distro, generic_packages)
 
-	return packages.flatten.compact.uniq
+  packages.flatten.compact.uniq
 end
