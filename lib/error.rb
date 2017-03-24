32268a7271109922b32e53603dd482145443e215
Philip Li <philip.li@intel.com>
2017-03-24 18:08:35 +0800

    lib/error: refactor code
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/error.rb
+++ lib/error.rb
@@ -1,11 +1,12 @@
+#!/usr/bin/env ruby
 
-LKP_SRC ||= ENV["LKP_SRC"]
+LKP_SRC ||= ENV['LKP_SRC']
 
 def puts_err(*messages)
-	$stderr.puts "#{File.basename $PROGRAM_NAME}: #{messages.join ' '}"
+  $stderr.puts "#{File.basename $PROGRAM_NAME}: #{messages.join ' '}"
 end
 
-def dump_exception(e, binding = nil)
-	$stderr.puts e.message
-	$stderr.puts e.backtrace.join "\n"
+def dump_exception(e, _binding = nil)
+  $stderr.puts e.message
+  $stderr.puts e.backtrace.join "\n"
 end
