c21df6cd3b048b420b230c7ccd073022098fbd83
Philip Li <philip.li@intel.com>
2017-03-31 11:41:39 +0800

    lib/error: remove deprecated puts_err method
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/error.rb
+++ lib/error.rb
@@ -2,10 +2,6 @@
 
 LKP_SRC ||= ENV['LKP_SRC']
 
-def puts_err(*messages)
-  $stderr.puts "#{File.basename $PROGRAM_NAME}: #{messages.join ' '}"
-end
-
 def dump_exception(e, _binding = nil)
   $stderr.puts e.message
   $stderr.puts e.backtrace.join "\n"
