3cf097b6b5fd19ca27e481db82bddeffdd8144f4
Wu Chenggang <chenggang.wu@intel.com>
2017-04-06 14:38:20 +0800

    lib/install.sh: adapt packages installed with pkg
    Skip the packages installed with pkg when installing with installer
    Otherwise it may cannot find the packege from repo
    
    Signed-off-by: Wu Chenggang <chenggang.wu@intel.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/install.sh
+++ lib/install.sh
@@ -23,18 +23,30 @@ sync_distro_sources()
 	esac
 }
 
+adapt_package()
+{
+	local pkg_name=$1
+	local distro_file=$2
+	[ -z "$distro_file" ] && return 1
+	[ -f "$distro_file" ] || return 1
+	grep "^$pkg_name:" $distro_file
+}
+
+# To adapt packages between distributions, use Debian as the default distribution.
+# There are two kinds of packages, packages provided by distribution and packages installed by makepkg
+# The priority for package adaptation is as follow:
+# - adaptation by makepkg, via $LKP_SRC/distro/adaptation-pkg/$distro
+# - adaptation by distro explicitly, via $LKP_SRC/distro/adaptation/$distro
+# - default to input package name, not found in above
 adapt_packages()
 {
 	local distro_file
 	if [ -z "$PKG_TYPE" ]; then
 		distro_file="$LKP_SRC/distro/adaptation/$distro"
+		makepkg_distro_file="$LKP_SRC/distro/adaptation-pkg/$distro"
 	else
 		distro_file="$LKP_SRC/distro/adaptation-$PKG_TYPE/$distro"
 	fi
-	[ -f "$distro_file" ] || {
-		echo $generic_packages
-		return
-	}
 
 	local distro_pkg=
 
@@ -43,9 +55,9 @@ adapt_packages()
 		local mapping=""
 		local is_arch_dep="$(echo $pkg | grep ":")"
 		if [ -n "$is_arch_dep" ]; then
-			mapping="$(grep "^$pkg:" $distro_file | tail -1)"
+			mapping="$(adapt_package $pkg $distro_file | tail -1)"
 		else
-			mapping="$(grep "^$pkg:" $distro_file | head -1)"
+			mapping="$(adapt_package $pkg $distro_file | head -1)"
 		fi
 		if [ -n "$mapping" ]; then
 			distro_pkg=$(echo $mapping | awk -F": " '{print $2}')
@@ -56,7 +68,7 @@ adapt_packages()
 				[ "$mapping" != "$distro_pkg" ] && [ -n "$distro_pkg" ] && echo $distro_pkg
 			fi
 		else
-			[ -z "$PKG_TYPE" ] && echo $pkg
+			[ -z "$PKG_TYPE" ] && ! adapt_package $pkg $makepkg_distro_file >/dev/null && echo $pkg
 		fi
 	done
 }
