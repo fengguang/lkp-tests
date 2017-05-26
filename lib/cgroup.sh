813170588182f9910c74ad3db6c922d065d54021
kemi <kemi.wang@intel.com>
2017-05-26 15:13:19 +0800

    fix the cgroup v1 creation failed
    Control Groups provide a mechnism for aggregating/partitioning sets oftasks,
    and all their future children, into hierarchical groups with specialized
    behaviour. Adding cgroup setup for lkp-tests benchmark is helpful to figure
    out potential preformance regression.
    
    The implementation of current cgroup setup intends to create a new mount
    point as well as one or more cgroups for benchmark(s). However, it does
    not consider that the existed subsystem controller may have been mounted
     (typically locate at /sys/fs/cgroup directory). Furtherly, to avoid
    interference of other existed cgroups for previous tasks or subtasks, it
    is also necessary to remove these cgroups.
    
    Additionally, mounting all of the subsystem controllers to a unified
    hierarchy in cgroup v1 may potentially fails in eariler kernel version.
    
    Changelog:
    
    v1.1
    a)Correct code indentation and delete redundant space to align with code style
    
    v1.2
    a)Move cgroup/cgroup2 clear/creation function to lib/cgroup.sh
    b)Set PPID to all of the cgroups in different hierarchy, each of which
    contains an individual subsystem controller except cpu,cpuacct hierarchy
    
    v1.3
    a)Remove the empty line beween comments and code
    b)Simpify the implemention of setting PPID to cgroup tasks
    c)Remove Cgroup2 clear/cration function in lib/cgroup.sh
    
    v1.4
    a)Code style modification
    b)Add command log function for reproducing
    
    v1.5
    a)Delete log_cmd for removale type of command, e.g. rm/umount and etc.
    
    Signed-off-by: kemi <kemi.wang@intel.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/cgroup.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+. $LKP_SRC/lib/reproduce-log.sh
+
+#Clear cgroups and subsystem controllers mount point for v1 version
+clear_cgroup() 
+{
+	cgmounts=$(grep ' cgroup ' /proc/mounts)
+
+	if [ -n "$cgmounts" ]; then
+		echo "$cgmounts" |
+		while read line
+		do
+			subsys=$(echo $line | awk '{ print $1 }')
+			subsys_mount=$(echo $line | awk '{ print $2 }')
+			if [ $(basename $subsys_mount) == "systemd" ]; then
+				continue
+			fi
+			cgroups=$(find $subsys_mount -type d | tail -n +2 | tac)
+			for cgroup in $cgroups
+			do
+				rmdir $cgroup 2>/dev/null
+			done
+			umount $subsys_mount
+		done
+	fi
+}
+
+#clear cgroups and subsystem controllers mount point for v2 version
+clear_cgroup2()
+{
+	cgroup2_mount=$(grep ' cgroup2 ' /proc/mounts | awk '{ print $2 }')
+
+	if [ -n "$cgroup2_mount" ]; then
+		cgroups2=$(find $cgroup2_mount -type d | tail -n +2 | tac)
+		for cgroup2 in $cgroups2
+		do
+			rmdir cgroup2
+		done
+		umount $cgroup2_mount
+	fi
+}
+
+#Bind each subsystem to an individual hierachy and create an individual control group
+create_cgroup()
+{
+	local CGROUP_MNT=$1
+	local testcase=$2
+
+	subsys=$(awk 'NR > 1 {printf $1 " "}' /proc/cgroups)
+
+	for item in $subsys
+	do
+		[ "$item" == "cpu" -o "$item" == "cpuacct" ] && {
+			log_cmd mkdir -p $CGROUP_MNT/cpu,cpuacct 2>/dev/null
+			log_cmd mount -t cgroup -o cpu,cpuacct cpu,cpuacct $CGROUP_MNT/cpu,cpuacct 2>/dev/null
+			log_cmd mkdir -p $CGROUP_MNT/cpu,cpuacct/$testcase
+			continue
+		}
+		log_cmd mkdir -p $CGROUP_MNT/$item
+		log_cmd mount -t cgroup -o $item $item $CGROUP_MNT/$item
+		log_cmd mkdir -p $CGROUP_MNT/$item/$testcase
+	done
+}
