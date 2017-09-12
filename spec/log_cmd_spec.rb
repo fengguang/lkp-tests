012e6b0196511013ce20a13afbc05fe45cb2f286
Chen Rong <rongx.a.chen@intel.com>
2017-09-12 15:20:28 +0800

    bin/log_cmd: fix exec command
    exec can not run built-in commands directly (e.g. cd)
    parse $@ for `/bin/sh -c cmd`
    
    ------------
    $ LKP_SRC=/home/rongc/lkp-tests rake spec spec=log_cmd
    cannot load such file -- rubocop/rake_task
    uninitialized constant RuboCop
    PWD = /home/rongc/lkp-tests
    ENV['LKP_SRC'] = /home/rongc/lkp-tests
    /usr/bin/ruby2.3 /usr/bin/rspec --pattern spec/\*\*\{,/\*/\*\*\}/log_cmd_spec.rb
    2017-09-11 10:15:39 mkdir a b
    .2017-09-11 10:15:39 mkdir a b
    .2017-09-11 10:15:39 mkdir "a
    .2017-09-11 10:15:39 mkdir "a b"
    .2017-09-11 10:15:39 cd .
    .
    
    Finished in 0.15997 seconds (files took 0.1806 seconds to load)
    5 examples, 0 failures
    ------------
    
    Signed-off-by: Chen Rong <rongx.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/log_cmd_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'fileutils'
+require 'shellwords'
+
+describe 'log_cmd' do
+  let(:log_cmd) { "#{LKP_SRC}/bin/log_cmd " }
+  before(:all) do
+    @tmp_dir = Dir.mktmpdir
+    FileUtils.chmod 'go+rwx', @tmp_dir
+    Dir.chdir(@tmp_dir)
+  end
+
+  it 'creates multi dirs' do
+    was_good = system(log_cmd + 'mkdir a b')
+    expect(was_good).to be(true)
+    expect(Dir).to be_exist('a')
+    expect(Dir).to be_exist('b')
+    Dir.delete('a')
+    Dir.delete('b')
+  end
+
+  it 'creates dir with space' do
+    was_good = system(log_cmd + 'mkdir "a b"')
+    expect(was_good).to be(true)
+    expect(Dir).to be_exist('a b')
+    Dir.delete('a b')
+  end
+
+  it 'creates dir with single quote' do
+    dir = '"a'
+    was_good = system(log_cmd + 'mkdir ' + Shellwords.escape(dir).to_s)
+    expect(was_good).to be(true)
+    expect(Dir).to be_exist('"a')
+    Dir.delete('"a')
+  end
+
+  it 'creates dir with space and double quotes' do
+    dir = '"a b"'
+    was_good = system(log_cmd + 'mkdir ' + Shellwords.escape(dir).to_s)
+    expect(was_good).to be(true)
+    expect(Dir).to be_exist('"a b"')
+    Dir.delete('"a b"')
+  end
+
+  it 'execute built-in command' do
+    was_good = system(log_cmd + 'cd .')
+    expect(was_good).to be(true)
+  end
+
+  after(:all) do
+    Dir.delete(@tmp_dir)
+  end
+end
