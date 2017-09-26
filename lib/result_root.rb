238498b21d13c905b4f821c74758a39c3ecf0318
Hailin Gu <hailinx.gu@intel.com>
2017-09-26 21:54:18 +0800

    cleanup rubocop issue Style/NestedParenthesizedCalls
    Signed-off-by: Hailin Gu <hailinx.gu@intel.com>
    Reviewed-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/result_root.rb
+++ lib/result_root.rb
@@ -134,7 +134,7 @@ end
 class Completion
   def initialize(line)
     fields = line.split
-    @time = Time.parse(fields[0..2].join ' ')
+    @time = Time.parse(fields[0..2].join(' '))
     @_rt = MResultRoot.new fields[4]
     @status = fields.drop(4).join ' '
   end
