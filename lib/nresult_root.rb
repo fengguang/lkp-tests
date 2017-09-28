564ddb61e3c9599f8ca64a2f0b9bb9c008c14d55
Hailin Gu <hailinx.gu@intel.com>
2017-09-28 11:21:13 +0800

    cleanup rubocop issue Style/MultilineIfModifier
    Signed-off-by: Hailin Gu <hailinx.gu@intel.com>
    Reviewed-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nresult_root.rb
+++ lib/nresult_root.rb
@@ -255,9 +255,11 @@ end
 class << LinuxMResultRootTable
   def create_layout(name, force = false)
     layout = super
-    layout.add_index(DataStore::AxisIndex, 'commit') do |index|
-      index.set_axis_keys ['commit']
-    end if layout
+    if layout
+      layout.add_index(DataStore::AxisIndex, 'commit') do |index|
+        index.set_axis_keys ['commit']
+      end
+    end
     layout
   end
 end
