4a84728d22af00c95bec241f41eae4bc1135fb00
Wu Chenggang <chenggang.wu@intel.com>
2017-10-09 15:35:32 +0800

    lib/constant.rb: change GIT_ROOT_DIR for local run
    Change the GIT_ROOT_DIR to /lkp/repo if it is local run
    
    Signed-off-by: Wu Chenggang <chenggang.wu@intel.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/constant.rb
+++ lib/constant.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(File.dirname(File.realpath(__FILE__)))
 
+require "#{LKP_SRC}/lib/run-env"
 require "#{LKP_SRC}/lib/constant-shared.rb"
 
 DEVEL_HOURLY_KCONFIGS = ['x86_64-rhel-7.2'].freeze
-GIT_ROOT_DIR = '/c/repo'.freeze
+GIT_ROOT_DIR = git_root_dir
