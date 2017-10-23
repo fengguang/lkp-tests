8521272003dd918d9e2617e6d1e535829b421974
Mengyang Liu <mengyang.liu@intel.com>
2017-10-23 10:03:25 +0800

    Add apache.sh to lkp-tests/lib/
    Help to enable apache modules and manage apache service under different distro.
    
    Signed-off-by: Mengyang Liu <mengyang.liu@intel.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/apache.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+apache_debian_style()
+{
+	[ "$APACHE_DEBIAN_STYLE" -eq 1 ] && return
+	if which a2enmod > /dev/null 2>&1; then
+		APACHE_DEBIAN_STYLE=1
+		return 0
+	else
+		APACHE_DEBIAN_STYLE=0
+		return 1
+	fi
+}
+
+set_apache_moddir()
+{
+	if [ -z "$APACHE_MODDIR" ]; then
+		if apache_debian_style; then
+			APACHE_MODDIR='/etc/apache2/mods-enabled'
+		else
+			APACHE_MODDIR='/etc/httpd/conf.modules.d'
+		fi
+	fi
+}
+
+enable_httpd_mod()
+{
+	set_apache_moddir
+
+	for mod in "$@"
+	do
+		sed -i -r "s/#LoadModule\s+${mod}_module/LoadModule ${mod}_module/" "$APACHE_MODDIR"/*conf
+	done
+}
+
+disable_httpd_mod()
+{
+	set_apache_moddir
+
+	for mod in "$@"
+	do
+		sed -i -r "s/LoadModule\s+${mod}_module/#LoadModule ${mod}_module/" "$APACHE_MODDIR"/*conf
+	done
+}
+
+restore_apache_mod()
+{
+	set_apache_moddir
+
+	if [ -d "$APACHE_MODDIR"/backup ]; then
+		rm -f "$APACHE_MODDIR"/*conf "$APACHE_MODDIR"/*load
+		cp -d "$APACHE_MODDIR"/backup/* "$APACHE_MODDIR"
+		rm -rf "$APACHE_MODDIR"/backup
+	fi
+}
+
+backup_apache_mod()
+{
+	set_apache_moddir
+
+	if apache_debian_style; then
+		MODFILE="$APACHE_MODDIR/*conf $APACHE_MODDIR/*load"
+	else
+		MODFILE="$APACHE_MODDIR/*conf"
+	fi
+
+	mkdir -p "$APACHE_MODDIR"/backup
+	rm -rf "$APACHE_MODDIR"/backup/*
+	cp -d $MODFILE "$APACHE_MODDIR"/backup/
+}
+
+enable_apache_mod()
+{
+	if apache_debian_style; then
+		a2enmod "$@"
+	else
+		enable_httpd_mod "$@"
+	fi
+}
+
+disable_apache_mod()
+{
+	if apache_debian_style; then
+		a2dismod "$@"
+	else
+		disable_httpd_mod "$@"
+	fi
+}
+
+systemctl_apache()
+{
+	if apache_debian_style; then
+		systemctl "$@" apache2
+	else
+		systemctl "$@" httpd
+	fi
+}
+
