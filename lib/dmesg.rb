3129f9fd6c49cdd04ddb02ea8ae4aff23f085faf
Xiaolong Ye <xiaolong.ye@intel.com>
2017-11-10 10:22:16 +0800

    dmesg: improve the normalization for KASAN messages
    Given that the KASAN bugs are pretty useful (it's publicly praised by Linus) in
    in finding bugs, previously simple unify of KASAN messages would reduce the
    bisects for them unexpectedly. However, as KASAN maybe messy sometimes in that
    the function name may be truncated at any place.
    
    Workaround it by truncating the function anme to the first character which
    should be good enough for reliably bisecting while keeping enough entropy and
    bisect parallelism.
    
    % g slab-out-of-bounds /tmp/dmesg
       226     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr"
       143     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at_addr"
       128     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_setxattr_at_addr"
        74     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_ip_options_get_from_user_at_addr"
        72     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_xfrm_user_policy_at_addr"
        59     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_to_user_at_addr"
        50     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_of_device_uevent_at_addr"
        43     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_inotify_read_at_addr"
        32     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_filldir_at_addr"
        32     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter"
        30     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write"
        19     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds"
        18     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in"
        12     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_f"
        12     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs"
        11     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at"
        11     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_wr"
        11     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop"
        11     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at"
        11     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_i"
        10     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy"
         9     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write+0x"
         9     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from"
         9     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_fr"
         9     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_c"
         8     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at_ad"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr_ff"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr_f"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_add"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_writ"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kern"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_k"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_to_user"
         7     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at_addr_ff"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr_ffff880"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr_ffff8"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_addr_fff"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_at_a"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_kernfs_fop_write_a"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_ker"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_to_user_at_addr_ffff88001"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at_addr_ffff880"
         6     "stat": "dmesg.BUG:KASAN:slab-out-of-bounds_in_copy_from_iter_at_addr_ffff8"
    
    Suggested-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Xiaolong Ye <xiaolong.ye@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/dmesg.rb
+++ lib/dmesg.rb
@@ -298,7 +298,7 @@ def analyze_error_id(line)
        /(Could not create tracefs)/,
        /(used greatest stack depth:)/,
        /([A-Z]+[ a-zA-Z]*): [a-f0-9]{4} \[#[0-9]+\] /,
-       /(BUG: KASAN: [a-z\-_ ]+) in /,
+       /(BUG: KASAN: [a-z\-_ ]+ in [a-z])/,
        /(cpu clock throttled)/
     line = $1
     bug_to_bisect = $1
