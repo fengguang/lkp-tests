73904a26f0f6af774ac8ca3082d75b7931516232
Li Zhijian <zhijianx.li@intel.com>
2018-01-31 09:53:25 +0800

    lib/stats.rb: fix /lkp/eywa/src/lib/stats.rb:105:in `>': comparison of Fixnum with Hash failed
    compare when they are same class.
    
    if we have a job like:
    --------------
    lizhijian@haswell-OptiPlex-9020:~/lkp-tests$ grep perf-profile -A2 -B2
    jobs/unixbench.yaml
    perf-profile:
      duration: 2
    --------------
    
    after parsing this yaml, we can get
    -------------
    irb(main):015:0> puts options['perf-profile']
    {"duration"=>2}
    => nil
    irb(main):016:0>
    -------------
    
    NOTE, we find options['perf-profile'] should never be a Fixnum in any case,
    so here we log error when we occur this to further analysis
    --------------
    lizhijian@haswell-OptiPlex-9020:~/lkp-tests$ head -n 10
     monitors/no-stdout/perf-profile
     #!/bin/sh
     # - events
     # - period
     # - freq
     # - delay
     # - duration
     # - callgraph
     # - children
     # - inline
    
    --------------
    
    Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/stats.rb
+++ lib/stats.rb
@@ -101,7 +101,8 @@ def is_changed_stats(sorted_a, min_a, mean_a, max_a,
                      is_failure_stat, is_latency_stat,
                      stat, options)
 
-  if options['perf-profile'] && stat =~ /^perf-profile\./
+  if options['perf-profile'] && stat =~ /^perf-profile\./ && options['perf-profile'].is_a?(mean_a.class)
+    log_err "#{options['result_root']}: options['perf-profile'] is a Fixnum"
     return mean_a > options['perf-profile'] ||
            mean_b > options['perf-profile']
   end
