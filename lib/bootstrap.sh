fengguang/lkp-tests

commit e82f8505b5467b7074da48a0080e6c69b52b3efc

Better tracking tboxes that can't use kexec
There are three types of tboxes that can't use kexec.

First is a set of tboxes that is known to have issues when kexec to
next Job kernel.
Second is the tboxes that follows lkp-ipxe solution.
Third is the tboxes that follows lkp-grub soltuion.

It's better to track these three types of machines separately.

Note: this patch is just a prototype one based on currently
information available. As it does not make any functional change,
we'd better have the patch applied first.

TODO:
1. audit all tboxes on this list
2. if some tbox is put into wrong function, move the tbox to proper function
3. if the tbox follows lkp-grub currently, but has PXE support,
   covert to lkp-ipxe solution, and move the tbox to proper function.
And this task is tracked at https://jira01.devtools.intel.com/browse/OLT-4210

CC: Han Ning <ningx.han@intel.com>
Signed-off-by: Zhang Rui <rui.zhang@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/bootstrap.sh
@@ -376,26 +376,39 @@ netconsole_init()
 	modprobe netconsole netconsole=@/,$netconsole_port@$netconsole_server/ 2>/dev/null
 }
 
+tbox_use_lkp_ipxe()
+{
+	[ "${HOSTNAME#*lkp-ivb-toshiba1}"       != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-kbl-lenovo1}"        != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-skl-d01}"    != "$HOSTNAME" ] && return 1
+	return 0
+}
+
+tbox_use_lkp_grub()
+{
+	[ "${HOSTNAME#*lkp-bxt01}"      != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-glk01}"      != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-kbly01}"     != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-kblr01}"     != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-kbls01}"     != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-cfl-s01}"    != "$HOSTNAME" ] && return 1
+	[ "${HOSTNAME#*lkp-sklu-lenovo1}"       != "$HOSTNAME" ] && return 1
+	return 0
+}
+
 tbox_cant_kexec()
 {
 	is_virt && return 0
 
+	tbox_use_lkp_ipxe && return 0
+	tbox_use_lkp_grub && return 0
+
 	# following tbox are buggy while using kexec to boot
-	[ "${HOSTNAME#*lkp-bxt01}"      != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-cfl-s01}"    != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-denverton2}" != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-g5}"         != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-glk01}"      != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-glk02}"      != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-ivb-d02}"    != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-ivb-toshiba1}"       != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-kbl-lenovo1}"        != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-kblr01}"     != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-kbls01}"     != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-kbly01}"     != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-minnow01}"   != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-skl-d01}"    != "$HOSTNAME" ] && return 0
-	[ "${HOSTNAME#*lkp-sklu-lenovo1}"       != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-skly01}"     != "$HOSTNAME" ] && return 0
 
 	[ -x '/sbin/kexec' ] || return 0

commit 83a898da7b4855ff1c5266b67e78e54d1668d76c

lib/bootstrap.sh: fix do_not_reboot_for_same_kernel doesn't take effect
This issue is introduced by commit: e82f850, it used incorrect return
value at tbox_use_lkp_ipxe() and tbox_use_lkp_grub()

CC: Zhang Rui <rui.zhang@intel.com>
Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/bootstrap.sh
@@ -378,22 +378,22 @@ netconsole_init()
 
 tbox_use_lkp_ipxe()
 {
-	[ "${HOSTNAME#*lkp-ivb-toshiba1}"       != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-kbl-lenovo1}"        != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-skl-d01}"    != "$HOSTNAME" ] && return 1
-	return 0
+	[ "${HOSTNAME#*lkp-ivb-toshiba1}"       != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-kbl-lenovo1}"        != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-skl-d01}"    != "$HOSTNAME" ] && return 0
+	return 1
 }
 
 tbox_use_lkp_grub()
 {
-	[ "${HOSTNAME#*lkp-bxt01}"      != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-glk01}"      != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-kbly01}"     != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-kblr01}"     != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-kbls01}"     != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-cfl-s01}"    != "$HOSTNAME" ] && return 1
-	[ "${HOSTNAME#*lkp-sklu-lenovo1}"       != "$HOSTNAME" ] && return 1
-	return 0
+	[ "${HOSTNAME#*lkp-bxt01}"      != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-glk01}"      != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-kbly01}"     != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-kblr01}"     != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-kbls01}"     != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-cfl-s01}"    != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-sklu-lenovo1}"       != "$HOSTNAME" ] && return 0
+	return 1
 }
 
 tbox_cant_kexec()