67123e29c27fce0b23a6200a012ca0162f31c948
Zhang Rui <rui.zhang@intel.com>
2018-03-01 15:55:05 +0800

    lkp-bytm-lenovo1: do not use kexec
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -380,6 +380,7 @@ tbox_use_lkp_ipxe()
 {
 	[ "${HOSTNAME#*lkp-ivb-toshiba1}"       != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-bxtm-lenovo1}"       != "$HOSTNAME" ] && return 0
+	[ "${HOSTNAME#*lkp-bytm-lenovo1}"       != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-kbl-lenovo1}"        != "$HOSTNAME" ] && return 0
 	[ "${HOSTNAME#*lkp-skl-d01}"    != "$HOSTNAME" ] && return 0
 	return 1
