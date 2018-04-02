0679bb73fba13b8c338242165565d9e3d293e03c
Li Zhijian <zhijianx.li@intel.com>
2018-04-02 12:17:48 +0800

    lib/result.rb: add build-perf_test rt scheme
    Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/result.rb
+++ lib/result.rb
@@ -43,7 +43,8 @@ class ResultPath < Hash
     'nvml-unit-tests' => %w[path_params tbox_group rootfs kconfig compiler commit nvml_commit run],
     'mbtest' => %w[path_params tbox_group rootfs kconfig compiler commit mbt_commit run],
     'sof_test' => %w[path_params tbox_group rootfs kconfig compiler commit sof_commit soft_commit run],
-    'kvm-kernel-boot-test' => %w[tbox_group kconfig commit qemu_config qemu_commit linux_commit run]
+    'kvm-kernel-boot-test' => %w[tbox_group kconfig commit qemu_config qemu_commit linux_commit run],
+    'build-perf_test' => %w[perf_test_commit run]
   }.freeze
 
   def path_scheme
