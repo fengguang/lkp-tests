7f204085e00069ba125f5a0091bc936984f96f23
Li Zhijian <zhijianx.li@intel.com>
2018-04-12 13:40:48 +0800

    cleanup rubocop issue Layout/MultilineMethodCallIndentation by --auto-correct
    Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/string_ext.rb
+++ lib/string_ext.rb
@@ -6,14 +6,14 @@ class String
   # for converting log lines into "Content-Type: text/plain;" emails
   def plain_text
     gsub(REGEX_ANSI_COLOR, '')
-        .tr("\r", "\n")
-        .gsub(/[^[:print:]\n]/, '')
+      .tr("\r", "\n")
+      .gsub(/[^[:print:]\n]/, '')
   end
 
   def remediate_invalid_byte_sequence(options = {})
     clone
-        .force_encoding('UTF-8')
-        .encode('UTF-8', 'UTF-8', options.merge(invalid: :replace, undef: :replace))
+      .force_encoding('UTF-8')
+      .encode('UTF-8', 'UTF-8', options.merge(invalid: :replace, undef: :replace))
   end
 
   def replace_invalid_utf8!(to = '_')
