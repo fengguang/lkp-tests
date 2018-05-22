63844686f0ecada53d575b32adb872d047c25ca5
Li Zhijian <zhijianx.li@intel.com>
2018-05-22 09:17:32 +0800

    lib/tests/nvml.sh: fix /usr/include/linux/ndctl.h:212:26: error: use of undeclared identifier 'PAGE_SIZE'
    fix build-nvml errors:
    In file included from ../../src/../src/common/os_dimm_ndctl.c:46:
    /usr/include/linux/ndctl.h:212:26: error: use of undeclared identifier
    'PAGE_SIZE'
            ND_MIN_NAMESPACE_SIZE = PAGE_SIZE,
                                    ^
    1 error generated.
    make[1]: *** [Makefile:167: libpmemblk] Error 2
    make[2]: *** [../Makefile.inc:311:
    ../nondebug/libpmemblk/os_dimm_ndctl.o] Error 1
    make[2]: *** Waiting for unfinished jobs....
    
    Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/nvml.sh
+++ lib/tests/nvml.sh
@@ -40,8 +40,8 @@ build_env()
 	setup_compiler
 
 	# All C++ container tests need customized version of libc --libc++ to compile. So specify the path of libc++ to make.
-	log_cmd make EXTRA_CFLAGS=-DUSE_VALGRIND USE_LLVM_LIBCPP=1 LIBCPP_INCDIR=/usr/local/libcxx/include/c++/v1/ LIBCPP_LIBDIR=/usr/local/libcxx/lib || return
-	log_cmd make EXTRA_CFLAGS=-DUSE_VALGRIND USE_LLVM_LIBCPP=1 LIBCPP_INCDIR=/usr/local/libcxx/include/c++/v1/ LIBCPP_LIBDIR=/usr/local/libcxx/lib test || return
+	log_cmd make EXTRA_CFLAGS="-DPAGE_SIZE=4096 -DUSE_VALGRIND" USE_LLVM_LIBCPP=1 LIBCPP_INCDIR=/usr/local/libcxx/include/c++/v1/ LIBCPP_LIBDIR=/usr/local/libcxx/lib || return
+	log_cmd make EXTRA_CFLAGS="-DPAGE_SIZE=4096 -DUSE_VALGRIND" USE_LLVM_LIBCPP=1 LIBCPP_INCDIR=/usr/local/libcxx/include/c++/v1/ LIBCPP_LIBDIR=/usr/local/libcxx/lib test || return
 }
 
 can_skip_copy_source()
