def0115deec16918c755c27e887a638c8a20bb84
Philip Li <philip.li@intel.com>
2018-07-17 14:40:17 +0800

    lib: refactor code to remove constant-shared.rb
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/constant.rb
+++ lib/constant.rb
@@ -2,7 +2,12 @@
 LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
 
 require "#{LKP_SRC}/lib/run-env"
-require "#{LKP_SRC}/lib/constant-shared.rb"
 
 DEVEL_HOURLY_KCONFIGS = ['x86_64-rhel-7.2'].freeze
 GIT_ROOT_DIR = git_root_dir
+
+TESTCASE_AXIS_KEY = 'testcase'.freeze
+COMMIT_AXIS_KEY = 'commit'.freeze
+TBOX_GROUP_AXIS_KEY = 'tbox_group'.freeze
+
+KERNEL_ROOT = '/pkg/linux'.freeze
