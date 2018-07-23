fengguang/lkp-tests

commit dde81a97e6503466151d4527b349289e28b24c57

sync audio tbox name
Signed-off-by: Wang, Jia W <jia.w.wang@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job-init.sh
@@ -151,8 +151,8 @@ wait_on_manual_check()
 		elif ! has_cmd 'users'; then
 			break
 		# t100 has XWindow auto login
-		# lkp-minnow01 has root user on its local rootfs
-		elif [ "$HOSTNAME" = 't100' ] || [ "$HOSTNAME" = 'lkp-minnow01' ]; then
+		# sof-minniow-1 and sof-minnow-2 has root user on its local rootfs
+		elif [ "$HOSTNAME" = 't100' ] || [ "$HOSTNAME" = 'sof-minnow-1' ] || [ "$HOSTNAME" = 'sof-minnow-2']; then
 			local users="$(users)"
 			[ "${users#* }" != "$users" ] || break
 		else

commit 4491600fde1bb7886686228b559c5622a353e69d

lib/job-init.sh: Fix /lkp/lkp/src/bin/run-lkp: 160: [: missing ]
Fixes: dde81a97e6("sync audio tbox name")
Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job-init.sh
@@ -157,7 +157,7 @@ wait_on_manual_check()
 			break
 		# t100 has XWindow auto login
 		# sof-minniow-1 and sof-minnow-2 has root user on its local rootfs
-		elif [ "$HOSTNAME" = 't100' ] || [ "$HOSTNAME" = 'sof-minnow-1' ] || [ "$HOSTNAME" = 'sof-minnow-2']; then
+		elif [ "$HOSTNAME" = 't100' ] || [ "$HOSTNAME" = 'sof-minnow-1' ] || [ "$HOSTNAME" = 'sof-minnow-2' ]; then
 			local users="$(users)"
 			[ "${users#* }" != "$users" ] || break
 		else