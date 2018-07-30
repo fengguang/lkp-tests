fengguang/lkp-tests

commit cecb3121a0909ca3fc3e7efc9c0ecd86f5e7f829

lib/bootstrap.sh: wait for the machine to load the disk
fix the issue
--------------------
user  :err   : [  +2.336098] mount: special device /dev/disk/by-id/scsi-3600508e000000000cd72e93c31c5150d-part1 does not exist
user  :err   : [  +0.859600] ERROR: failed to check size for /dev/disk/by-id/scsi-3600508e000000000cd72e93c31c5150d-part1: No such file or directory
user  :err   : [  +0.002127] mount: special device /dev/disk/by-id/scsi-3600508e000000000cd72e93c31c5150d-part1 does not exist
--------------------
It will cost 2s to load the disk at lkp-hsx04 in my test.
If fail to load the disk, it will report like below:
--------------------
/result/borrow/20-ucode=0xb00002e/lkp-bdw-ep3c/debian-x86_64-2018-04-03.cgz/x86_64-rhel-7.2/gcc-7/ce397d215ccd07b8ae3f71db689aedb85d56ab40/3$ cat last_state
is_incomplete_run: 1
load_disk_fail: 1
--------------------

Signed-off-by: leishaoting <leist.fnst@cn.fujitsu.com>

Signed-off-by: Philip Li <philip.li@intel.com>

lib/bootstrap.sh
@@ -337,9 +337,31 @@ cleanup_pkg_cache()
 	find "$pkg_cache" \( -type f -mtime +7 -delete \) -or \( -type d -ctime +7 -empty -delete \)
 }
 
+wait_load_disk()
+{
+	# set the max time of wait is 30s
+	for i in $(seq 30)
+	do
+		# eg: /dev/disk/by-id/ata-WDC_WD1002FAEX-00Z3A0_WD-WCATRC577623-part2
+		ls $rootfs_partition >/dev/null 2>&1 && return
+		# eg: LABEL=LKP-ROOTFS
+		blkid | grep -q ${rootfs_partition#*=} && return
+		sleep 1
+	done
+
+	return 1
+}
+
 mount_rootfs()
 {
 	if [ -n "$rootfs_partition" ]; then
+		# wait for the machine to load the disk
+		wait_load_disk || {
+			# skipping following test if disk can't be load
+			echo "can't load the disk $rootfs_partition, skip testing..."
+			set_job_state 'load_disk_fail'
+			return 1
+		}
 		ROOTFS_DIR=/opt/rootfs
 		mkdir -p $ROOTFS_DIR
 		mount $rootfs_partition $ROOTFS_DIR || {
@@ -596,7 +618,7 @@ boot_init()
 	run_ntpdate
 
 	mount_debugfs
-	mount_rootfs
+	mount_rootfs || return
 
 	netconsole_init
 }

bin/lkp-setup-rootfs
@@ -29,7 +29,12 @@ cat > /proc/$$/oom_score_adj <<EOF
 -1000
 EOF
 
-boot_init
+boot_init || {
+	echo "LKP: rebooting due to boot init failure"
+	set_tbox_wtmp 'rebooting'
+	start_reboot_watchdog
+	reboot_tbox 2>/dev/null && exit 1
+}
 
 # when to not reboot is a policy issue, which can be refined later.
 # here just serves as an example only.

commit 5447ec96d25e60f46307a37383c75cd5917606b5

lib/bootstrap.sh: don't init netconsole at local run
Currently(since cecb3121a "lib/bootstrap.sh: wait for the machine to load
the disk"), tbox will be rebooted if boot_init failed.
But at local env(includes lkp qemu), netconsole is not always passed due
to lack of netconsole modules etc.

So if use lkp qemu to run a job which has be specified netconsole, the
guest will be rebooted due to boot_init failed

Fixes: cecb3121a("lib/bootstrap.sh: wait for the machine to load the disk")
Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/bootstrap.sh
@@ -622,5 +622,7 @@ boot_init()
 	mount_debugfs
 	mount_rootfs || return
 
+	# don't init netconsole at local run
+	[ "$LKP_LOCAL_RUN" = "1" ] && return
 	netconsole_init
 }