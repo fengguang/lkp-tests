fengguang/lkp-tests

commit fd83036da3e23c2c2d583a756f4af354aac908ea

etc: merge bisect-blacklist to blacklist
Signed-off-by: Philip Li <philip.li@intel.com>

lib/stats.rb
@@ -154,11 +154,6 @@ def blacklist_auto_report_stat?(stat)
   stat =~ auto_report_blacklist_re
 end
 
-def blacklist_bisect_stat?(stat)
-  bisect_blacklist_re = load_regular_expressions("#{LKP_SRC}/etc/bisect-blacklist")
-  stat =~ bisect_blacklist_re
-end
-
 def is_changed_stats(sorted_a, min_a, mean_a, max_a,
                      sorted_b, min_b, mean_b, max_b,
                      is_failure_stat, is_latency_stat,

etc/bisect-blacklist
@@ -1,24 +1,0 @@
-^boot-meminfo.
-^boot-slabinfo.
-^boot-time.
-^cpuidle.
-^interrupts.
-^iostat.
-^latency_stats.
-^lock_stat.
-^meminfo.
-^mpstat.
-^numa-meminfo.
-^numa-numastat.
-^numa-vmstat.
-^numa-vmstat.
-^perf-profile.
-^perf-stat.
-^proc-vmstat.
-^sched_debug.
-^slabinfo.
-^softirqs.
-^time.
-^turbostat.
-^uptime.
-^vmstat.

etc/blacklist
@@ -4,13 +4,17 @@
 ^aim7.*.jobs-per-min-per-task
 ^aim7.*.jti
 ^aim7.*.real
-^build-sof.dmesg.*
-^build-sof.stderr.*
-^build-sof.kmsg.*
+^boot-meminfo.
+^boot-slabinfo.
+^boot-time.
 ^buddyinfo.*
 ^build-dpdk.build-dpdk.fail
 ^build-dpdk.time.*
 ^build-qemu.time.*
+^build-sof.dmesg.*
+^build-sof.kmsg.*
+^build-sof.stderr.*
+^cpuidle.
 ^DDT.*\.pass
 ^diskstats.*
 ^dmesg.backtrace
@@ -25,15 +29,17 @@
 ^dmesg.INFO:rcu_sched_detected_stalls_on_CPUs/tasks:
 ^dmesg.INFO:Stall_ended_before_state_dump_start
 ^dmesg.invoked_oom-killer:gfp_mask=0x
-^dmesg.kobject\(.+#\):tried_to_init_an_initialized_object
-^dmesg.kobject\(\(_+ptrval_+\)\):tried_to_init_an_initialized_object
 ^dmesg.Kernel_panic-not_syncing:Out_of_memory.*
+^dmesg.kobject\(\(_+ptrval_+\)\):tried_to_init_an_initialized_object
+^dmesg.kobject\(.+#\):tried_to_init_an_initialized_object
 ^dmesg..*/lkp/
 ^dmesg.Mem-Info
 ^dmesg.timestamp:.*
 ^internal-lkp-service.*
+^interrupts.
 ^interrupts.(?!CAL:).*
 ^interrupts.CPU
+^iostat.
 ^iostat.cpu
 ^iostat.*.max$
 ^iostat.*.svctm
@@ -49,18 +55,33 @@
 ^kmsg.Starting_Bootlog_daemon:bootlogd:cannot_allocate_pseudo_tty:No_such_file_or_directory
 ^kmsg.start:Job_is_already_running:rc
 ^kmsg.timestamp:.*
+^kmsg.*torture:.*
 ^kmsg.tsc:Fast_TSC_calibration_failed
 ^kmsg.tsc:HPET/PMTIMER_calibration_failed
 ^kmsg.tsc:Unable_to_calibrate_against_PIT
-^kmsg.*torture:.*
 ^last_state.*
+^latency_stats.
 ^latency_stats.*cifs.*
 ^lkp-install-run.*
+^lock_stat.
 ^max_iostat.
+^meminfo.
+^mpstat.
+^numa-meminfo.
+^numa-numastat.
+^numa-vmstat.
+^numa-vmstat.
 ^pagetypeinfo.*
 ^pagetypeinfo.page_block_order
 ^pagetypeinfo.pages_per_block
+^perf-profile.
+^perf-stat.
+^perf-stat\.i\.
 ^phoronix-test-suite.*.not_installed
+^proc-vmstat.
+^sched_debug.
+^slabinfo.
+^softirqs.
 ^softirqs.CPU
 ^stderr.*:cannot_create_regular_file.*:File_exists
 ^stderr.Can_not_find_perf_command
@@ -95,10 +116,13 @@
 ^stderr.*write_error
 ^syzkaller.*
 ^tcrypt.*
+^time.
 .*\.total_fail
 .*\.total_pass
 .*\.total_test
+^turbostat.
+^uptime.
+^vmstat.
 ^vmstat.cpu
 ^will-it-scale.[0-9]
 ^zoneinfo.*
-^perf-stat\.i\.

sbin/mmplot
@@ -83,5 +83,7 @@ else
   changed_fields = changed_stats.keys
 end
 
-changed_fields.reject! { |stat| is_failure(stat) || blacklist_bisect_stat?(stat) } if $options['concise']
+blacklist_re = load_regular_expressions("#{LKP_SRC}/etc/blacklist")
+
+changed_fields.reject! { |stat| is_failure(stat) || stat =~ blacklist_re } if $options['concise']
 mmplot(matrix1, matrix2, changed_fields)

commit 6919e2d50c3c818ba3f4d83552ec273a4c155912

lib/stats.rb: fix many matrixes lost in report
We merged bisect-blacklist to blacklist, but the stats in blacklist
will be ignored when comparing the results. so we introduce report-whitelist
to allow the matrixes shown in reports.
------------
bad compare result:
         %stddev     %change         %stddev
             \          |                \
    681430            -1.5%     671449        will-it-scale.per_process_ops
  59965884            -1.5%   59087561        will-it-scale.workload

expect compare result:
         %stddev     %change         %stddev
             \          |                \
    681430            -1.5%     671449        will-it-scale.per_process_ops
  59965884            -1.5%   59087561        will-it-scale.workload
  11240661 ± 29%    +106.6%   23227862 ± 13%  perf-stat.i.node-loads
      1.57 ± 29%     +10.5%       1.74        perf-stat.overall.cpi
  39294872            -1.4%   38752360        proc-vmstat.numa_hit
  39277717            -1.4%   38735207        proc-vmstat.numa_local
  39384695            -1.3%   38854363        proc-vmstat.pgalloc_normal
 1.802e+10            -1.5%  1.775e+10        proc-vmstat.pgfault
------------

Fixes: fd83036d ("etc: merge bisect-blacklist to blacklist")
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

etc/report-whitelist
@@ -1,0 +1,24 @@
+^boot-meminfo.
+^boot-slabinfo.
+^boot-time.
+^cpuidle.
+^interrupts.
+^iostat.
+^latency_stats.
+^lock_stat.
+^meminfo.
+^mpstat.
+^numa-meminfo.
+^numa-numastat.
+^numa-vmstat.
+^numa-vmstat.
+^perf-profile.
+^perf-stat.
+^proc-vmstat.
+^sched_debug.
+^slabinfo.
+^softirqs.
+^time.
+^turbostat.
+^uptime.
+^vmstat.

lib/stats.rb
@@ -25,6 +25,7 @@ $index_perf = load_yaml "#{LKP_SRC}/etc/index-perf-all.yaml"
 
 $perf_metrics_re = load_regular_expressions("#{LKP_SRC}/etc/perf-metrics-patterns")
 $metrics_blacklist_re = load_regular_expressions("#{LKP_SRC}/etc/blacklist")
+$report_whitelist_re = load_regular_expressions("#{LKP_SRC}/etc/report-whitelist")
 $kill_pattern_whitelist_re = load_regular_expressions("#{LKP_SRC}/etc/dmesg-kill-pattern")
 
 class LinuxTestcasesTableSet
@@ -505,7 +506,7 @@ def __get_changed_stats(a, b, is_incomplete_run, options)
     next if v[-1].is_a?(String)
     next if options['perf'] && !is_perf_metric(k)
     next if is_incomplete_run && k !~ /^(dmesg|last_state|stderr)\./
-    next if !options['more'] && k =~ $metrics_blacklist_re
+    next if !options['more'] && k =~ $metrics_blacklist_re && k !~ $report_whitelist_re
 
     is_failure_stat = if options['force_' + k]
                         true