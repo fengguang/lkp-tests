92fa1058f608cba49732d3e7b5955ba92e1361cd
Philip Li <philip.li@intel.com>
2019-03-05 16:18:38 +0800

    lib/tests.rb: fix rubocop issues
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests.rb
+++ lib/tests.rb
@@ -2,17 +2,17 @@ LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
 
 require 'set'
 
-def get_all_tests
+def all_tests
   tests = Dir["#{LKP_SRC}/{tests,daemon}/**/*"].map { |d| File.basename(d) }
   tests.delete 'wrapper'
   tests.sort!
   tests
 end
 
-def all_tests
-  $__all_tests ||= get_all_tests.freeze
+def cached_all_tests
+  $__all_tests ||= all_tests.freeze
 end
 
 def all_tests_set
-  $__all_tests_set ||= Set.new(all_tests).freeze
+  $__all_tests_set ||= Set.new(cached_all_tests).freeze
 end
