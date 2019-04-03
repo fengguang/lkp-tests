fengguang/lkp-tests

commit 433f69ffcf76a60eddc266aaaff03b8603458930

stats: correct extremely high bisect priority issue
To fix abnormally high bisect stat priority as below:

{
  "/result/stress-ng/100%-60s-filesystem-performance/lkp-bdw-ep6/debian-x86_64-2016-08-31.cgz/x86_64-rhel-7.2/gcc-7/67b8d5c7081221efa252e111cd52532ec6d4266f": [
    ...
    {
      "stat": "proc-vmstat.pgmigrate_fail",
      "interval": "[ 0          - 13312      ] -- [ 0          - 0          ]",
      "a": [
        0,
        0,
        6144,
        7168,
        7680,
        8192,
        11264,
        13312
      ],
      "b": [
        0
      ],
      "ttl": "2018-05-15 18:36:03 +0800",
      ...
      "prio": 8208.5186
      ...}
}

This would impact the bisection toil selection since it would try to queue highest priority toil first.

After correction, the prio is 10.0.

Signed-off-by: Xiaolong Ye <xiaolong.ye@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/stats.rb
@@ -371,6 +371,7 @@ end
 def __is_failure(stats_field)
   return false if stats_field.index('.time.')
   return false if stats_field.index('.timestamp.')
+  return false if $perf_metrics_prefixes.any? { |prefix| stats_field.index(prefix) == 0 }
   $metric_failure.each { |pattern| return true if stats_field =~ %r{^#{pattern}} }
   false
 end

etc/perf-metrics-prefixes
@@ -3,6 +3,7 @@ vmstat.io.
 vmstat.cpu.
 vmstat.system.
 vmstat.swap.
+proc-vmstat.
 perf-stat.
 boot-time.
 pmeter.

commit ff444b1db43986fd91ba527ddb959d4693e3953d

lib/stats.rb: mark ltp.*.fail as a failure stat
perf_metrics_prefixes contains "ltp.", and ".+\.(error|warn|fail).*" is in
etc/failure, the order should be corrected.

Fixes: 433f69ffcf("stats: correct extremely high bisect priority issue")
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/stats.rb
@@ -407,8 +407,9 @@ end
 def __is_failure(stats_field)
   return false if stats_field.index('.time.')
   return false if stats_field.index('.timestamp.')
-  return false if $perf_metrics_prefixes.any? { |prefix| stats_field.index(prefix) == 0 }
   $metric_failure.each { |pattern| return true if stats_field =~ %r{^#{pattern}} }
+  return false if $perf_metrics_prefixes.any? { |prefix| stats_field.index(prefix) == 0 }
+
   false
 end
 
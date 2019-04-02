ff444b1db43986fd91ba527ddb959d4693e3953d
Chen Rong <rong.a.chen@intel.com>
2019-04-02 17:52:04 +0800

    lib/stats.rb: mark ltp.*.fail as a failure stat
    perf_metrics_prefixes contains "ltp.", and ".+\.(error|warn|fail).*" is in
    etc/failure, the order should be corrected.
    
    Fixes: 433f69ffcf("stats: correct extremely high bisect priority issue")
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/stats.rb
+++ lib/stats.rb
@@ -407,8 +407,9 @@ end
 def __is_failure(stats_field)
   return false if stats_field.index('.time.')
   return false if stats_field.index('.timestamp.')
-  return false if $perf_metrics_prefixes.any? { |prefix| stats_field.index(prefix) == 0 }
   $metric_failure.each { |pattern| return true if stats_field =~ %r{^#{pattern}} }
+  return false if $perf_metrics_prefixes.any? { |prefix| stats_field.index(prefix) == 0 }
+
   false
 end
 
