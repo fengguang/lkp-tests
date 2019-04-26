0c6728972a93836269bb6f35ee0d66bb0e093361
Philip Li <philip.li@intel.com>
2019-04-26 16:21:16 +0800

    spec/filter_nr_threads_spec.rb: add :lkp flag
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/filter_nr_threads_spec.rb
+++ spec/filter_nr_threads_spec.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 require 'tmpdir'
 require "#{LKP_SRC}/lib/job"
 
-describe 'filter/nr_threads' do
+describe 'filter/nr_threads', :lkp do
   before(:all) do
     @tmp_dir = Dir.mktmpdir(nil, '/tmp')
     FileUtils.chmod 'go+rwx', @tmp_dir
@@ -14,39 +14,43 @@ describe 'filter/nr_threads' do
     FileUtils.remove_entry @tmp_dir
   end
 
-  context 'when nr_threads is defined in top level with valid value'
-  it 'does not filter the job' do
-    File.open(@test_yaml_file, 'w') do |f|
-      f.write({ 'testcase' => 'testcase', 'nr_threads' => 1 }.to_yaml)
+  context 'when nr_threads is defined in top level with valid value' do
+    it 'does not filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'nr_threads' => 1 }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      job.expand_params
     end
-    job = Job.open(@test_yaml_file)
-    job.expand_params
   end
 
-  context 'when nr_threads is defined in top level with invalid value'
-  it 'filters the job' do
-    File.open(@test_yaml_file, 'w') do |f|
-      f.write({ 'testcase' => 'testcase', 'nr_threads' => 0 }.to_yaml)
+  context 'when nr_threads is defined in top level with invalid value' do
+    it 'filters the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'nr_threads' => 0 }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      expect { job.expand_params }.to raise_error Job::ParamError
     end
-    job = Job.open(@test_yaml_file)
-    expect { job.expand_params }.to raise_error Job::ParamError
   end
 
-  context 'when nr_threads is defined in second level with valid value'
-  it 'does not filter the job' do
-    File.open(@test_yaml_file, 'w') do |f|
-      f.write({ 'testcase' => 'testcase', 'sleep' => { 'nr_threads' => 1 } }.to_yaml)
+  context 'when nr_threads is defined in second level with valid value' do
+    it 'does not filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'sleep' => { 'nr_threads' => 1 } }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      job.expand_params
     end
-    job = Job.open(@test_yaml_file)
-    job.expand_params
   end
 
-  context 'when nr_threads is defined in second level with invalid value'
-  it 'does not filter the job' do
-    File.open(@test_yaml_file, 'w') do |f|
-      f.write({ 'testcase' => 'testcase', 'sleep' => { 'nr_threads' => 0 } }.to_yaml)
+  context 'when nr_threads is defined in second level with invalid value' do
+    it 'does not filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'sleep' => { 'nr_threads' => 0 } }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      job.expand_params
     end
-    job = Job.open(@test_yaml_file)
-    job.expand_params
   end
 end
