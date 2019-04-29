70399ea6c9a58889b4a321a69c51e8c00cf219c5
Haiyan Song <haiyanx.song@intel.com>
2019-04-29 17:46:11 +0800

    spec/stats_part: add rspec test case for stats_part
    Added 3 rspec test examples to verify the feature of stats_part,
    with below 3 types of settings in job.yaml.
    
    ----------
    stats_part: from_10_to_20
    stats_part_begin: 10
    stats_part_end: 20
    
    ----------
    stats_part: remaining
    stats_part_begin: 20
    
    ----------
    stats_part: beginning
    stats_part_end: 10
    
    Signed-off-by: Haiyan Song <haiyanx.song@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/stats_part_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require "#{LKP_SRC}/lib/matrix"
+require "#{LKP_SRC}/lib/job"
+require "#{LKP_SRC}/lib/stats"
+
+describe 'Metrix/create_stats_matrix' do
+  result_root = "#{LKP_SRC}/spec/stats_part/result_root"
+  sub_dirs = Dir.glob("#{result_root}/*")
+  sub_dirs.each do |dir|
+    next if dir =~ /.json/
+
+    it "invariance: #{dir}" do
+      result_number = File.basename(dir)
+      create_stats_matrix(dir)
+      Dir.chdir(dir)
+      system('gzip -d matrix.json.gz') if File.exist?('matrix.json.gz')
+      new_stats_json =  File.read("#{dir}/stats.json")
+      new_matrix_json = File.read("#{dir}/matrix.json")
+      actual_stats_json = File.read("#{result_root}/stats_#{result_number}.json")
+      actual_matrix_json = File.read("#{result_root}/matrix_#{result_number}.json")
+      expect(new_stats_json).to eq actual_stats_json
+      expect(new_matrix_json).to eq actual_matrix_json
+    end
+  end
+end
