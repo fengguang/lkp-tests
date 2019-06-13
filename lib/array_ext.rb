3d4a80fe4579db153e2809c89c6b00c56f0ff581
Xu Chao <chaox.a.xu@intel.com>
2019-06-13 13:50:06 +0800

    lib/array_ext.rb: correct spelling errors
    Signed-off-by: Xu Chao <chaox.a.xu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/array_ext.rb
+++ lib/array_ext.rb
@@ -5,7 +5,7 @@ require 'active_support/core_ext/enumerable'
 class Array
   # multiple two arrays via multiple element with same index,
   # return the result array.
-  def pos_mulitple(an_arr)
+  def pos_multiple(an_arr)
     zip(an_arr).map { |v1, v2| v1 * v2 }
   end
 end
