93a0d11d771f6eb8dec88e643fd7e0556e3b1a0f
Ma XinJian <max.xinjian@intel.com>
2019-08-08 14:08:01 +0800

    spec: add test to check filters/need_memory for local run usage
    maxj@inn:/lkp/maxj/src$ rspec spec/filter_need_memory_spec.rb
    /lkp/maxj/src/lib/log.rb:4: warning: already initialized constant LKP_CORE_SRC
    /lkp/maxj/.src-20190807-164312/spec/spec_helper.rb:13: warning: previous definition of LKP_CORE_SRC was here
    ..rm /tmp/d20190808-64732-17ceh3j/test.yaml due to not enough memory
    need: 108544 MB, has: 107325 MB
    .
    
    Finished in 0.05203 seconds (files took 0.2781 seconds to load)
    3 examples, 0 failures
    
    maxj@lkp-boy:~/lkp-tests/spec$ rubocop filter_need_memory_spec.rb
    Inspecting 1 file
    .
    
    1 file inspected, no offenses detected
    
    Signed-off-by: Ma XinJian <max.xinjian@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/filter_need_memory_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'fileutils'
+require 'tmpdir'
+require "#{LKP_SRC}/lib/job"
+require "#{LKP_SRC}/lib/bash"
+
+system_free_mem_gb = Integer(Bash.call("free -g | sed -n '2, 1p' | awk '{print $7}'"))
+
+describe 'filters/need_memory', :lkp do
+  before(:all) do
+    @tmp_dir = Dir.mktmpdir(nil, '/tmp')
+    FileUtils.chmod 'go+rwx', @tmp_dir
+
+    @test_yaml_file = "#{@tmp_dir}/test.yaml".freeze
+  end
+
+  after(:all) do
+    FileUtils.remove_entry @tmp_dir
+  end
+
+  context 'when do not have need_memory' do
+    it 'does not filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase' }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      job.expand_params
+    end
+  end
+
+  context 'when need_memory smaller than available_memory' do
+    it 'does not filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'need_memory' => "#{system_free_mem_gb - 1}G" }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      job.expand_params
+    end
+  end
+
+  context 'when need_memory larger than available_memory' do
+    it 'filter the job' do
+      File.open(@test_yaml_file, 'w') do |f|
+        f.write({ 'testcase' => 'testcase', 'need_memory' => '100%', 'nr_cpu' => system_free_mem_gb + 2 }.to_yaml)
+      end
+      job = Job.open(@test_yaml_file)
+      expect { job.expand_params }.to raise_error Job::ParamError
+    end
+  end
+end
