fengguang/lkp-tests

commit 15459800a4de88fbe2a3603c2f0baaa70cabca35

lib/job.rb: delete old tbox keys imported from #{LKP_SRC}/include/testbox/xxx
for physical tbox, delete old tbox keys imported from #{lkp_src}/include/testbox/#{@job['old_tbox_group']}
for vm tbox, still need to delete old tbox keys imported from #{lkp_src}/include/testbox/vm

Signed-off-by: Ma XinJian <max.xinjian@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job.rb
@@ -242,14 +242,26 @@ class Job
     @jobfile = jobfile
   end
 
+  def delete_keys_from_spec(spec_file)
+    return unless File.exist? spec_file
+
+    spec_file_context = load_yaml(spec_file, nil)
+    spec_file_context.each_key { |k| @job.delete k }
+  end
+
   def delete_old_hosts_info
     return if @job['old_tbox_group'].nil?
 
     old_hosts_file = "#{lkp_src}/hosts/#{@job['old_tbox_group']}"
-    if File.exist? old_hosts_file
-      old_hwconfig = load_yaml(old_hosts_file, nil)
-      old_hwconfig.each_key { |k| @job.delete k }
-    end
+    delete_keys_from_spec(old_hosts_file)
+
+    # delele old include tbox info
+    old_include_file = "#{lkp_src}/include/testbox/#{@job['old_tbox_group']}"
+    delete_keys_from_spec(old_include_file)
+
+    # if old_tbox_group is vm, need delete keys imported from #{lkp_src}/include/testbox/vm
+    vm_include_files = "#{lkp_src}/include/testbox/vm"
+    delete_keys_from_spec(vm_include_files)
 
     @job.delete 'old_tbox_group'
   end

commit f4db3226224f3f8e34d6d305c49e00b9d949d2ad

lib/job.rb: fix "cpufreq_governor is removed during bisection"
Fixes: 15459800a4de ("lib/job.rb: delete old tbox keys imported from #{LKP_SRC}/include/testbox/xxx")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/job.rb
@@ -259,9 +259,11 @@ class Job
     old_include_file = "#{lkp_src}/include/testbox/#{@job['old_tbox_group']}"
     delete_keys_from_spec(old_include_file)
 
-    # if old_tbox_group is vm, need delete keys imported from #{lkp_src}/include/testbox/vm
-    vm_include_files = "#{lkp_src}/include/testbox/vm"
-    delete_keys_from_spec(vm_include_files)
+    # if old_tbox_group is vm, need delete keys imported from include/testbox/vm
+    if @job['old_tbox_group'] =~ /(^vm-|.+-vm-)/
+      vm_include_files = "#{lkp_src}/include/testbox/vm"
+      delete_keys_from_spec(vm_include_files)
+    end
 
     @job.delete 'old_tbox_group'
   end