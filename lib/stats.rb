0402e516b7a143786339e26d04c3fddb7a592a3f
Haiyan Song <haiyanx.song@intel.com>
2019-11-08 09:57:44 +0800

    Revert "tests: add benchmark hachbench2"
    This reverts commit 97d40a15433b5abd41740fee414b1aa1027df13c.
    
    Acked-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/stats.rb
+++ lib/stats.rb
@@ -47,7 +47,7 @@ class LinuxTestcasesTableSet
      'sysbench-memory', 'sysbench-threads', 'sysbench-mutex', 'stream',
      'perf-bench-futex', 'mutilate', 'lmbench3', 'lib-micro', 'schbench',
      'pmbench', 'linkbench', 'rocksdb', 'cassandra', 'redis', 'power_idle',
-     'mongodb', 'ycsb', 'memtier', 'mcperf', 'fio-jbod', 'hackbench2'].freeze
+     'mongodb', 'ycsb', 'memtier', 'mcperf', 'fio-jbod'].freeze
   LINUX_TESTCASES =
     ['analyze_suspend', 'boot', 'blktests', 'cpu-hotplug', 'ext4-frags', 'ftq', 'ftrace_onoff', 'fwq',
      'galileo', 'irda-kernel', 'kernel_selftests', 'kvm-unit-tests', 'kvm-unit-tests-qemu',
