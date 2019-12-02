26fdac9a67a53dec1bd89b2166073b83b6020df7
Haiyan Song <haiyanx.song@intel.com>
2019-12-02 09:42:59 +0800

    pkg/ndctl: Fix ndctl libraries loading failure
    To fix ndctl libraries loading failure and make ndctl libraries
    also can be loaded by other program that needed, make the following changes:
    1. Set prefix to '/lkp/benchmarks/' when make ndctl, and install ndctl under '/lkp/benchmarks/'.
    2. Update some place that will call ndctl, let it call from '/lkp/benchmarks/bin/ndctl'.
    
    After building, we can get pkgconfig as below:
    cat libdaxctl.pc
    ---------------------------------------------
    prefix=/lkp/benchmarks
    exec_prefix=/lkp/benchmarks
    libdir=/lkp/benchmarks/lib
    includedir=/lkp/benchmarks/include
    
    Name: libdaxctl
    Description: Manage "Device DAX" devices
    Version: 67.10.g93ce38e
    Libs: -L${libdir} -ldaxctl
    Libs.private:
    Cflags: -I${includedir}
    ---------------------------------------------
    
    Signed-off-by: Haiyan Song <haiyanx.song@intel.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/nvdimm.sh
+++ lib/nvdimm.sh
@@ -1,6 +1,5 @@
 # nvdimm functions
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lkp/benchmarks/ndctl/lib/
 configure_nvdimm()
 {
 	for ns in $(ls -d /sys/bus/nd/devices/namespace*); do
@@ -11,7 +10,7 @@ configure_nvdimm()
 		rmode=$(echo -n $rmode)
 		mode=$(echo -n $mode)
 		[ "$rmode" = "$mode" ] && continue
-		/lkp/benchmarks/ndctl/bin/ndctl create-namespace --reconfig=$bns \
+		/lkp/benchmarks/bin/ndctl create-namespace --reconfig=$bns \
 						--force --mode="$mode" || exit 1
 	done
 }
