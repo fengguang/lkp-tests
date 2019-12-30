282d5fd2b71e27f5e1eeca7f8199fb6cd907324c
Ma XinJian <max.xinjian@intel.com>
2019-12-30 12:35:37 +0800

    rename leaking_addresses to leaking-addresses
    Fix the following issue
    ------
    dpkg-deb: error: package name has characters that aren't lowercase alphanums or '-+.'
    ------
    Signed-off-by: Ma XinJian <max.xinjian@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/stats.rb
+++ lib/stats.rb
@@ -52,7 +52,7 @@ class LinuxTestcasesTableSet
   LINUX_TESTCASES =
     ['analyze-suspend', 'boot', 'blktests', 'cpu-hotplug', 'ext4-frags', 'ftq', 'ftrace_onoff', 'fwq',
      'galileo', 'irda-kernel', 'kernel_selftests', 'kvm-unit-tests', 'kvm-unit-tests-qemu',
-     'leaking_addresses', 'locktorture', 'ltp', 'mce-test', 'otc_ddt', 'piglit', 'pm-qa', 'nvml',
+     'leaking-addresses', 'locktorture', 'ltp', 'mce-test', 'otc_ddt', 'piglit', 'pm-qa', 'nvml',
      'qemu', 'rcuperf', 'rcutorture', 'suspend', 'suspend_stress', 'trinity', 'ndctl', 'nfs-test', 'hwsim',
      'idle-inject', 'mdadm-selftests', 'xsave-test', 'nvml', 'test_bpf', 'mce-log', 'perf-sanity-tests',
      'build-perf_test', 'update-ucode', 'reboot', 'cat', 'libhugetlbfs-test', 'ocfs2test', 'syzkaller',
