bc130825721491a74acb1d4b3b41df4d1b1e874a
Philip Li <philip.li@intel.com>
2020-02-18 12:11:31 +0800

    lib/tests/common.sh: add double quotes to eval
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/common.sh
+++ lib/tests/common.sh
@@ -13,8 +13,8 @@ get_ignored_and_worked_tests()
 		ignored_tests_cmd=${all_tests_cmd}" | grep -F -f merged_ignored_files"
 		all_tests_cmd=${all_tests_cmd}" | grep -v -F -f merged_ignored_files"
 
-		ignored_tests=$(eval $ignored_tests_cmd)
+		ignored_tests=$(eval "$ignored_tests_cmd")
 	}
-	all_tests=$(eval $all_tests_cmd)
+	all_tests=$(eval "$all_tests_cmd")
 	[ -f merged_ignored_files ] && rm merged_ignored_files > /dev/null 2>&1
 }
