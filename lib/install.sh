fengguang/lkp-tests

commit 18862c32c8fcca8f5ce977f88bf789b9c588b30f

lib/install.sh: fix "bash: btrfs-progs: command not found"
before rt:
carel@inn:/result/makepkg-deps/fxmark/lkp-opteron1/debian-x86_64-2019-11-14.cgz/x86_64-rhel-7.6/gcc-7/c79f46a282390e0f5b306007bf7b11a46d529538/0$ cat stderr
bash: btrfs-progs: command not found
Failed to apt-get install btrfs-tools|btrfs-progs f2fs-tools jfsutils nilfs-tools python3 reiserfsprogs sudo xfsprogs
/lkp/carel/src/lib/debug.sh:12: die
/lkp/carel/src/distro/debian:134: resolve_depends
/lkp/carel/src/distro/debian:242: pack_benchmark_deps
/lkp/carel/src/tests/makepkg-deps:45: main
:0:

after rt:
carel@inn:/result/makepkg-deps/fxmark/lkp-opteron1/debian-x86_64-2019-11-14.cgz/x86_64-rhel-7.6/gcc-7/b3a987b0264d3ddbb24293ebff10eddfc472f653/1$ cat makepkg-deps.json
{
    "makepkg-deps.pass": [
        1
    ]
}

Signed-off-by: Carel Si <beibei.si@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/install.sh
@@ -63,6 +63,7 @@ adapt_packages()
 		fi
 		if [ -n "$mapping" ]; then
 			distro_pkg=$(echo $mapping | awk -F": " '{print $2}')
+			diftro_pkg="^($distro_pkg)$"
 			if [ -n "$distro_pkg" ]; then
 				echo $distro_pkg
 			else

commit 5ff59209d5511ef2a2e412cbf79e5ac0391ae77d

lib/install.sh: fix a typo: "diftro_pkg" should be "distro_pkg"
Fixes: 18862c32c8fc ("lib/install.sh: fix "bash: btrfs-progs: command not found"")
Signed-off-by: Carel Si <beibei.si@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/install.sh
@@ -67,7 +67,7 @@ adapt_packages()
 		fi
 		if [ -n "$mapping" ]; then
 			distro_pkg=$(echo $mapping | awk -F": " '{print $2}')
-			diftro_pkg="^($distro_pkg)$"
+			distro_pkg="^($distro_pkg)$"
 			if [ -n "$distro_pkg" ]; then
 				echo $distro_pkg
 			else
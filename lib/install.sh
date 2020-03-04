5ff59209d5511ef2a2e412cbf79e5ac0391ae77d
Carel Si <beibei.si@intel.com>
2020-03-04 08:31:49 +0800

    lib/install.sh: fix a typo: "diftro_pkg" should be "distro_pkg"
    Fixes: 18862c32c8fc ("lib/install.sh: fix "bash: btrfs-progs: command not found"")
    Signed-off-by: Carel Si <beibei.si@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/install.sh
+++ lib/install.sh
@@ -67,7 +67,7 @@ adapt_packages()
 		fi
 		if [ -n "$mapping" ]; then
 			distro_pkg=$(echo $mapping | awk -F": " '{print $2}')
-			diftro_pkg="^($distro_pkg)$"
+			distro_pkg="^($distro_pkg)$"
 			if [ -n "$distro_pkg" ]; then
 				echo $distro_pkg
 			else
