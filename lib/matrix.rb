7ff05f00a0331c0a9209d38aac03bb2c722ee6a6
Liu Yiding <yidingx.liu@intel.com>
2020-04-14 09:58:02 +0800

    Revert "lib/matrix.rb: add debug codes to output nil json file"
    This reverts commit 62bfd1b2dbbc64b3939d70825022e713c041c7e1.
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -121,11 +121,7 @@ def create_stats_matrix(result_root)
     end
 
     monitor_stats = load_json file
-    if monitor_stats
-      sample_size = max_cols(monitor_stats)
-    else
-      log_warn "failed to load json: #{file}"
-    end
+    sample_size = max_cols(monitor_stats)
 
     i_stats_part_begin = 0
     stats_part_len = sample_size
