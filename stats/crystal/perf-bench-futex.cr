f742b25364323698581abbae792744ed4187b48f
Cao xueliang <caoxueliang2@huawei.com>
2020-05-08 17:20:15 +0800

    done ruby2crystal-stats-15.md

--- stats/crystal/perf-bench-futex.cr
+++ stats/crystal/perf-bench-futex.cr
@@ -1,6 +1,6 @@
 #!/usr/bin/env crystal
 
-$stdin.each_line do |line|
+STDIN.each_line do |line|
   if line =~ /Averaged ([\d.]+) operations\/sec \(\+- ([\d.]+)%\)/
     puts "ops/s: #{$1}"
     puts "stddev%: #{$2}"
