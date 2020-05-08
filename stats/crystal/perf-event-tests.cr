d30df88925a0fb259004b95859826e3b0fccfcbe
WuZhende <wuzhende1@huawei.com>
2020-05-08 18:02:19 +0800

    fix hash nil key

--- stats/crystal/perf-event-tests.cr
+++ stats/crystal/perf-event-tests.cr
@@ -1,13 +1,13 @@
 #!/usr/bin/env crystal
 
-stats = Hash(String|Nil, String).new
+stats = Hash(String, String).new
 item = nil
 while (line = STDIN.gets)
   case line
   when /^[ ]+\+ \S+\/(\S+)$/
     item = $1
   when /\.\.\.[ ]+([\S\s]+)$/
-    stats[item] = $1.strip.to_s.tr(" ", "_").downcase
+    stats[item] = $1.strip.to_s.tr(" ", "_").downcase if item
   when /^(ignored_by_lkp)\s+(.*)\s+/
     stats[$2] = $1
   end
