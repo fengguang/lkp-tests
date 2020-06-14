ff39cd2797410c53466df07f38480356e954ee79
Carel Si <beibei.si@intel.com>
2020-06-14 10:42:34 -0400

    cleanup rubocop issue Layout/Tab
    $ bundle exec rubocop --auto-correct --only Layout/Tab
    Inspecting 253 files
    .........................................................................................C...................................................................................................................................................................
    
    Offenses:
    
    spec/result_path_spec.rb:344:1: C: [Corrected] Layout/Tab: Tab detected.
            #The commit name is kernel version in local run.
    ^
    spec/result_path_spec.rb:345:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path.parse_result_root("#{RESULT_ROOT_DIR}/will-it-scale/process-100%-brk1/shao2-debian/debian/x86_64-rhel-7.6/gcc-7/4.19.0-4-amd64/0", true)).to be true
    ^
    spec/result_path_spec.rb:346:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['testcase']).to eq 'will-it-scale'
    ^
    spec/result_path_spec.rb:347:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['path_params']).to eq 'process-100%-brk1'
    ^
    spec/result_path_spec.rb:348:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['ucode']).to eq nil
    ^
    spec/result_path_spec.rb:349:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['tbox_group']).to eq 'shao2-debian'
    ^
    spec/result_path_spec.rb:350:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['rootfs']).to eq 'debian'
    ^
    spec/result_path_spec.rb:351:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['kconfig']).to eq 'x86_64-rhel-7.6'
    ^
    spec/result_path_spec.rb:352:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['compiler']).to eq 'gcc-7'
    ^
    spec/result_path_spec.rb:353:1: C: [Corrected] Layout/Tab: Tab detected.
            expect(result_path['commit']).to eq '4.19.0-4-amd64'
    ^
    
    253 files inspected, 10 offenses detected, 10 offenses corrected
    
    Signed-off-by: Carel Si <beibei.si@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/result_path_spec.rb
+++ spec/result_path_spec.rb
@@ -341,16 +341,16 @@ describe ResultPath do
       it 'do not check commit id' do
         result_path = described_class.new
 
-	#The commit name is kernel version in local run.
-	expect(result_path.parse_result_root("#{RESULT_ROOT_DIR}/will-it-scale/process-100%-brk1/shao2-debian/debian/x86_64-rhel-7.6/gcc-7/4.19.0-4-amd64/0", true)).to be true
-	expect(result_path['testcase']).to eq 'will-it-scale'
-	expect(result_path['path_params']).to eq 'process-100%-brk1'
-	expect(result_path['ucode']).to eq nil
-	expect(result_path['tbox_group']).to eq 'shao2-debian'
-	expect(result_path['rootfs']).to eq 'debian'
-	expect(result_path['kconfig']).to eq 'x86_64-rhel-7.6'
-	expect(result_path['compiler']).to eq 'gcc-7'
-	expect(result_path['commit']).to eq '4.19.0-4-amd64'
+  #The commit name is kernel version in local run.
+  expect(result_path.parse_result_root("#{RESULT_ROOT_DIR}/will-it-scale/process-100%-brk1/shao2-debian/debian/x86_64-rhel-7.6/gcc-7/4.19.0-4-amd64/0", true)).to be true
+  expect(result_path['testcase']).to eq 'will-it-scale'
+  expect(result_path['path_params']).to eq 'process-100%-brk1'
+  expect(result_path['ucode']).to eq nil
+  expect(result_path['tbox_group']).to eq 'shao2-debian'
+  expect(result_path['rootfs']).to eq 'debian'
+  expect(result_path['kconfig']).to eq 'x86_64-rhel-7.6'
+  expect(result_path['compiler']).to eq 'gcc-7'
+  expect(result_path['commit']).to eq '4.19.0-4-amd64'
       end
     end
   end
