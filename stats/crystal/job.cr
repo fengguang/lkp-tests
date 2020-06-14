dba7d0e6432f5f377c8d4831727ed5cdd9ac7661
chief <tongqunfeng@huawei.com>
2020-06-15 06:15:59 +0800

    add job2sh function
    the result is same to lkp-tests/sbin/job2sh with jobs/boot.yaml
    and no ameba warning.

---
+++ stats/crystal/job.cr
@@ -0,0 +1,115 @@
+require "yaml"
+
+LKP_SRC = ENV["LKP_SRC"]? || "/c/lkp-tests"
+
+class Job
+    class ParamError < ArgumentError
+    end
+end
+
+class Job
+    def initialize
+        @jobs =Array(YAML::Any).new
+        @job = Hash(YAML::Any, YAML::Any).new
+        @jobx = Hash(YAML::Any, YAML::Any).new
+        @jobfile = String.new
+
+        @available_programs = Hash(Symbol, Hash(String, String)).new
+        @programs_cache = Hash(String, Hash(String, String)).new
+        @programs = Hash(String, String).new
+    end
+
+    def load(jobfile, expand_template = false)
+        yaml = File.read jobfile
+        ArgumentError.new("empty jobfile #{jobfile}") if yaml.empty?
+        @jobs = YAML.parse_all(yaml)
+
+        # if can not find @jobs.first["job_origin"]
+        # no need <try symlink? or Add a symbol :#! filename>
+        jobs_hash = @jobs.shift.as_h
+        @job.merge!(jobs_hash)
+        if @job["job_origin"]? == nil
+          @job.merge!( YAML.parse("job_origin: #{jobfile}").as_h)
+        end
+        @jobfile = jobfile
+    end
+
+    def lkp_src
+        if @job["user"]? && Dir.exists?("/lkp/" + @job["user"].to_s + "/src")
+            "/lkp/" + @job["user"].to_s + "/src"
+        else
+            LKP_SRC
+        end
+    end
+
+    def expand_params(run_scripts = true)
+    end
+
+    def atomic_job?
+        true
+    end
+
+    def __create_programs_hash(glob, lkp_src)
+        programs = Hash(String, String).new
+
+        Dir.glob("#{lkp_src}/#{glob}").each do |path|
+            next if File.directory?(path)
+            next if path =~ /\.yaml$/
+            next if path =~ /\.[0-9]+$/
+
+            unless File.executable?(path)
+                puts "skip non-executable #{path}"
+                next
+            end
+
+            file = File.basename(path)
+            next if file == "wrapper"
+
+            if programs.includes? file
+                puts "Conflict names #{programs[file]} and #{path}"
+                next
+            end
+
+            programs[file] = path
+        end
+
+        programs
+    end
+
+    def create_programs_hash(glob, lkp_src = LKP_SRC)
+        cache_key = [glob, lkp_src].join ':'
+        if @programs_cache[cache_key]? == nil
+            @programs_cache[cache_key] = __create_programs_hash(glob, lkp_src)
+        end
+        @programs = @programs_cache[cache_key]
+    end
+
+    def available_programs(type)
+        case type
+        when Array
+          p = Hash(String, String).new
+          type.each do |t|
+            p.merge! available_programs(t)
+          end
+          return p
+        when :workload_and_monitors
+          # This is all scripts that run in testbox.
+          # The other stats/* and filters/* run in server.
+          if @available_programs[:workload_and_monitors]? == nil
+            @available_programs[:workload_and_monitors] = available_programs(%i(workload_elements monitors))
+          end
+          return @available_programs[:workload_and_monitors]
+        when :workload_elements
+          # the options of these programs could impact test result
+          if @available_programs[:workload_elements]? == nil
+            @available_programs[:workload_elements] = available_programs(%i(setup tests daemon))
+          end
+          return @available_programs[:workload_elements]
+        else
+            if @available_programs[type]? == nil
+              @available_programs[type] = create_programs_hash("#{type}/**/*", lkp_src)
+            end
+            return @available_programs[type]
+        end
+    end
+end
---
+++ stats/crystal/job2sh.cr
@@ -0,0 +1,48 @@
+require "option_parser"
+
+require "./job2sh_inn"
+require "../../lib/log"
+
+destination = ""
+output = STDOUT
+
+OptionParser.parse do |parser|
+    parser.banner = "Usage: #{PROGRAM_NAME} [options] job.yaml"
+    parser.on("-o FILE", "--output FILE", "save shell script to FILE (default: stdout)") { |name| destination = name }
+    parser.on("-h", "--help", "Show this message") do
+        puts parser
+        exit
+    end
+    parser.invalid_option do |flag|
+        STDERR.puts "ERROR: #{flag} is not a valid option."
+        STDERR.puts parser
+        exit(1)
+    end
+end
+
+if destination != ""
+    output = File.open(destination, "w+")
+end
+
+begin
+    job = Job2sh.new
+    job.load(ARGV[0])
+    job.expand_params
+
+    unless job.atomic_job?
+      log_error "Looks #{ARGV[0]} isn't a atomic jobfile, only atomic jobfile is supported"
+      log_error "Please run 'lkp split-job #{ARGV[0]}' first"
+      exit(1)
+    end
+  rescue e : Job::ParamError
+    log_error "Abandon job: #{e.message}"
+    exit(1)
+  rescue e
+    log_error "#{e.message}"
+    exit(1)
+end
+
+output.puts job.to_shell
+if destination != ""
+    output.close
+end
