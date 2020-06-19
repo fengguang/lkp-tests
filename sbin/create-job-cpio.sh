69dfcdc7f214b96b42998b4a8150ac7815353bb0
Wu Fengguang <wufengguang@huawei.com>
2020-06-19 11:11:55 +0800

    create-job-cpio.sh: refactor code
    Signed-off-by: Wu Fengguang <wufengguang@huawei.com>

--- sbin/create-job-cpio.sh
+++ sbin/create-job-cpio.sh
@@ -4,17 +4,18 @@
 
 job_yaml=$1
 job_name=${job_yaml##*/}
-out_cgz=$(dirname $(realpath $job_yaml))/${job_name%.yaml}.cgz
+job_dir=$(dirname $(realpath $job_yaml))
+out_cgz=$job_dir/${job_name%.yaml}.cgz
 
-tmp_dir=/tmp/$$
+tmp_dir=$job_dir/.tmp
 lkp_dir=$tmp_dir/lkp/scheduled
 
 mkdir -p $lkp_dir
-cp $job_yaml $lkp_dir
+cp -l $job_yaml $lkp_dir
 $LKP_SRC/sbin/job2sh $job_yaml -o $lkp_dir/${job_name%.yaml}.sh
 
-cp $lkp_dir/${job_name%.yaml}.sh $(dirname $(realpath $job_yaml))/
 cd $tmp_dir
 find lkp | cpio --quiet -o -H newc | gzip > $out_cgz
 
+mv $lkp_dir/${job_name%.yaml}.sh $job_dir/
 rm -fr $tmp_dir
