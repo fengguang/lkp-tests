e1ae85ffe88e4e3045cbe22ec1a42a357b6c5f21
Sun Yukui <sunyukui@huawei.com>
2020-06-24 17:37:38 +0800

    review ruby-to-crystal

--- stats/crystal/stack.cr
+++ stats/crystal/stack.cr
@@ -4,40 +4,39 @@ MAX_STACK_DEPTH = 5
 THRESHOLD = 0.01
 
 def parse
-  results = {} of String => Int32
-  cmd = Nil
-  funcs = [] of String 
+  results = {} of String=>Hash(Float64,Float64)
+  cmd = "0"
+  funcs = [] of String
 
-  add_result = lambda do
+  add_result = ->{
     unless funcs.empty?
-      funcs = funcs.slice(0, MAX_STACK_DEPTH)
-      results[cmd] ||= Hash.new(0)
-      results[cmd][funcs.join(".")] += 1
+      funcs = funcs.first(MAX_STACK_DEPTH)
+      results[cmd] ||= Hash(Float64,Float64).new(0)
+      results[cmd][funcs.join(".").to_f] += 1_f64
       funcs = [] of String
     end
-  end
-
-  @@class_stdin.each_line do |line|
-    if line.start_with? "/proc/"
-      add_result.call
-      cmd = line.split[1].split("/")[0]
-    elsif line =~ /\[<[0-9a-f]+>\] ([a-zA-Z_][a-zA-Z0-9_]+)/
-      funcs.push $1
+  }
+    STDIN.each_line do |line|
+      if line.starts_with?("/proc/")
+        add_result.call
+        cmd = line.split[1].split("/")[0]
+      elsif line =~ /\[<[0-9a-f]+>\] ([a-zA-Z_][a-zA-Z0-9_]+)/
+        funcs.push $1
+      end
     end
-  end
-  add_result.call
+    add_result.call
 
   results.each do |c, cmd_result|
     nr = cmd_result.reduce(0) { |acc, elem| acc + elem[1] }
-    results[c] = cmd_result.map { |bt, val| [bt, val * 100.0 / nr] }
+    results[c] = cmd_result.map { |bt, val| [bt, val * 100.0 / nr] }.to_h
   end
   results
 end
 
 def display(results)
   results.each do |cmd, cmd_result|
-    cmd_result.sort_by! { |r| r[1] }
-    cmd_result.reverse_each do |bt, p|
+    cmd_result.to_a.sort_by! { |r| r[1] }
+    cmd_result.to_a.reverse_each do |bt, p|
       break if p < THRESHOLD
 
       puts "#{cmd}.#{bt}: #{p}"
