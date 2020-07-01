40c19613c9e9a3cf86da5498526eb334ad4afdb4
Lin Jiaxin <linjiaxin5@huawei.com>
2020-07-01 17:02:21 +0800

    fio.cr: fix compile errors
    stats/crystal/fio.cr  fix compile errors

--- stats/crystal/fio.cr
+++ stats/crystal/fio.cr
@@ -6,16 +6,16 @@ require "json"
 require "../../lib/log"
 
 workload = 0
-OPS = %w(read write).freeze
-PERCENTILES = %w(90.000000 95.000000 99.000000).freeze
+OPS = %w(read write)
+PERCENTILES = %w(90.000000 95.000000 99.000000)
 
 if ENV["RESULT_ROOT"]
-  fn = File.join ENV["RESULT_ROOT"], "fio.output"
+  fn = File.join(ENV["RESULT_ROOT"], "fio.output")
 elsif ARGV[0]
   fn = ARGV[0]
 end
 
-contents = File.read(fn)
+contents = File.read(fn.to_s)
 idx = contents.index("{")
 unless idx
   log_error "#{fn}: maybe it is not a json format file"
@@ -27,12 +27,12 @@ unless res
   log_error "Invalid/empty fio output"
   exit
 end
-res_job = res["jobs"].first
+res_job = res["jobs"][0]
 OPS.each do |ops|
   res = res_job[ops]
-  puts "#{ops}_bw_MBps: #{res["bw"].to_f / 1024}"
+  puts "#{ops}_bw_MBps: #{res["bw"].as_f / 1024}"
   puts "#{ops}_iops: #{res["iops"]}"
-  workload += res["total_ios"].to_i
+  workload += res["total_ios"].as_i
   res_clat = res["clat"] || res["clat_ns"]
   res_clat_percentiles = res_clat["percentile"]
   puts "#{ops}_clat_mean_us: #{res_clat["mean"]}"
@@ -40,8 +40,10 @@ OPS.each do |ops|
   if res_clat_percentiles
     PERCENTILES.each do |p|
       sp = p.chomp("0")
-      true while sp.chomp!("0")
-      sp.chomp!(".")
+      while sp[-1] == '0'
+        sp = sp.chomp("0")
+      end
+      sp = sp.chomp(".")
       puts "#{ops}_clat_#{sp}%_us: #{res_clat_percentiles[p]}"
     end
   end
@@ -50,12 +52,12 @@ OPS.each do |ops|
   puts "#{ops}_slat_stddev: #{res_slat["stddev"]}"
 end
 
-res_latency_us = res_job["latency_us"]
+res_latency_us = res_job["latency_us"].as_h
 res_latency_us.each do |k, v|
   puts "latency_#{k}us%: #{v}"
 end
 
-res_latency_ms = res_job["latency_ms"]
+res_latency_ms = res_job["latency_ms"].as_h
 res_latency_ms.each do |k, v|
   puts "latency_#{k}ms%: #{v}"
 end
