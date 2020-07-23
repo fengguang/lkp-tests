ea92a33db7bb2062c38d59c8fca73052cf8c4d23
Philip Li <philip.li@intel.com>
2020-07-23 15:29:51 +0800

    lib/matrix.rb: "in `unite_to': undefined method `index' for nil:NilClass"
    matrix file can exist but is empty with content like {}
    sometimes, which leads to the failure
    
    lib/matrix.rb:245:in `unite_to': undefined method `index' for nil:NilClass (NoMethodError)
    	from /lkp-src/lib/matrix.rb:450:in `unite_stats'
    	from /lkp-src/lib/unite.rb:881:in `unite_result'
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -241,7 +241,7 @@ def unite_to(stats, matrix_root, max_cols = nil, delete = false)
   matrix = load_matrix_file(matrix_root + '/matrix.json')
   matrix ||= load_matrix_file(matrix_root + '/matrix.yaml')
 
-  if matrix
+  if matrix && matrix[STATS_SOURCE_KEY]
     dup_col = matrix[STATS_SOURCE_KEY].index stats[STATS_SOURCE_KEY]
     matrix_delete_col(matrix, dup_col) if dup_col
   else
