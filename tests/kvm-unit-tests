kvm-unit-tests/kvm-unit-tests

commit 1a31ffab16864b9aa1806bae8fd483555f1b989f

Fix out-of-tree builds
Since b16df9ee5f3b out-of-tree builds have been broken because we
started validating the newly user-configurable $erratatxt file
before linking it into the build dir. We fix this not by moving
the validation, but by removing the linking and instead using the
full path of the $erratatxt file. This allows one to keep that file
separate from the src and build dirs.

Fixes: b16df9ee5f3b ("arch-run: Add reserved variables to the default environ")
Reported-by: Thomas Huth <thuth@redhat.com>
Signed-off-by: Andrew Jones <drjones@redhat.com>
Message-Id: <20200511070641.23492-1-drjones@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

configure
@@ -17,7 +17,7 @@ environ_default=yes
 u32_long=
 vmm="qemu"
 errata_force=0
-erratatxt="errata.txt"
+erratatxt="$srcdir/errata.txt"
 
 usage() {
     cat <<-EOF
@@ -89,7 +89,8 @@ while [[ "$1" = -* ]]; do
 	    environ_default=no
 	    ;;
 	--erratatxt)
-	    erratatxt="$arg"
+	    erratatxt=
+	    [ "$arg" ] && erratatxt=$(eval realpath "$arg")
 	    ;;
 	--help)
 	    usage
@@ -169,9 +170,6 @@ if test ! -e Makefile; then
 
     echo "linking scripts..."
     ln -sf "$srcdir/scripts"
-
-    echo "linking errata.txt..."
-    ln -sf "$srcdir/errata.txt"
 fi
 
 # link lib/asm for the architecture

fengguang/lkp-tests

commit 97cef1d81d11b1b0ac7b9a49c400fc4ff30bfca1

tests/kvm-unit-tests: fix "errata.txt not found"
the error was caused by 1a31ffab16864b9aa1806bae8fd483555f1b989f
in kvm-unit-tests/, it uses "$srcdir/errata.txt" to set ERRATATXT
(errata.txt's path), so when we pack kvm-unit-tests, ERRATATXT will
be set as "/tmp/lkp/kvm-unit-tests/src/kvm-unit-tests/errata.txt",
but this tmp/ dir won't exists when we run kvm-unit-tests jobs
(more specifically, when we run "./run_tests.sh"), thus it will output
"/tmp/lkp/kvm-unit-tests/src/kvm-unit-tests/errata.txt not found" error.

for the solution, since this error only happened in kvm-unit-tests,
other test suites seam correct now, and errata.txt& run_tests.sh are
under same directory, we choose to only change kvm-related code by
directly use relative path (errata.txt).

before:
/result/kvm-unit-tests/ucode=0x28/lkp-hsw-d02/debian-10.4-x86_64-20200603.cgz/x86_64-rhel-8.3/gcc-9/dcb7fd82c75ee2d6e6f9d8cc71c52519ed52e258/11$ cat kvm-unit-tests
2020-07-24 10:14:48 ./run_tests.sh
SKIP apic-split (/tmp/lkp/kvm-unit-tests/src/kvm-unit-tests/errata.txt not found. (ERRATATXT=/tmp/lkp/kvm-unit-tests/src/kvm-unit-tests/errata.txt))

after:
/result/kvm-unit-tests/ucode=0x28/lkp-hsw-d02/debian-10.4-x86_64-20200603.cgz/x86_64-rhel-8.3/gcc-9/dcb7fd82c75ee2d6e6f9d8cc71c52519ed52e258/17$ cat kvm-unit-tests.json
{
  "kvm-unit-tests.apic-split.pass": [
    1
  ],
  "kvm-unit-tests.ioapic-split.pass": [
    1
  ],
  "kvm-unit-tests.apic.pass": [
    1
  ],
  "kvm-unit-tests.ioapic.pass": [
    1
  ],
  "kvm-unit-tests.cmpxchg8b.skip": [
    1
  ],
  "kvm-unit-tests.smptest.pass": [
    1
  ],
  "kvm-unit-tests.smptest3.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_cpuid.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_vmcall.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_mov_from_cr8.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_mov_to_cr8.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_inl_pmtimer.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_ipi.pass": [
    1
  ],
  "kvm-unit-tests.vmexit_ipi_halt.pass": [

tests/kvm-unit-tests
@@ -27,6 +27,11 @@ else
 	fi
 fi
 
+# errata.txt and run_tests.sh are under same directory, using
+# absolute path for errata.sh will increase the risk of missing
+# file, here we directly use relative path.
+sed -i 's/\(ERRATATXT=\).*/\1errata.txt/' config.mak
+
 setup_test_environment || die "failed to setup test environment"
 
 check_ignored_cases