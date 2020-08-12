06a2e62c7e668d18984d2fc0e043ad3fe206bc7c
Lu Kaiyi <lukaiyi@huawei.com>
2020-08-12 11:25:08 +0800

    stats/crystal: kmemleak to kmemleak.cr
    1. all singe-quotes to double-quotes
    2. $PROGRAM_NAME to __FILE__
    3. line.chomp! to line
    4. add type declaration
      from
        memleaks = []
        memleak = {}
      to
        memleaks = [] of Hash(String, String)
        memleak = {} of String => String
    
    Signed-off-by: Lu Kaiyi <lukaiyi@huawei.com>

--- stats/crystal/kmemleak.cr
+++ stats/crystal/kmemleak.cr
@@ -1,23 +1,23 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env crystal
 
-LKP_SRC = ENV['LKP_SRC'] || File.dirname(File.dirname(File.realpath($PROGRAM_NAME)))
-RESULT_ROOT = ENV['RESULT_ROOT']
+LKP_SRC = ENV["LKP_SRC"] || File.dirname(File.dirname(File.realpath(__FILE__)))
+RESULT_ROOT = ENV["RESULT_ROOT"]
 
-memleaks = []
-memleak = {}
+memleaks = [] of Hash(String, String)
+memleak = {} of String => String
 bts = ""
 
 while (line = STDIN.gets)
-  case line.chomp!
+  case line
   when /^unreferenced object/ # unreferenced object 0xffff9375a9b84e00 (size 128):
-    if !bts.empty? && memleak['type']
-      memleak['bt'] = bts
+    if !bts.empty? && memleak["type"]
+      memleak["bt"] = bts
       memleaks << memleak
     end
-    bts = ''
-    memleak['type'] = 'unreferenced_object'
+    bts = ""
+    memleak["type"] = "unreferenced_object"
   when /comm "(.*)"/ # comm "swapper/0", pid 1, jiffies 4294667990 (age 37.508s)
-    memleak['comm'] = $1
+    memleak["comm"] = $1
 #    [<(____ptrval____)>] unpack_to_rootfs+0x3d/0x304
 #    [<(____ptrval____)>] populate_rootfs+0x19/0x106
   when /\[.*\] (.*)\+/
@@ -25,9 +25,9 @@ while (line = STDIN.gets)
   end
 end
 
-if !bts.empty? && memleak['type']
-  memleak['bt'] = bts
+if !bts.empty? && memleak["type"]
+  memleak["bt"] = bts
   memleaks << memleak
 end
 
-memleaks.uniq.each { |m| puts "#{m['comm']}.#{m['type']}#{m['bt']}: 1" }
+memleaks.uniq.each { |m| puts "#{m["comm"]}.#{m["type"]}#{m["bt"]}: 1" }
