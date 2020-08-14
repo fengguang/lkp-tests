868dede315e18dc49269cc8c15b4e9e367fe505a
Lu Kaiyi <lukaiyi@huawei.com>
2020-08-14 11:44:47 +0800

    stats/crystal: kunit to kunit.cr
    1. all single-quotes to double-quotes
    2. type declaration
       from
         stats = {}
       to
         stats = {} of String => String
    
    Signed-off-by: Lu Kaiyi <lukaiyi@huawei.com>

--- stats/crystal/kunit.cr
+++ stats/crystal/kunit.cr
@@ -1,17 +1,16 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env crystal
 
-stats = {}
+stats = {} of String => String
 
 while (line = STDIN.gets)
-  line = line.remediate_invalid_byte_sequence(replace: '_') unless line.valid_encoding?
   case line
   when /\d*(not ok|ok).* - (.*)/
-    result = $1.strip.tr(' ', '_')
-    test = $2.strip.tr(' ', '_')
+    result = $1.strip.tr(" ", "_")
+    test = $2.strip.tr(" ", "_")
     stats[test] = result
   end
 end
 
 stats.each do |k, v|
-  puts k + '.' + v + ': 1'
+  puts k + "." + v + ": 1"
 end
