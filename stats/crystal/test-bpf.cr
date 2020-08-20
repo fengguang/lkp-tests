aab3a7bf1499e1724c73b1ce8da4dcfcbfdcd21d
Lu Kaiyi <lukaiyi@huawei.com>
2020-08-20 19:04:40 +0800

    stats/crystal: test-bpf to test-bpf.cr
    fix syntax problem in line with crystal
    
    1. single-quotes to double-quotes
    2. $PROGRAM_NAME to PROGRAM_NAME
    3. stats = [] to stats = [] of String
    4. add type = "" before using
    
    Signed-off-by: Lu Kaiyi <lukaiyi@huawei.com>

--- stats/crystal/test-bpf.cr
+++ stats/crystal/test-bpf.cr
@@ -1,46 +1,45 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env crystal
 
-LKP_SRC = ENV['LKP_SRC'] || File.dirname(File.dirname(File.reapath($PROGRAM_NAME)))
+LKP_SRC = ENV["LKP_SRC"] || File.dirname(File.dirname(File.real_path(PROGRAM_NAME)))
 
-require "#{LKP_SRC}/lib/string_ext"
+require "../../lib/string_ext"
 
-stats = []
+stats = [] of String
 success_test = 0
 fail_test = 0
 is_divided = false
 
 while (line = STDIN.gets)
-  line = line.remediate_invalid_byte_sequence(replace: '_') unless line.valid_encoding?
-
   case line
   when /test_bpf: #[0-9]+ (.+) jited:.+ PASS$/
-    type = $1.tr(' ', '_')
-    stats << type + '.pass: 1'
+    type = $1.tr(" ", "_")
+    stats << type + ".pass: 1"
   when /test_bpf: #[0-9]+ (.+) jited:.+ FAIL/
-    type = $1.tr(' ', '_')
-    stats << type + '.fail: 1'
+    type = $1.tr(" ", "_")
+    stats << type + ".fail: 1"
   when /test_bpf: #[0-9]+ check: (.+) PASS$/
-    type = $1.tr(' ', '_')
-    stats << 'check:_' + type + '.pass: 1'
+    type = $1.tr(" ", "_")
+    stats << "check:_" + type + ".pass: 1"
   when /test_bpf: #[0-9]+ check: (.+) FAIL/
-    type = $1.tr(' ', '_')
-    stats << 'check:_' + type + '.fail: 1'
+    type = $1.tr(" ", "_")
+    stats << "check:_" + type + ".fail: 1"
   when /test_bpf: #[0-9]+ (.+) PASS$/
-    type = $1.tr(' ', '_')
-    stats << type + '.pass: 1'
+    type = $1.tr(" ", "_")
+    stats << type + ".pass: 1"
   when /test_bpf: #[0-9]+ (.+) FAIL/
-    type = $1.tr(' ', '_')
-    stats << type + '.fail: 1'
+    type = $1.tr(" ", "_")
+    stats << type + ".fail: 1"
   when /test_bpf: #[0-9]+ check: (.+)/
-    type = 'check:_' + $1.strip.tr(' ', '_')
+    type = "check:_" + $1.strip.tr(" ", "_")
     is_divided = true
   when /test_bpf: #[0-9]+ (.+)/
-    type = $1.strip.tr(' ', '_')
+    type = $1.strip.tr(" ", "_")
     is_divided = true
   when /test_bpf: Summary: ([0-9]+) PASSED, ([0-9]+) FAILED, \[.+ JIT'ed\]$/
     success_test = $1
     fail_test = $2
   when /(PASS|FAIL)/
+    type = ""
     if is_divided
       stats << type + ".#{$1.downcase}: 1"
       is_divided = false
