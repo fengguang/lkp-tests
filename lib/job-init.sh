480439ba2df1f557cb0dc71ef7c3e1ddf4523973
Xu Xijian <hdxuxijian@163.com>
2020-09-01 13:05:00 +0800

    fix: don't mount result when run a docker job
    error log:
    mount.nfs: rpc.statd is not running  but is required for remote locking.
    mount.nfs: Either use '-o nolock' to keep locks local, or start statd.
    mount.nfs: Operation not permitted
    
    It's because in docker, mount operation is default not permitted, add
    "--cap -add sys_admin" when "docker run" can enable mount, but it's
    unrecommended for safety reason. So we avoid mount and sync result by
    uploading.
    
    Signed-off-by: Xu Xijian <xuxijian@huawei.com>

--- lib/job-init.sh
+++ lib/job-init.sh
@@ -53,11 +53,13 @@ setup_result_service()
 	[ -n "$NO_NETWORK" ] && return 1
 	[ -n "$RESULT_SERVER" ] || RESULT_SERVER=$LKP_SERVER
 
-	if [ "$os_mount" = 'nfs' ] || [ -z "$os_mount" ]; then
-		supports_netfs 'nfs'    && result_service=$RESULT_SERVER:/result   && return
-	elif [ "$os_mount" = 'cifs' ] || [ -z "$os_mount" ]; then
-		supports_netfs 'cifs'   && result_service=//$RESULT_SERVER/result  && return
-	fi
+	is_docker || {
+		if [ "$os_mount" = 'nfs' ] || [ -z "$os_mount" ]; then
+			supports_netfs 'nfs'    && result_service=$RESULT_SERVER:/result   && return
+		elif [ "$os_mount" = 'cifs' ] || [ -z "$os_mount" ]; then
+			supports_netfs 'cifs'   && result_service=//$RESULT_SERVER/result  && return
+		fi
+	}
 
 	supports_raw_upload && result_service=raw_upload && return
 
