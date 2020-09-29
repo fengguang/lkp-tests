218f3e212b8fe331250df16d6f5d667d81430259
Xia Hui <hui.xia@intel.com>
2020-09-29 07:46:34 -0400

    lib/tests/cassandra.sh: support jdk 11
    refer to:
    https://cassandra.apache.org/doc/latest/new/java11.html#using-java-11-to-build
    
    Signed-off-by: Xia Hui <hui.xia@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/cassandra.sh
+++ lib/tests/cassandra.sh
@@ -4,8 +4,10 @@ setup_java_home()
 {
 	if [ -d /usr/lib/jvm/java-1.11.0-openjdk ]; then
 		export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk
+		export CASSANDRA_USE_JDK11=true
 	elif [ -d /usr/lib/jvm/java-11-openjdk-amd64 ]; then
 		export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+		export CASSANDRA_USE_JDK11=true
 	elif [ -d /usr/lib/jvm/java-1.8.0-openjdk ]; then
 		export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 	elif [ -d /usr/lib/jvm/java-8-openjdk-amd64 ]; then
