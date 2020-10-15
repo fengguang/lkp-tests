68170097fa0362170e5b2de2497673a25e9a32d8
Chen Rong <rong.a.chen@intel.com>
2020-10-15 16:16:17 +0800

    lib/dmesg.rb: convert 'f90>]usb_hcd_irq' to 'usb_hcd_irq'
    $ rake spec spec=stats
    PWD = /lkp/rongc/.src-20201012-161407
    ENV['LKP_CORE_SRC'] = /lkp/rongc/.src-20201012-161407
    ENV['LKP_SRC'] = /lkp/rongc/.src-20201012-161407
    ENV['KERNEL_TESTS_SRC'] = /c/kernel-tests
    /usr/bin/ruby2.3 -I/var/lib/gems/2.3.0/gems/rspec-core-3.8.0/lib:/var/lib/gems/2.3.0/gems/rspec-support-3.8.0/lib /var/lib/gems/2.3.0/gems/rspec-core-3.8.0/exe/rspec --pattern spec/\*\*\{,/\*/\*\*\}/stats_spec.rb
    .......
    
    Finished in 1.54 seconds (files took 0.4395 seconds to load)
    7 examples, 0 failures
    
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/dmesg.rb
+++ lib/dmesg.rb
@@ -367,6 +367,10 @@ def analyze_error_id(line)
     # [   12.344185 ] INFO: Slab 0x(____ptrval____) objects=22 used=11 fp=0x(____ptrval____) flags=0x10201
     bug_to_bisect = oops_to_bisect_pattern line
     line = $1 + '(#) ' + $2
+  when /^[0-9a-z]+>\] (.+)/
+    # [   13.708945 ] [<0000000013155f90>] usb_hcd_irq
+    line = $1
+    bug_to_bisect = oops_to_bisect_pattern line
   else
     bug_to_bisect = oops_to_bisect_pattern line
   end
