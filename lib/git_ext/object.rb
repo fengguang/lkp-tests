fengguang/lkp-tests

commit 7903c65b58ce932fffe6c09409c26c7f0f171d2e

lib/git_ext/object.rb: move fix_commit_exist? to fixed?
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/git_ext/object.rb
@@ -181,6 +181,11 @@ module Git
         tag || sha[0..9]
       end
 
+      def fixed?(branch, commit)
+        short_sha = sha[0..7]
+        !@base.command("log --grep 'Fixes:' #{sha}..#{branch} | grep \"Fixes: #{short_sha}\"").empty?
+      end
+
       def reverted?(branch)
         reverted_subject = 'Revert "' + subject + '"'
         !@base.command("log --format=%s #{sha}..#{branch} | grep -x -m1 '#{reverted_subject}'").empty?

lib/git_ext/base.rb
@@ -44,11 +44,6 @@ module Git
       !command('branch', ['--list', '-a', pattern]).empty?
     end
 
-    def fix_commit_exist?(branch, commit)
-      short_commit = commit[0..7]
-      !command("log --grep 'Fixes:' #{commit}..#{branch} | grep \"Fixes: #{short_commit}\"").empty?
-    end
-
     def linux_last_release_tag_strategy(commit_sha)
       version = patch_level = sub_level = rc = nil
 

commit 7892b977b15caccb86ebe70b0e2fc2e508f5e93f

lib/git_ext/object.rb: remove useless argument
Fixes: 7903c65b58ce (lib/git_ext/object.rb: move fix_commit_exist? to fixed?)
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/git_ext/object.rb
@@ -181,7 +181,7 @@ module Git
         tag || sha[0..9]
       end
 
-      def fixed?(branch, commit)
+      def fixed?(branch)
         short_sha = sha[0..7]
         !@base.command("log --grep 'Fixes:' #{sha}..#{branch} | grep \"Fixes: #{short_sha}\"").empty?
       end