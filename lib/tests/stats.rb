3aff88d1bad4e439d0df2e3988789df1aab39afb
Philip Li <philip.li@intel.com>
2020-11-26 14:29:51 +0800

    lib/tests: add LKP::Stats class to assist test stats script
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/tests/stats.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(File.dirname(File.dirname(File.realpath($PROGRAM_NAME))))
+
+require "#{LKP_SRC}/lib/log"
+
+module LKP
+  class Stats
+    def initialize
+      @stats = {}
+    end
+
+    def add(test_case, test_result)
+      test_case = test_case.strip.tr(' ', '_')
+      raise "#{test_case} has already existed" if @stats.key? test_case
+
+      test_result = test_result.strip.tr(' ', '_').downcase if test_result.instance_of? String
+
+      @stats[test_case] = test_result
+    end
+
+    # mapping: { 'ok' => 'pass', 'not_ok' => 'fail' }
+    def dump(mapping = {})
+      @stats.each do |k, v|
+        v = mapping[v] || v
+
+        if v.instance_of? String
+          puts "#{k}.#{v}: 1"
+        else
+          puts "#{k}: #{v}"
+        end
+      end
+    end
+
+    # def exit(warn)
+    #   log_warn warn if warn
+    #   exit 1
+    # end
+
+    # def validate_duplication(hash, key)
+    #   self.exit "#{key} has already existed" if hash.key? key
+    # end
+  end
+end
