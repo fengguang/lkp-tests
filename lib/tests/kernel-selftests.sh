7488a19e06555072909c3fdfe8e76c24fd22d3b7
Chen Rong <rong.a.chen@intel.com>
2020-12-09 08:23:31 +0800

    lib/tests/kernel-selftests.sh: ignore subtest landlock
    landlock depends on the new version libc6-dev:
    --------------
    In file included from common.h:14,
                     from fs_test.c:22:
    fs_test.c: In function 'layout1_move_mount':
    fs_test.c:1173:23: error: 'SYS_move_mount' undeclared (first use in this function); did you mean 'SYS_mount'?
      ASSERT_EQ(0, syscall(SYS_move_mount, AT_FDCWD, dir_s3d2, AT_FDCWD,))
    
    $ grep -r move_mount /usr/include/x86_64-linux-gnu/
    /usr/include/x86_64-linux-gnu/bits/syscall.h:#ifdef __NR_move_mount
    /usr/include/x86_64-linux-gnu/bits/syscall.h:# define SYS_move_mount __NR_move_mount
    --------------
    
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/kernel-selftests.sh
+++ lib/tests/kernel-selftests.sh
@@ -355,7 +355,8 @@ prepare_for_selftest()
 		# subtest lib cause kselftest incomplete run, it's a kernel issue
 		# report [LKP] [software node] 7589238a8c: BUG:kernel_NULL_pointer_dereference,address
 		# lkdtm is unstable [validated 1] f825d3f7ed
-		selftest_mfs=$(ls -d [c-l]*/Makefile | grep -v -e ^livepatch -e ^lib -e ^cpufreq -e ^kvm -e ^firmware -e ^lkdtm)
+		# landlock depends on new version libc6-dev
+		selftest_mfs=$(ls -d [c-l]*/Makefile | grep -v -e ^livepatch -e ^lib -e ^cpufreq -e ^kvm -e ^firmware -e ^lkdtm -e ^landlock)
 	elif [ "$group" = "group-02" ]; then
 		# m* is slow
 		# pidfd caused soft_timeout in kernel-selftests.splice.short_splice_read.sh.fail.v5.9-v5.10-rc1.2020-11-06.132952
