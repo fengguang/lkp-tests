34fcb829cb6e67f100a9040ea8ca4cc8e17e0377
Wei Jihui <weijihuiall@163.com>
2020-12-22 17:46:56 +0800

    sbin/install-dependencies: adjust file format
    Signed-off-by: Wei Jihui <weijihuiall@163.com>

--- sbin/install-dependencies.sh
+++ sbin/install-dependencies.sh
@@ -6,28 +6,27 @@ PROJECT_DIR=$(dirname $SCRIPT_DIR)
 . $PROJECT_DIR/lib/env.sh
 . $PROJECT_DIR/sbin/set-env.sh
 
-# choose install function base on 
-# DISTRIBUTION
+# choose install function base on DISTRIBUTION
 linux_dep()
 {
-source /etc/os-release
-case $ID in
-ubuntu|debian)
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get install -yqm ruby-git ruby-activesupport ruby-rest-client
-	;;
-openEuler|fedora|rhel|centos)
-	if type dnf > /dev/null 2>&1; then
-		dnf install -y --skip-broken ruby rubygems gcc gcc-c++ make ruby-devel git
-	else
-		yum install -y --skip-broken ruby rubygems gcc gcc-c++ make ruby-devel git
-	fi
-	sudo gem install -f git activesupport rest-client
-	;;
-*)
-	echo "$ID not support! please install dependencies manually."
-	;;
-esac
+	source /etc/os-release
+	case $ID in
+	ubuntu|debian)
+		export DEBIAN_FRONTEND=noninteractive
+		apt-get install -yqm ruby-git ruby-activesupport ruby-rest-client
+		;;
+	openEuler|fedora|rhel|centos)
+		if type dnf > /dev/null 2>&1; then
+			dnf install -y --skip-broken ruby rubygems gcc gcc-c++ make ruby-devel git
+		else
+			yum install -y --skip-broken ruby rubygems gcc gcc-c++ make ruby-devel git
+		fi
+		sudo gem install -f git activesupport rest-client
+		;;
+	*)
+		echo "$ID not support! please install dependencies manually."
+		;;
+	esac
 }
 
 
