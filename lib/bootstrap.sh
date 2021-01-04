e6a69e43546dd1f706034f064cd0f753d51137a7
Yu Chuan <13186087857@163.com>
2021-01-04 16:03:08 +0800

    lib/bootstrap.sh: fix error when os is not integrated into lkp-tests
    [Why]
    If the current os has not been integrated into lkp-tests, run method
    `detect_system`, then var `$_system_name_lowercase` will be 'unknown'.
    So we should adapt this.
    
    [Error]
    + local DISTRO=unknown
    ++ echo ntpdate
    ++ /home/yuchuan/lkp-tests/sbin/adapt-packages unknown
    Traceback (most recent call last):
            3: from /home/yuchuan/lkp-tests/sbin/adapt-packages:11:in `<main>'
            2: from /usr/share/ruby/psych.rb:497:in `load_file'
            1: from /usr/share/ruby/psych.rb:497:in `open'
    /usr/share/ruby/psych.rb:497:in `initialize': No such file or directory @ rb_sysopen - /home/yuchuan/lkp-tests/distro/adaptation/unknown (Errno::ENOENT)
    
    [How]
    If the current os has not been integrated into lkp-tests, then return.
    
    Signed-off-by: Yu Chuan <13186087857@163.com>

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -240,6 +240,10 @@ run_ntpdate()
 	has_cmd ntpdate || {
 		detect_system
 		local DISTRO=$_system_name_lowercase
+		[ "$DISTRO" == "unknown" ] && {
+			echo "[WARNING] Current os cannot be recognized by lkp-test, so skip run_ntpdate."
+			return
+		}
 		. $LKP_SRC/distro/installer/$DISTRO $(echo ntpdate | $LKP_SRC/sbin/adapt-packages $DISTRO)
 	}
 
