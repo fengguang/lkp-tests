67aa128b699cf763e6e56f7e083953258f9d00e0
Liu Yinsi <liuyinsi@163.com>
2021-01-26 15:52:32 +0800

    lib/log.sh: add square bracket
    before: 2021-01-26 14:53:36 ERROR -- cifs module is not loaded in
    after:  2021-01-26 15:00:27 [ERROR] -- cifs module is not loaded in
    
    [why]
    use square bracket make log more obvious.
    
    Signed-off-by: Liu Yinsi <liuyinsi@163.com>

--- lib/log.sh
+++ lib/log.sh
@@ -2,25 +2,25 @@
 
 log()
 {
-	echo "$(date +'%F %T') INFO -- $*"
+	echo "$(date +'%F %T') [INFO] -- $*"
 }
 
 log_info()
 {
-	echo "$(date +'%F %T') INFO -- $*"
+	echo "$(date +'%F %T') [INFO] -- $*"
 }
 
 log_debug()
 {
-	echo "$(date +'%F %T') DEBUG -- $*"
+	echo "$(date +'%F %T') [DEBUG] -- $*"
 }
 
 log_warn()
 {
-	echo "$(date +'%F %T') WARN -- $*" >&2
+	echo "$(date +'%F %T') [WARN] -- $*" >&2
 }
 
 log_error()
 {
-	echo "$(date +'%F %T') ERROR -- $*" >&2
+	echo "$(date +'%F %T') [ERROR] -- $*" >&2
 }
