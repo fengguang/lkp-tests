fengguang/lkp-tests

commit 69847f098c48a978468c42e38ae6ac784cc389ad

lib/dmesg.rb: remove the end of '#]' in error_id
There is similar error message in end Kernel panic:
--------------
[    0.317424 ] Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000
[    0.318712 ] CPU: 0 PID: 0 Comm: swapper Not tainted 5.12.0-rc7 #1
[    0.319274 ] Call Trace:
...
[    0.323802 ] ---[ end Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000  ]---
--------------

Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/dmesg.rb
@@ -411,6 +411,7 @@ def analyze_error_id(line)
 
   error_id = common_error_id(error_id)
 
+  error_id.gsub!(/#\]$/, '') # ---[ end Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000   ]---
   error_id.gsub!(/([a-z]:)[0-9]+\b/, '\1') # WARNING: at arch/x86/kernel/cpu/perf_event.c:1077 x86_pmu_start+0xaa/0x110()
   error_id.gsub!(/#:\[<#>\]\[<#>\]/, '') # RIP: 0010:[<ffffffff91906d8d>]  [<ffffffff91906d8d>] validate_chain+0xed/0xe80
   error_id.gsub!(/RIP:#:/, 'RIP:')       # RIP: 0010:__might_sleep+0x72/0x80

spec/stats/dmesg/71
@@ -1,0 +1,16 @@
+[    0.317424] Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000
+[    0.318712] CPU: 0 PID: 0 Comm: swapper Not tainted 5.12.0-rc7 #1
+[    0.319274] Call Trace:
+[    0.319516]  dump_stack+0x95/0xe0
+[    0.319844]  panic+0xb2/0x269
+[    0.320123]  __populate_section_memmap+0x6d/0x76
+[    0.320562]  sparse_init_nid+0x166/0x22d
+[    0.320943]  sparse_init+0x190/0x19e
+[    0.321267]  paging_init+0x82/0x8a
+[    0.321585]  native_pagetable_init+0x106/0x10e
+[    0.321988]  setup_arch+0xa65/0xb22
+[    0.322336]  ? vprintk_default+0x10/0x20
+[    0.322726]  start_kernel+0x5d/0x59f
+[    0.323061]  i386_start_kernel+0x43/0x45
+[    0.323418]  startup_32_smp+0x164/0x168
+[    0.323802] ---[ end Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000 ]---

spec/stats/dmesg/71.yaml
@@ -1,0 +1,9 @@
+boot_failures: 1
+
+# Kernel panic - not syncing: __populate_section_memmap: Failed to allocate .* bytes align=
+Kernel_panic-not_syncing:__populate_section_memmap:Failed_to_allocate#bytes_align=#nid=#from=: 1
+message:Kernel_panic-not_syncing:__populate_section_memmap:Failed_to_allocate#bytes_align=#nid=#from=: [    0.317424] Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000
+pattern:Kernel_panic-not_syncing:__populate_section_memmap:Failed_to_allocate#bytes_align=#nid=#from=: Kernel panic - not syncing: __populate_section_memmap: Failed to allocate .* bytes align=
+
+timestamp:last: 0.323802
+timestamp:Kernel_panic-not_syncing:__populate_section_memmap:Failed_to_allocate#bytes_align=#nid=#from=: 0.317424

commit 2ecaebaf6ac558b2ccea87e0ba58764c7cce5c5a

lib/dmesg.rb: don't remove '#]' if string is like '[##]'
Before:
general_protection_fault:#[#: 1
message:general_protection_fault:#[#: [   17.070650 ] general protection fault: 0000 [#1] PREEMPT SMP
pattern:general_protection_fault:#[#: general protection fault:]

After:
general_protection_fault:#[##]: 1
message:general_protection_fault:#[##]: [   17.070650 ] general protection fault: 0000 [#1] PREEMPT SMP
pattern:general_protection_fault:#[##]: general protection fault:

Fixes: 69847f098c48 (lib/dmesg.rb: remove the end of '#]' in error_id)
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/dmesg.rb
@@ -412,7 +412,7 @@ def analyze_error_id(line)
 
   error_id = common_error_id(error_id)
 
-  error_id.gsub!(/#\]$/, '') # ---[ end Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000   ]---
+  error_id.gsub!(/#\]$/, '') unless error_id =~ /\[[^\]]+\]$/ # ---[ end Kernel panic - not syncing: __populate_section_memmap: Failed to allocate 5242880 bytes align=0x1000 nid=0 from=0x0000000001000000   ]---
   error_id.gsub!(/([a-z]:)[0-9]+\b/, '\1') # WARNING: at arch/x86/kernel/cpu/perf_event.c:1077 x86_pmu_start+0xaa/0x110()
   error_id.gsub!(/#:\[<#>\]\[<#>\]/, '') # RIP: 0010:[<ffffffff91906d8d>]  [<ffffffff91906d8d>] validate_chain+0xed/0xe80
   error_id.gsub!(/RIP:#:/, 'RIP:')       # RIP: 0010:__might_sleep+0x72/0x80