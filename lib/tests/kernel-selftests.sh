382c4cf953770187f4db86649bbe8ceb5732cace
Chen Rong <rong.a.chen@intel.com>
2021-04-29 13:43:12 +0800

    lib/tests/kernel-selftests.sh: fix timeout for tests in /kselftests
    not ok 11 selftests: net: xfrm_policy.sh # TIMEOUT 45 seconds
    not ok 15 selftests: net: pmtu.sh # TIMEOUT 45 seconds
    not ok 18 selftests: net: udpgso_bench.sh # TIMEOUT 45 seconds
    not ok 29 selftests: net: tcp_fastopen_backup_key.sh # TIMEOUT 45 seconds
    not ok 34 selftests: net: fib_nexthops.sh # TIMEOUT 45 seconds
    not ok 36 selftests: net: icmp_redirect.sh # TIMEOUT 45 seconds
    
    Signed-off-by: Chen Rong <rong.a.chen@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/kernel-selftests.sh
+++ lib/tests/kernel-selftests.sh
@@ -658,6 +658,7 @@ run_tests()
 
 	# kselftest introduced runner.sh since kernel commit 42d46e57ec97 "selftests: Extract single-test shell logic from lib.mk"
 	[[ -e kselftest/runner.sh ]] && log_cmd sed -i 's/default_timeout=45/default_timeout=300/' kselftest/runner.sh
+	[[ -e /kselftests/kselftest/runner.sh ]] && log_cmd sed -i 's/default_timeout=45/default_timeout=300/' /kselftests/kselftest/runner.sh
 	for mf in $selftest_mfs; do
 		subtest=${mf%/Makefile}
 		subtest_config="$subtest/config"
