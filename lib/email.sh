882ec64a1f2ff2e162dd4d436dff500908942406
Li Ping <1477412247@qq.com>
2021-04-30 10:51:49 +0800

    add email feedback for rpmbuild-pkg
    tests/rpmbuild-pkg: add a new input parameter whether_email
    [usage]:
    rpmbuild-pkg:
      whether_email: 1
    
    when job failed, send an rpmbuild failed email.
    when job run success, send an rpmbuild success email.
    
    Signed-off-by: Li Ping <1477412247@qq.com>

--- lib/email.sh
+++ lib/email.sh
@@ -96,6 +96,41 @@ Compass-CI
 "
 }
 
+rpmbuild_report()
+{
+	email_content="To: $author_email
+Subject: [NOTIFY Compass-CI] rpmbuild report
+
+Dear $author_name:
+
+	$rpmbuild_result
+
+Regards
+Compass-CI
+"
+}
+
+rpmbuild_success_content()
+{
+	rpmbuild_result="Your RPM Package is successfully built.
+
+You can click the follow link to obtain your RPM Package:
+http://api.compass-ci.openeuler.org:20012/rpm/pub/${os}-${os_version}/${compat_os}/${os_arch}/Packages
+
+And you can click the following link to view RPM Package build details:
+http://api.compass-ci.openeuler.org:${SRV_HTTP_RESULT_PORT:-20007}$result_root"
+
+	rpmbuild_report
+}
+
+rpmbuild_failed_content()
+{
+	rpmbuild_result="Sorry to inform you that RPM Package built failed, you can click the following link to view details.
+http://api.compass-ci.openeuler.org:${SRV_HTTP_RESULT_PORT:-20007}$result_root"
+
+	rpmbuild_report
+}
+
 send_email()
 {
 	local subject=$1
