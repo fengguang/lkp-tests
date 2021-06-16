d6d48b254824f82f5b64cdcb698d9589c07d686f
Cao Xueliang <caoxl78320@163.com>
2021-06-16 16:17:54 +0800

    lib: add get testbox deadline function
    Signed-off-by: Cao Xueliang <caoxl78320@163.com>

--- lib/scheduler_client.rb
+++ lib/scheduler_client.rb
@@ -1,25 +1,40 @@
 # frozen_string_literal: true
 
 require 'rest-client'
+require_relative './unit'
 
 # scheduler client class
 class SchedulerClient
-  def initialize(host = '127.0.0.1', port = 3000)
+  def initialize(host = '172.17.0.1', port = 3000)
     @host = host
     @port = port
+    @url_prefix = url_prefix
   end
 
   def submit_job(job_json)
-    url_prefix = ''
+    resource = RestClient::Resource.new("#{@url_prefix}#{@host}:#{@port}/submit_job")
+    resource.post(job_json)
+  end
+
+  def renew_deadline(job_id, time)
+    resource = RestClient::Resource.new(
+      "#{@url_prefix}#{@host}:#{@port}/renew_deadline?job_id=#{job_id}&time=#{to_seconds(time)}")
+    resource.get
+  end
+
+  def get_deadline(testbox)
+    resource = RestClient::Resource.new(
+      "#{@url_prefix}#{@host}:#{@port}/get_deadline?testbox=#{testbox}")
+    resource.get
+  end
+
+  private def url_prefix
     if @host.match('.*[a-zA-Z]+.*')
       # Internet users should use domain name and https
-      url_prefix = 'https://'
+      @url_prefix = 'https://'
     else
       # used in intranet for now
-      url_prefix = 'http://'
+      @url_prefix = 'http://'
     end
-
-    resource = RestClient::Resource.new("#{url_prefix}#{@host}:#{@port}/submit_job")
-    resource.post(job_json)
   end
 end
