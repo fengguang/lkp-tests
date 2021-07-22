f82a6ed74959d990118ca639cd61ff2f46f49b2c
Philip Li <philip.li@intel.com>
2021-07-22 19:46:01 +0800

    filters/initrds.rb: fix "enqueue job results in SystemExit"
    SystemExit - exit:
    	filters/initrds.rb:7:in `exit'
    	filters/initrds.rb:7:in `block in expand_expression'
    	lib/erb.rb:51:in `eval'
    	lib/erb.rb:51:in `expand_expression'
    	lib/job.rb:640:in `evaluate_param'
    	lib/job.rb:687:in `block in expand_params'
    	lib/job.rb:79:in `block in for_each_in'
    	lib/job.rb:78:in `each'
    	lib/job.rb:78:in `for_each_in'
    	lib/job.rb:684:in `expand_params'
    	lib/queue.rb:534:in `update_enqueue_job'
    	lib/queue.rb:557:in `enqueue_one_job'
    	queue.rb:596:in `block in enqueue_job'
    
    Signed-off-by: Philip Li <philip.li@intel.com>

--- filters/initrds.rb
+++ filters/initrds.rb
@@ -4,16 +4,15 @@ LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
 
 require "#{LKP_SRC}/lib/log"
 
-exit unless self['kernel']
-exit unless self['initrds']
+def check_initrds
+  missing_initrds = Array(self['initrds']).reject do |initrd_name|
+    initrd_name = "#{initrd_name.tr('_', '-')}.cgz"
 
-missing_initrds = []
-Array(self['initrds']).each do |initrd_name|
-  initrd_name = "#{initrd_name.tr('_', '-')}.cgz"
-  initrd_path = File.join(File.dirname(File.realpath(self['kernel'])), initrd_name)
-  next if File.exist? initrd_path
+    initrd_path = File.join(File.dirname(File.realpath(self['kernel'])), initrd_name)
+    File.exist? initrd_path
+  end
 
-  missing_initrds << initrd_name
+  raise Job::ParamError, "initrd [#{missing_initrds.join(',')}] not exist in #{File.dirname(File.realpath(self['kernel']))}" unless missing_initrds.empty?
 end
 
-raise Job::ParamError, "initrd [#{missing_initrds.join(',')}] not exist in #{File.dirname(File.realpath(self['kernel']))}" unless missing_initrds.empty?
+check_initrds if self['kernel'] && self['initrds']
