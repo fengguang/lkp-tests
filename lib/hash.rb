fengguang/lkp-tests

commit f5b677b4c02dea7a9859c4c3960653e288d7dde6

lib/hash.rb: fix duplicate value if last character of key is '+'
Before in job.yaml:
initrds:
- linux_headers
- linux_perf
- linux_headers

After:
initrds:
- linux_headers
- linux_perf

Signef-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/hash.rb
@@ -117,7 +117,7 @@ def revise_hash(original, revisions, overwrite_top_keys = true)
     elsif k[-1] == '+'
       kk = k.chomp '+'
       _parent, _pkey, hash, key, _keys = lookup_hash(original, kk, true)
-      hash[key] = merge_accumulative(hash[key], v)
+      hash[key] = merge_accumulative(hash[key], v).uniq
       next false
     end
 

commit 2d18ed012e5b637822e9c2b9ca676a82c50faa8a

lib/hash.rb: only remove duplicate array items in revise_hash
$ rake spec spec=hash
start spec

PWD = /lkp/rongc/.src-20210730-134520
ENV['LKP_CORE_SRC'] = /lkp/rongc/.src-20210730-134520
ENV['LKP_SRC'] = /lkp/rongc/.src-20210730-134520
/usr/bin/ruby2.5 -I/var/lib/gems/2.5.0/gems/rspec-support-3.10.2/lib:/var/lib/gems/2.5.0/gems/rspec-core-3.10.1/lib /var/lib/gems/2.5.0/gems/rspec-core-3.10.1/exe/rspec --pattern spec/\*\*\{,/\*/\*\*\}/hash_spec.rb --tag ~env:cluster --tag ~speed:slow
Run options: exclude {:env=>"cluster", :speed=>"slow"}
.............................

Finished in 0.01338 seconds (files took 0.12743 seconds to load)
29 examples, 0 failures

Fixes: f5b677b4c02d (lib/hash.rb: fix duplicate value if last character of key is '+')
Signed-off-by: Chen Rong <rong.a.chen@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/hash.rb
@@ -117,7 +117,9 @@ def revise_hash(original, revisions, overwrite_top_keys = true)
     elsif k[-1] == '+'
       kk = k.chomp '+'
       _parent, _pkey, hash, key, _keys = lookup_hash(original, kk, true)
-      hash[key] = merge_accumulative(hash[key], v).uniq
+      merge_v = merge_accumulative(hash[key], v)
+      merge_v.uniq! if merge_v.instance_of? Array
+      hash[key] = merge_v
       next false
     end
 

spec/hash_spec.rb
@@ -15,6 +15,10 @@ expects = [
    'a.b.c: [1, 2]',
    'a.b.c+: 3',
    { 'a' => { 'b' => { 'c' => [1, 2, 3] } } }],
+  ['add duplicate scalar to array',
+   'a.b.c: [1, 2]',
+   'a.b.c+: 2',
+   { 'a' => { 'b' => { 'c' => [1, 2] } } }],
   ['add scalar to hash',
    'a.b.c: {1: 2}',
    'a.b.c+: 3',