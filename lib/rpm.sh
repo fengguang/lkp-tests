b7469e9e1af35fda75f467ff7b40dc3223d908a4
Wang Yong <wangyong0117@qq.com>
2021-08-05 11:10:04 +0800

    lib/rpm.sh: adapt SPEC based on $compat_os
    [why]
    get release from /etc/os-release is useless because we build RPM on
    openEuler, we show judge how SRPMS from based on $compat_os and add some
    solutions
    
    $ rpm --eval %{vimfiles_root}
    %{vimfiles_root}
    $ rpm --eval %{cmake_build}
    %{cmake_build}
    $ rpm --eval %{cmake_install}
    %{cmake_install}
    
    openEuler doesn't have these macros, so add define when necessary and
    change macro to normal form
    
    Signed-off-by: Wang Yong <wangyong0117@qq.com>

--- lib/rpm.sh
+++ lib/rpm.sh
@@ -23,11 +23,23 @@ add_repo()
 
 fix_on_distro()
 {
-	source /etc/os-release
-	case "${ID}-${VERSION_ID}" in
-		centos-8|openEuler-20.03)
+	case "${compat_os}" in
+		compat-centos8)
 			# New version macro adapt
 			sed -i '/%{?fedora} >\|%{?rhel} >/ s/$/ || %{?openEuler} == 2/' ${spec_dir}/*.spec
+			;;
+		compat-fedora33)
+			# Add fedora33 macro define
+			if $(grep vimfiles_root ${spec_dir}/*.spec >/dev/null); then
+				sed -i '1 i%global vimfiles_root %{_datadir}/vim/vimfiles' ${spec_dir}/*.spec
+			fi
+			# Fix fedora33 uniq macro: %cmake_build %cmake_install
+			# ref: https://docs.fedoraproject.org/en-US/packaging-guidelines/CMake/
+			sed -i '/^%cmake3/ s;$; ../;' ${spec_dir}/*.spec
+			sed -i '/^%cmake3/i mkdir build && cd build/' ${spec_dir}/*.spec
+			sed -i '/^%cmake_build/a %__cmake --build . %{?_smp_mflags} --verbose' ${spec_dir}/*.spec
+			sed -i '/^%cmake_install/a DESTDIR="%{buildroot}" %__cmake --install ./build' ${spec_dir}/*.spec
+			sed -i '/%cmake_install\|%cmake_build/ s/^/#/' ${spec_dir}/*.spec
 			;;
 	esac
 }
