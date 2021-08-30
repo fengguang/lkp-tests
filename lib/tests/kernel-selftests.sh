4e4ab429c5a79d6e0af2372ba85595187af4d35d
Li Zhijian <lizhijian@cn.fujitsu.com>
2021-08-30 18:01:14 +0800

    kernel-selftests: pickup 8ddc345a ("kernel-selftests: Export bpf instead")
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/kernel-selftests.sh
+++ lib/tests/kernel-selftests.sh
@@ -366,6 +366,8 @@ fixup_bpf()
 		sed -i "49aecho \$BASENAME_TEST | grep test_progs && CMD='./\$BASENAME_TEST -b mmap'" kselftest/runner.sh
 		sed -i "s/tap_timeout .\/\$BASENAME_TEST/eval \$CMD/" kselftest/runner.sh
 	fi
+	# tools/testing/selftests/bpf/tools/sbin/bpftool
+	export $linux_selftests_dir/tools/testing/selftests/bpf/sbin:$PATH
 }
 
 fixup_kmod()
