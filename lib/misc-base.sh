592eb7908e055bbba50d53f01ca08b1c14b64ad1
Yujie Liu <yujie.liu@intel.com>
2021-10-29 10:08:05 +0800

    lib/misc-base.sh: access etc files by lkp_src method
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/misc-base.sh
+++ lib/misc-base.sh
@@ -1,7 +1,9 @@
 # Some misc-functions
 
+. $LKP_SRC/lib/lkp_path.sh
+
 setup_proxy()
 {
-	proxy_file=$LKP_SRC/etc/proxy
+	proxy_file=$(lkp_src)/etc/proxy
 	[ -f "$proxy_file" ] && . "$proxy_file"
 }
