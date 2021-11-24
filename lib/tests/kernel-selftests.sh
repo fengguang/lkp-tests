2d997811d7c01a667422e05ae65a995cecb5741a
Jie2x Zhou <jie2x.zhou@intel.com>
2021-11-24 16:52:46 +0800

    Revert "lib/tests/kernel-selftests.sh: ignore subtest landlock"
    This reverts commit 7488a19e06555072909c3fdfe8e76c24fd22d3b7
    
    Reviewed-by: Ma Xinjian <xinjianx.ma@intel.com>
    Signed-off-by: Jie2x Zhou <jie2x.zhou@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/kernel-selftests.sh
+++ lib/tests/kernel-selftests.sh
@@ -468,8 +468,7 @@ prepare_for_selftest()
 		# subtest lib cause kselftest incomplete run, it's a kernel issue
 		# report [LKP] [software node] 7589238a8c: BUG:kernel_NULL_pointer_dereference,address
 		# lkdtm is unstable [validated 1] f825d3f7ed
-		# landlock depends on new version libc6-dev
-		selftest_mfs=$(ls -d [c-l]*/Makefile | grep -v -e ^ftrace -e ^livepatch -e ^lib -e ^cpufreq -e ^kvm -e ^firmware -e ^lkdtm -e ^landlock)
+		selftest_mfs=$(ls -d [c-l]*/Makefile | grep -v -e ^ftrace -e ^livepatch -e ^lib -e ^cpufreq -e ^kvm -e ^firmware -e ^lkdtm)
 	elif [ "$group" = "group-02" ]; then
 		# m* is slow
 		# pidfd caused soft_timeout in kernel-selftests.splice.short_splice_read.sh.fail.v5.9-v5.10-rc1.2020-11-06.132952
