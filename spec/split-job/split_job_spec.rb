177c8bd4096c1f90d25928107dd016fe7c8a029b
Li Zhijian <zhijianx.li@intel.com>
2021-12-16 19:04:07 +0800

    spec/split-job: add split job['split-job']['test'] testing
    We will create a new artifact LKP_SRC the this spec test.
    
    the final atomic splited jobs refer to:
    
    spec/split-job/1-test_1.yaml
    spec/split-job/1-test_2.yaml
    spec/split-job/1-test_3.yaml
    
    lizhijian@lkp-bingo:~/lkp/lkp-tests$ bundle exec rake spec SPEC=$PWD/spec/split-job/split_job_spec.rb 2>/dev/null
    PWD = /home/lizhijian/lkp/lkp-tests
    ENV['LKP_SRC'] = /lkp/lizhijian/src
    /usr/bin/ruby2.7 -I/var/lib/gems/2.7.0/gems/rspec-core-3.8.0/lib:/var/lib/gems/2.7.0/gems/rspec-support-3.8.0/lib
    /var/lib/gems/2.7.0/gems/rspec-core-3.8.0/exe/rspec
    /home/lizhijian/lkp/lkp-tests/spec/split-job/split_job_spec.rb
    .
    
    Finished in 0.55083 seconds (files took 0.16525 seconds to load)
    1 example, 0 failures
    
    Signed-off-by: Li Zhijian <zhijianx.li@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ spec/split-job/split_job_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'fileutils'
+require 'tmpdir'
+require "#{LKP_SRC}/lib/bash"
+require "#{LKP_SRC}/lib/yaml"
+
+describe 'lkp-split-job' do
+  before(:all) do
+    @tmp_src_dir = Dir.mktmpdir(nil, '/tmp')
+
+    `rsync -aix #{LKP_SRC}/ #{@tmp_src_dir}`
+    `rsync -aix #{LKP_SRC}/spec/split-job/tests #{LKP_SRC}/spec/split-job/include #{@tmp_src_dir}/`
+  end
+
+  after(:all) do
+    FileUtils.rm_rf @tmp_src_dir
+  end
+
+  before(:each) do
+    @tmp_dir = Dir.mktmpdir(nil, '/tmp')
+  end
+
+  after(:each) do
+    FileUtils.rm_rf @tmp_dir
+  end
+
+  it "split job['split-job']['test'] only" do
+    Dir.chdir(@tmp_src_dir) do
+      `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job -t lkp-tbox -o #{@tmp_dir} spec/split-job/1.yaml`
+
+      Dir[File.join(@tmp_dir, '1-*.yaml')].each do |actual_yaml|
+        actual = YAML.load_file(actual_yaml)
+        expect = YAML.load_file("#{LKP_SRC}/spec/split-job/#{File.basename(actual_yaml)}")
+
+        expect(actual).to eq expect
+      end
+    end
+  end
+end
